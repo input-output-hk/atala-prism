4948b47011f529630b72d7ff4fd3bd4758c00ade
Jeremy Townson <jeremy.townson@gmail.com>
2018-05-31 18:18:58 +0200

    Add a doc comment to the network app to describe the command line.

--- apps/network-app-1/src/main/scala/io/iohk/cef/NetworkApp1.scala
+++ apps/network-app-1/src/main/scala/io/iohk/cef/NetworkApp1.scala
@@ -15,6 +15,12 @@ import io.iohk.cef.demo.SimpleNode3._
 
 import ConfigExtensions._
 
+// Example command line
+// Create a standalone node
+// -Dnode-name=A -Dgateway-host=localhost -Dgateway-port=8080 -Dserver-host=localhost -Dserver-port=3000
+//
+// Create an additional node that references the first as a bootstrap peer.
+// -Dnode-name=B -Dgateway-host=localhost -Dgateway-port=9090 -Dserver-host=localhost -Dserver-port=4000 -Dbootstrap-peer=enode://6fc6fb2f1c9ec87825059bca627dd78698a8142ea07c9019668e06df37b3079e970776797cf90d9d847d8ab708566158f307917f184c1de228c786a7109c28bb@localhost:3000
 object NetworkApp1 extends App {
 
   implicit val untypedSystem = ActorSystem("network-app-1")
