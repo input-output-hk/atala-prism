4ccfb1d3c1116687e875b437f8060b206933568d
Jeremy Townson <jeremy.townson@gmail.com>
2018-06-01 10:54:57 +0200

    Add option to set bootstrap node. Demo can now send a message between peers.

--- apps/network-app-1/post-message.sh
+++ apps/network-app-1/post-message.sh
@@ -3,4 +3,4 @@ curl \
   -H "Content-Type: application/json" \
   -X POST \
   --data '{"message":"'$1'"}' \
-  http://localhost:8080/message
+  "http://$2/message"
--- apps/network-app-1/src/main/scala/io/iohk/cef/NetworkApp1.scala
+++ apps/network-app-1/src/main/scala/io/iohk/cef/NetworkApp1.scala
@@ -1,19 +1,10 @@
 package io.iohk.cef
 
-
 import akka.actor.ActorSystem
-import akka.actor.typed.{ActorRef, Behavior}
-import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.scaladsl.adapter._
-
 import akka.stream.ActorMaterializer
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.server.Route
 import com.typesafe.config.ConfigFactory
-import io.iohk.cef.demo.SimpleNode3
-import io.iohk.cef.demo.SimpleNode3._
-
-import ConfigExtensions._
+import io.iohk.cef.ConfigExtensions._
 
 // Example command line
 // Create a standalone node
@@ -29,34 +20,11 @@ object NetworkApp1 extends App {
 
   val config = ConfigFactory.load
 
-  val gatewayHost = config.getString("gateway-host")
-  val gatewayPort = config.getInt("gateway-port")
-  val serverHost = config.getString("server-host")
-  val serverPort = config.getInt("server-port")
-  val nodeName = config.getString("node-name")
-  val bootstrapPeer = config.getOption(_.getURI("bootstrap-peer"))
+  val behavior = BehaviorRoot.start(
+    config.getString("node-name"),
+    config.getString("server-host"), config.getInt("server-port"),
+    config.getString("gateway-host"), config.getInt("gateway-port"),
+    config.getOption(_.getURI("bootstrap-peer")))
 
-  val start: Behavior[String] = Behaviors.setup {
-    context =>
-
-      val nodeActor: ActorRef[NodeCommand] =
-        context.spawn(new SimpleNode3(nodeName, serverHost, serverPort, None).server, "NodeActor")
-
-      val httpGatewayRoute: Route = HttpGateway.route(
-        request => nodeActor ! Send(request.message))
-
-      val startupListener: Behavior[Started] = Behaviors.receiveMessage {
-        case Started(nodeUri) =>
-
-          Http().bindAndHandle(httpGatewayRoute, gatewayHost, gatewayPort.toInt)
-
-          Behavior.ignore
-      }
-
-      nodeActor ! Start(context.spawn(startupListener, "Node_Startup_Listener"))
-
-      Behaviors.receiveMessage(_ => Behavior.ignore)
-  }
-
-  untypedSystem.spawn(start, "NetworkAppActor")
+  untypedSystem.spawn(behavior, "NetworkAppActor")
 }
