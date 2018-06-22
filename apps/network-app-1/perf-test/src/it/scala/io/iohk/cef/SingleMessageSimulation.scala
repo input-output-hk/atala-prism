d03ee4cc531f6149707c9f25620b3c39fc225a5a
Jeremy Townson <jeremy.townson@gmail.com>
2018-06-22 10:51:21 +0200

    Adding multi-region hosts to the perf test inventory.

---
+++ apps/network-app-1/perf-test/src/it/scala/io/iohk/cef/SingleMessageSimulation.scala
@@ -0,0 +1,31 @@
+package io.iohk.cef
+
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ChainBuilder
+import io.gatling.http.Predef._
+
+import scala.util.Random
+
+class SingleMessageSimulation extends Simulation {
+
+  private val feeder: Iterator[Map[String, String]] =
+    Iterator.continually(Map("message" -> Random.alphanumeric.take(256).mkString))
+
+  private val sendMessage: ChainBuilder =
+    feed(feeder).exec(
+      http("SendMessage")
+        .post("/message")
+        .body(StringBody("""{"message":"${message}"}""")))
+
+  private val messageScenario =
+    scenario(s"Message scenario").exec(sendMessage)
+
+  private val httpConf = http
+    .baseURL("http://localhost:8000")
+    .contentTypeHeader("application/json; charset=utf-8")
+    .acceptHeader("application/json")
+
+  setUp(messageScenario.
+    inject(atOnceUsers(1))).
+    protocols(httpConf)
+}
