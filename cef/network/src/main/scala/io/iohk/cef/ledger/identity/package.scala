01817468f56a1a1bfd917f1c7ffe4c24c8d96a2f
Carlos Roberto Vargas Montero <carlos.vargas@iohk.io>
2018-07-16 21:31:49 -0600

    Adding some comments

--- cef/network/src/main/scala/io/iohk/cef/ledger/identity/package.scala
+++ cef/network/src/main/scala/io/iohk/cef/ledger/identity/package.scala
@@ -5,5 +5,6 @@ import akka.util.ByteString
 package object identity {
   type IdentityLedgerState = LedgerState[String, Set[ByteString]]
 
+  //Mimics the apply method
   def IdentityLedgerState(map: Map[String, Set[ByteString]] = Map()): IdentityLedgerState = LedgerState[String, Set[ByteString]](map)
 }
