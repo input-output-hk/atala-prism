dc71a0e870cd2eaed055a8dd99227d310f3a8cbe
Jeremy Townson <jeremy.townson@gmail.com>
2018-07-31 12:01:42 +0200

    Stage the correct change.

--- cef/network/src/main/scala/io/iohk/cef/network/transport/NetworkTransport.scala
+++ cef/network/src/main/scala/io/iohk/cef/network/transport/NetworkTransport.scala
@@ -1,28 +1,44 @@
 package io.iohk.cef.network.transport
 
+object NetworkTransport {
+  class TransportInitializationException extends RuntimeException
+}
+
 /**
-  * TransportProtocol defines an abstraction over network transports,
-  * such as TCP, TLS, UDP or RLPx.
+  * NetworkTransports define a p2p abstraction over TCP, TLS, UDP, RLPx, etc.
   */
-abstract class TransportProtocol3[NodeId, Message](
+abstract class NetworkTransport[NodeId, Message](
     /**
       * The messageHandler receives inbound messages from peers.
       */
     messageHandler: (NodeId, Message) => Unit) {
 
-  class TransportInitializationException extends RuntimeException
+  /**
+    * Although not specified here, you probably want your transport
+    * implementation to accept a Codec in its constructor.
+    * This is not specified here, however, in case some impls want to decode down to
+    * Array[Byte] whilst others to netty bytebuf or akka ByteString, etc.
+    */
+
 
   /**
     * The primary purpose of this method is to decouple the creation
-    * and the turning on of a transport.
+    * and the 'turning on' of a transport.
     *
     * This method should make the transport ready
     * for sending and receiving messages. This might mean, for example,
     * binding to an address.
     *
+    * This method should run synchronously.
+    *
     * @throws TransportInitializationException if initialization fails.
     */
   def start(): Unit
 
+  /**
+    * Send a message to another peer.
+    * @param nodeId the nodeId of the peer to which to send the message
+    * @param message the message body itself.
+    */
   def sendMessage(nodeId: NodeId, message: Message): Unit
 }
