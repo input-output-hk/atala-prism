29e16455bfd81b14893647113948a84773b22a7d
Carlos Roberto Vargas Montero <carlos.vargas@iohk.io>
2018-08-08 02:36:45 -0600

    Adding the tx processing logic

--- cef/network/src/main/scala/io/iohk/cef/ledger/chimeric/ChimericTx.scala
+++ cef/network/src/main/scala/io/iohk/cef/ledger/chimeric/ChimericTx.scala
@@ -4,30 +4,104 @@ import io.iohk.cef.ledger.{LedgerError, LedgerState, Transaction}
 
 class ChimericTx(fragments: Seq[ChimericTxFragment]) extends Transaction[ChimericStateValue] {
 
+  type StateEither = Either[LedgerError, LedgerState[ChimericStateValue]]
+
   private case class InputOutputValues(inputs: Value = Value.empty, outputs: Value = Value.empty)
 
-  override def apply(s: LedgerState[ChimericStateValue]): Either[LedgerError, LedgerState[ChimericStateValue]] = {
-    val currentState: LedgerState[ChimericStateValue] = ???
-    val ioValues = fragments.foldLeft[Either[LedgerError, LedgerState[ChimericStateValue]]](Right(currentState))(
-      (state, current) => {
-      state.flatMap(currentIOValues => {
+  override def apply(currentState: LedgerState[ChimericStateValue]): StateEither = {
+    val outputs = fragments.collect{ case o: Output => o }.zipWithIndex
+    val txOutRefs = outputs.map{ case (o, i) => (TxOutRef(txId, i), o.value) }
+    val newState = fragments.foldLeft[StateEither](testPreservationOfValue(Right(currentState)))(
+      (stateEither, current) => {
+      stateEither.flatMap(state => {
         current match {
-          case Withdrawal(address, value, _) =>
-            val addressKey = ChimericLedgerState.getAddressPartitionId(address)
-            val addressValue =
-              s.get(addressKey).collect{ case ValueHolder(value) => value }
-                .getOrElse(Value.empty)
-            if(addressValue >= value) {
-              Right(currentIOValues.put(addressKey, ValueHolder(addressValue - value)))
-            } else {
-              Left(InsufficientBalance(address, value))
-            }
-          case _ => ???
+          case Withdrawal(address, value, _) => handleWithdrawal(state, address, value)
+          case Input(txOutRef, value) => handleInput(state, txOutRef, value)
+          case Output(value) =>
+            val txOutRef = txOutRefs.headOption
+            require(txOutRef.isDefined)
+            require(txOutRef.get._2 == value)
+            handleOutput(state, txOutRef.get._1, value)
+          case Deposit(address, value) => handleDeposit(state, address, value)
+          case cc : CreateCurrency => handleCreateCurrency(state, cc)
+          case Mint(value, address) => handleDeposit(state, address, value)
+          case Fee(value, address) => handleWithdrawal(state, address, value)
         }
       })
     })
-    ???
+    newState
+  }
+
+  private def txId: String = ??? //Some hash function
+
+  private def handleCreateCurrency(state: LedgerState[ChimericStateValue], cc: CreateCurrency) = {
+    val createCurrencyKey = ChimericLedgerState.getCurrencyPartitionId(cc.currency)
+    val currencyOpt = state.get(createCurrencyKey)
+    if (currencyOpt.isDefined) {
+      Left(CurrencyAlreadyExists(cc.currency))
+    } else {
+      Right(state.put(createCurrencyKey, CreateCurrencyHolder(cc)))
+    }
+  }
+
+  private def handleDeposit(state: LedgerState[ChimericStateValue], address: Address, value: Value) = {
+    val addressKey = ChimericLedgerState.getAddressPartitionId(address)
+    val addressValueOpt =
+      state.get(addressKey).collect { case ValueHolder(value) => value }
+    Right(state.put(addressKey, ValueHolder(addressValueOpt.getOrElse(Value.empty) + value)))
+  }
+
+  private def handleOutput(state: LedgerState[ChimericStateValue],
+                           txOutRef: TxOutRef,
+                           value: Value) = {
+    val txOutKey = ChimericLedgerState.getUtxoPartitionId(txOutRef)
+    val txOutValueOpt =
+      state.get(txOutKey).collect { case ValueHolder(value) => value }
+    if (txOutValueOpt.isDefined) {
+      Left(UnspentOutputAlreadyExists(txOutRef))
+    } else {
+      Right(state.put(ChimericLedgerState.getUtxoPartitionId(txOutRef), ValueHolder(value)))
+    }
+  }
+
+  private def handleInput(state: LedgerState[ChimericStateValue], txOutRef: TxOutRef, value: Value) = {
+    val txOutKey = ChimericLedgerState.getUtxoPartitionId(txOutRef)
+    val txOutValueOpt =
+      state.get(txOutKey).collect { case ValueHolder(value) => value }
+    if (txOutValueOpt.isEmpty) {
+      Left(UnspentOutputNotFound(txOutRef))
+    } else if (txOutValueOpt.get != value) {
+      Left(UnspentOutputInvalidValue(txOutRef, value, txOutValueOpt.get))
+    } else {
+      Right(state.remove(txOutKey))
+    }
+  }
+
+  private def handleWithdrawal(state: LedgerState[ChimericStateValue], address: Address, value: Value) = {
+    val addressKey = ChimericLedgerState.getAddressPartitionId(address)
+    val addressValue =
+      state.get(addressKey).collect { case ValueHolder(value) => value }.getOrElse(Value.empty)
+    if (addressValue >= value) {
+      Right(state.put(addressKey, ValueHolder(addressValue - value)))
+    } else {
+      Left(InsufficientBalance(address, value))
+    }
   }
 
   override def partitionIds: Set[String] = fragments.foldLeft(Set[String]())((st, curr) => st ++ curr.partitionIds)
+
+  private def testPreservationOfValue(currentStateEither: StateEither): StateEither =
+    currentStateEither.flatMap { currentState =>
+    val totalValue = fragments.foldLeft(Value.empty)((state, current) =>
+      current match {
+        case input: TxInput => state + input.value
+        case output: TxOutput => state - output.value
+      }
+    )
+    if (totalValue == Value.empty) {
+      Right(currentState)
+    } else {
+      Left(ValueNotPreserved(totalValue))
+    }
+  }
 }
--- cef/network/src/main/scala/io/iohk/cef/ledger/chimeric/ChimericTxFragment.scala
+++ cef/network/src/main/scala/io/iohk/cef/ledger/chimeric/ChimericTxFragment.scala
@@ -19,27 +19,21 @@ sealed trait TxAction extends ChimericTxFragment
 case class Withdrawal(address: Address, value: Value, nonce: Int) extends TxInput {
   override def partitionIds: Set[String] = Set(ChimericLedgerState.getAddressPartitionId(address))
 }
-case class Mint(value: Value) extends TxInput {
+case class Mint(value: Value, address: Address) extends TxInput {
   override def partitionIds: Set[String] = Set()
 }
 case class Input(txOutRef: TxOutRef, value: Value) extends TxInput {
   override def partitionIds: Set[String] = Set(ChimericLedgerState.getUtxoPartitionId(txOutRef))
 }
-
-case class Fee(value: Value) extends TxOutput {
+case class Fee(value: Value, address: Address) extends TxOutput {
   override def partitionIds: Set[String] = Set()
 }
-case class Output(address: Address, value: Value) extends TxOutput {
-  override def partitionIds: Set[String] = Set(ChimericLedgerState.getAddressPartitionId(address))
+case class Output(value: Value) extends TxOutput {
+  override def partitionIds: Set[String] = Set(ChimericLedgerState.getUtxoPartitionId(address))
 }
 case class Deposit(address: Address, value: Value) extends TxOutput {
   override def partitionIds: Set[String] = Set()
 }
-
 case class CreateCurrency(currency: Currency) extends TxAction {
   override def partitionIds: Set[String] = Set(ChimericLedgerState.getCurrencyPartitionId(currency))
-}
-
-case class LedgerId(id: Int) extends TxMetaData {
-  override def partitionIds: Set[String] = Set()
 }
