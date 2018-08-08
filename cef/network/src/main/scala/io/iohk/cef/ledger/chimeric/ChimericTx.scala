3e90a9e720c6adec70bd59393f343dbf64a70efe
Carlos Roberto Vargas Montero <carlos.vargas@iohk.io>
2018-08-08 12:44:13 -0600

    Moving the partitionIds logic to the ChimericTx instead of the fragments

--- cef/network/src/main/scala/io/iohk/cef/ledger/chimeric/ChimericTx.scala
+++ cef/network/src/main/scala/io/iohk/cef/ledger/chimeric/ChimericTx.scala
@@ -9,39 +9,54 @@ class ChimericTx(fragments: Seq[ChimericTxFragment]) extends Transaction[Chimeri
   private case class InputOutputValues(inputs: Value = Value.empty, outputs: Value = Value.empty)
 
   override def apply(currentState: LedgerState[ChimericStateValue]): StateEither = {
-    val txOutRefs = getTxOutRefs
+    val txOutRefs = getFragmentTxOutRefs
     val newState = fragments.foldLeft[StateEither](testPreservationOfValue(Right(currentState)))(
       (stateEither, current) => {
-      stateEither.flatMap(state => {
-        current match {
-          case Withdrawal(address, value, _) => handleWithdrawal(state, address, value)
-          case Input(txOutRef, value) => handleInput(state, txOutRef, value)
-          case Output(value) =>
-            val txOutRef = txOutRefs.headOption
-            require(txOutRef.isDefined)
-            require(txOutRef.get._2 == value)
-            handleOutput(state, txOutRef.get._1, value)
-          case Deposit(address, value) => handleDeposit(state, address, value)
-          case cc : CreateCurrency => handleCreateCurrency(state, cc)
-          case Mint(value, address) => handleDeposit(state, address, value)
-          case Fee(value, address) => handleWithdrawal(state, address, value)
-        }
+        stateEither.flatMap(state => {
+          current match {
+            case Withdrawal(address, value, _) => handleWithdrawal(state, address, value)
+            case Input(txOutRef, value) => handleInput(state, txOutRef, value)
+            case Output(value) =>
+              val txOutRef = txOutRefs.headOption
+              require(txOutRef.isDefined)
+              require(txOutRef.get._2 == value)
+              handleOutput(state, txOutRef.get._1, value)
+            case Deposit(address, value) => handleDeposit(state, address, value)
+            case cc : CreateCurrency => handleCreateCurrency(state, cc)
+            case Mint(_) => Right(state)
+            case Fee(_) => Right(state)
+          }
+        })
       })
-    })
     newState
   }
 
   override def partitionIds: Set[String] = {
-    getTxOutRefs.map{ case (txOutRef, _) => ChimericLedgerState.getUtxoPartitionId(txOutRef)}.toSet ++
-      fragments.foldLeft(Set[String]())((st, curr) => st ++ curr.partitionIds)
+    //Special treatment for the tx outputs because there's an ordering component that is implicit in the output's
+    // position in the fragments Seq. In other words, not all the required info is in the Output type (and shouldn't).
+    val txOutRefPartitionIds =
+      getFragmentTxOutRefs.map{ case (txOutRef, _) => ChimericLedgerState.getUtxoPartitionId(txOutRef)}.toSet
+    val otherPartitionIds: Set[String] = fragments.map(_ match {
+      case Withdrawal(address, _, _) =>
+        Set(ChimericLedgerState.getAddressPartitionId(address))
+      case Input(txOutRef, _) =>
+        Set(ChimericLedgerState.getUtxoPartitionId(txOutRef))
+      case CreateCurrency(currency) =>
+        Set(ChimericLedgerState.getCurrencyPartitionId(currency))
+      case Deposit(_, _) => Set()
+      case Mint(_) => Set()
+      case Fee(_) => Set()
+      case Output(_) => Set()
+    }).toSet.flatten
+    otherPartitionIds ++ txOutRefPartitionIds
   }
 
-  private def getTxOutRefs: Seq[(TxOutRef, Value)] = {
+  private def getFragmentTxOutRefs: Seq[(TxOutRef, Value)] = {
     val outputs = fragments.collect{ case o: Output => o }.zipWithIndex
     outputs.map{ case (o, i) => (TxOutRef(txId, i), o.value) }
   }
 
-  private def txId: String = ??? //Some hash function
+  private def txId: ChimericTxId = ??? //Some hash function
 
   private def handleCreateCurrency(state: LedgerState[ChimericStateValue], cc: CreateCurrency) = {
     val createCurrencyKey = ChimericLedgerState.getCurrencyPartitionId(cc.currency)
--- cef/network/src/main/scala/io/iohk/cef/ledger/chimeric/ChimericTxFragment.scala
+++ cef/network/src/main/scala/io/iohk/cef/ledger/chimeric/ChimericTxFragment.scala
@@ -1,8 +1,6 @@
 package io.iohk.cef.ledger.chimeric
 
-sealed trait ChimericTxFragment {
-  def partitionIds: Set[String]
-}
+sealed trait ChimericTxFragment
 
 sealed trait TxInput extends ChimericTxFragment {
   def value: Value
@@ -14,24 +12,11 @@ sealed trait TxOutput extends ChimericTxFragment {
 
 sealed trait TxAction extends ChimericTxFragment
 
-case class Withdrawal(address: Address, value: Value, nonce: Int) extends TxInput {
-  override def partitionIds: Set[String] = Set(ChimericLedgerState.getAddressPartitionId(address))
-}
-case class Mint(value: Value, address: Address) extends TxInput {
-  override def partitionIds: Set[String] = Set()
-}
-case class Input(txOutRef: TxOutRef, value: Value) extends TxInput {
-  override def partitionIds: Set[String] = Set(ChimericLedgerState.getUtxoPartitionId(txOutRef))
-}
-case class Fee(value: Value, address: Address) extends TxOutput {
-  override def partitionIds: Set[String] = Set()
-}
-case class Output(value: Value) extends TxOutput {
-  override def partitionIds: Set[String] = Set() //Contextual info missing. Need to add partitionIds in the tx
-}
-case class Deposit(address: Address, value: Value) extends TxOutput {
-  override def partitionIds: Set[String] = Set()
-}
-case class CreateCurrency(currency: Currency) extends TxAction {
-  override def partitionIds: Set[String] = Set(ChimericLedgerState.getCurrencyPartitionId(currency))
-}
+case class Withdrawal(address: Address, value: Value, nonce: Int) extends TxInput
+case class Mint(value: Value) extends TxInput
+case class Input(txOutRef: TxOutRef, value: Value) extends TxInput
+case class Fee(value: Value) extends TxOutput
+//TODO: Add the identity concept here
+case class Output(value: Value) extends TxOutput
+case class Deposit(address: Address, value: Value) extends TxOutput
+case class CreateCurrency(currency: Currency) extends TxAction
