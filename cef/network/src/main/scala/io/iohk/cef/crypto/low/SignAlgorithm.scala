e518d3acdccea36bcfcee03c51ded65fc708bb71
Marc Esquerra <marc.esquerra@iohk.io>
2018-08-14 10:54:51 +0100

    Simplify crypot.low structure

---
+++ cef/network/src/main/scala/io/iohk/cef/crypto/low/SignAlgorithm.scala
@@ -0,0 +1,102 @@
+package io.iohk.cef.crypto.low
+
+import akka.util.ByteString
+
+/**
+  * Contract all signing algorithm implementations should follow
+  */
+sealed trait SignAlgorithm {
+
+
+  /**
+    * Computes the signature for the provided `source` bytes, using the provided
+    * `key`
+    *
+    * @param source     the bytes to be signed
+    * @param key        the cryptographic key to be used to perform the signature
+    *
+    * @return a cryptographic signature for the provided `source` bytes
+    */
+  def sign(source: ByteString, key: ByteString): ByteString
+
+  /**
+    * Validates the `signature` of the provided `source` bytes, using the provided `key`
+    *
+    * @param signature  the signature that need to be checked against `source`
+    * @param source     the bytes that need to be validated against the `signature`
+    * @param key        the cryptographic key to be used to perform the validation
+    *
+    * @return `true` if the `signature` corresponds to the `source` when validated using
+    *         the provided `key`. `false` otherwise
+    */
+  def validate(signature: ByteString, source: ByteString, key: ByteString): Boolean
+
+}
+
+/**
+  * Helper trait that allows the implementation of a `SignAlgorithm` basing it on
+  * `Array[Byte]` instead of `ByteString`
+  */
+sealed trait ArrayBasedSignAlgorithm extends SignAlgorithm {
+
+  /**
+    * Computes the signature for the provided `source` bytes, using the provided
+    * `key`
+    *
+    * @param source     the bytes to be signed
+    * @param key        the cryptographic key to be used to perform the signature
+    *
+    * @return a cryptographic signature for the provided `source` bytes
+    */
+  protected def sign(source: Array[Byte], key: Array[Byte]): Array[Byte]
+
+  /** @inheritdoc */
+  override final def sign(source: ByteString, key: ByteString): ByteString =
+    ByteString(sign(source.toArray, key.toArray))
+
+  /**
+    * Validates the `signature` of the provided `source` bytes, using the provided `key`
+    *
+    * @param signature  the signature that need to be checked against `source`
+    * @param source     the bytes that need to be validated against the `signature`
+    * @param key        the cryptographic key to be used to perform the validation
+    *
+    * @return `true` if the `signature` corresponds to the `source` when validated using
+    *         the provided `key`. `false` otherwise
+    */
+  protected def validate(signature: Array[Byte], source: Array[Byte], key: Array[Byte]): Boolean
+
+  /** @inheritdoc */
+  override final def validate(signature: ByteString, source: ByteString, key: ByteString): Boolean =
+    validate(signature.toArray, source.toArray, key.toArray)
+
+}
+
+/**
+  * Companion object to SignAlgorithm, containing all the implemented `SignAlgorithm`
+  */
+object SignAlgorithm {
+
+  /**
+    * `SignAlgorithm` based on the composition of a `CryptoAlgorithm` and a `HashAlgorithm`
+    *
+    * @param cryptoAlgorithm used to encrypt the signature, allowing the validation of the
+    *                        signature author
+    *
+    * @param hashAlgorithm used to ensure the content of the message has not been medled
+    *        with
+    */
+  case class Composed(cryptoAlgorithm: CryptoAlgorithm, hashAlgorithm: HashAlgorithm) extends SignAlgorithm {
+
+    /** @inheritdoc */
+    def sign(source: ByteString, key: ByteString): ByteString =
+      cryptoAlgorithm.encrypt(
+        hashAlgorithm(source),
+        key)
+
+    /** @inheritdoc */
+    def validate(signature: ByteString, source: ByteString, key: ByteString): Boolean =
+      cryptoAlgorithm.decrypt(signature, key) == hashAlgorithm(source)
+  }
+
+}
--- cef/network/src/main/scala/io/iohk/cef/crypto/low/package.scala
+++ cef/network/src/main/scala/io/iohk/cef/crypto/low/package.scala
@@ -1,15 +1,211 @@
 package io.iohk.cef.crypto
-
-trait CryptoLow extends low.HashAlgorithmPackageFragment
-                   with low.HasherPackageFragment
-                   with low.CryptoAlgorithmPackageFragment
-                   with low.CrypterPackageFragment
-                   with low.SignAlgorithmPackageFragment
-                   with low.SignerPackageFragment
+import akka.util.ByteString
 
 /**
   * Collection of low-level cryptographic primitives
   */
-package object low extends CryptoLow {
+package object low {
+
+
+
+      /*           *
+          HASHING
+       *           */
+
+
+  /**
+    * Hash the provided `source` bytes with the provided `algorithm`
+    *
+    * @param algorithm  the hashing algorithm to be used to perform the hashing
+    * @param source     the bytes to be encrypted
+    *
+    * @return a hashed version of the `source` bytes
+    */
+  def hashBytes(algorithm: HashAlgorithm)(source: ByteString): ByteString =
+    algorithm(source)
+
+
+
+
+  /**
+    * Extends `ByteString` with hashing related methods
+    *
+    * @param source     the bytes to be hashed
+    */
+  implicit class ByteStringHashOps(val source: ByteString) extends AnyVal {
+
+  /**
+    * Computes the hash with the provided `algorithm`
+    *
+    * @param algorithm  the hashing algorithm to be used to perform the hashing
+    *
+    * @return a hashed version of `source`
+    */
+    def hashWith(algorithm: HashAlgorithm): ByteString =
+      hashBytes(algorithm)(source)
+
+  }
+
+
+
+      /*           *
+          ENCRYPT
+          DECRYPT
+       *           */
+
+
+  /**
+    * Encrypts the provided `source` bytes with the provided `algorithm` and `key`
+    *
+    * @param algorithm  the encrypting algorithm to be used to perform the encryption
+    * @param source     the bytes to be encrypted
+    * @param key        the cryptographic key to be used to perform the encryption
+    *
+    * @return an encrypted version of the `source` bytes
+    */
+  def encryptBytes(algorithm: CryptoAlgorithm)(source: ByteString, key: ByteString): ByteString =
+    algorithm.encrypt(source, key)
+
+  /**
+    * Decrypts the provided `source` bytes with the provided `algorithm` and `key`
+    *
+    * @param algorithm  the decrypting algorithm to be used to perform the decryption
+    * @param source     the encrypted string of bytes to be decrypted
+    * @param key        the cryptographic key to be used to perform the decryption
+    *
+    * @return a decrypted version of the encrypted `source` if the `algorithm` and `key` are
+    *         correct, or gibberish otherwise
+    */
+  def decryptBytes(algorithm: CryptoAlgorithm)(source: ByteString, key: ByteString): ByteString =
+    algorithm.decrypt(source, key)
+
+
+
+
+  /**
+    * Extends `ByteString` with encryption and decryption related methods
+    *
+    * @param source     the bytes to be encrypted or decrypted
+    */
+  implicit class ByteStringCryptoOps(val source: ByteString) extends AnyVal {
+
+    /**
+      * Encrypts `source` with the provided `algorithm` and `key`
+      *
+      * @param algorithm  the encrypting algorithm to be used to perform the encryption
+      * @param key        the cryptographic key to be used to perform the encryption
+      *
+      * @return an encrypted version of the `source` bytes
+      */
+    def encryptWith(algorithm: CryptoAlgorithm, key: ByteString): ByteString =
+      encryptBytes(algorithm)(source, key)
+
+
+    /**
+      * Decrypts the encrypted `source` with the provided `algorithm` and `key`
+      *
+      * @param algorithm  the decrypting algorithm to be used to perform the decryption
+      * @param key        the cryptographic key to be used to perform the decryption
+      *
+      * @return a decrypted version of the encrypted `source` if the `algorithm` and `key` are
+      *         correct, or gibberish otherwise
+      */
+    def decryptWith(algorithm: CryptoAlgorithm, key: ByteString): ByteString =
+      decryptBytes(algorithm)(source, key)
+
+  }
+
+
+
+      /*           *
+          SIGNING
+       *           */
+
+
+  /**
+    * Computes the signature for the provided `source` bytes, using the provided
+    * `algorithm` and `key`
+    *
+    * @param algorithm  the signing algorithm to be used to generate the signature
+    * @param source     the bytes to be signed
+    * @param key        the cryptographic key to be used to perform the signature
+    *
+    * @return a cryptographic signature for the provided `source` bytes
+    */
+  def signBytes(algorithm: SignAlgorithm)(source: ByteString, key: ByteString): ByteString =
+    algorithm.sign(source, key)
+
+  /**
+    * Validates the `signature` of the provided `source` bytes, using the `algorithm` and `key`
+    *
+    * @param algorithm  the signing algorithm to be used to validate the signature
+    * @param signature  the signature that need to be checked against `source`
+    * @param source     the bytes that need to be validated against the `signature`
+    * @param key        the cryptographic key to be used to perform the validation
+    *
+    * @return `true` if the `signature` corresponds to the `source` when validated using
+    *         `algorithm` and `key`. `false` otherwise
+    */
+  def validateSignatureOfBytes(algorithm: SignAlgorithm)(signature: ByteString, source: ByteString, key: ByteString): Boolean =
+    algorithm.validate(signature, source, key)
+
+
+
+
+  /**
+    * Extends `ByteString` with siging related methods
+    *
+    * @param source     the bytes to be signed
+    */
+  implicit class ByteStringSignOps(val source: ByteString) extends AnyVal {
+
+    /**
+      * Computes the signature of `source`, using the provided `algorithm` and `key`
+      *
+      * @param algorithm  the signing algorithm to be used to generate the signature
+      * @param key        the cryptographic key to be used to perform the signature
+      *
+      * @return a cryptographic signature for the provided `source` bytes
+      */
+    def signWith(algorithm: SignAlgorithm, key: ByteString): ByteString =
+      signBytes(algorithm)(source, key)
+
+  }
+
+  /**
+    * Extends `ByteString` with signature validation related methods
+    *
+    * @param signature  the signature that need to be checked
+    */
+  implicit class ByteStringValidateSignatureOps(val signature: ByteString) extends AnyVal {
+
+    /**
+      * Starting point for a small DSL used to compute the validity of a
+      * signature.
+      *
+      * If it was a normal `def` and not a DSL, the params and return would be:
+      *
+      * @param signature  the signature that need to be checked against `source`
+      * @param source     the bytes that need to be validated against the `signature`
+      * @param algorithm  the signing algorithm to be used to validate the signature
+      * @param key        the cryptographic key to be used to perform the validation
+      *
+      * @return `true` if the `signature` corresponds to the `source` when validated using
+      *         `algorithm` and `key`. `false` otherwise
+      *
+      * USAGE:
+      *   signature.isSignatureOf(source).when(algorithm, key)
+      */
+    def isSignatureOf(source: ByteString): CanCheckIfIsSignatureOf =
+      CanCheckIfIsSignatureOf(signature, source)
+
+  }
+
+  /** Helper case class for the implementation of the signature validation DSL */
+  case class CanCheckIfIsSignatureOf(signature: ByteString, source: ByteString) {
+    def when(algorithm: SignAlgorithm, key: ByteString): Boolean =
+      validateSignatureOfBytes(algorithm)(signature, source, key)
+  }
+
 
 }
