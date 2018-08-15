4298514cef8a7b4bb63d030e6bc7975da94c19bd
Marc Esquerra <marc.esquerra@iohk.io>
2018-08-15 10:57:34 +0100

    Add property based testing for hashing

--- cef/network/src/test/scala/io/iohk/cef/crypto/low/HashingAlgorithmSpec.scala
+++ cef/network/src/test/scala/io/iohk/cef/crypto/low/HashingAlgorithmSpec.scala
@@ -4,11 +4,26 @@ import akka.util.ByteString
 import org.bouncycastle.util.encoders.Hex
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers._
+import org.scalatest.prop.PropertyChecks
 
-class HashingAlogorithmSpec extends FlatSpec {
+class HashingAlogorithmSpec extends FlatSpec
+                               with PropertyChecks {
 
   "kec256" should "get the correct result for a ByteString" in {
     ByteString("a").hashWith(HashAlgorithm.KEC256) shouldBe Hex.decode("3ac225168df54212a25c1c01fd35bebfea408fdac2e31ddd6f80a4bbf9a5f1cb")
+  }
+
+  "kec256" should "generate hashes that are unique for each ByteString" in {
+    val algo = HashAlgorithm.KEC256
+    forAll { (aString: String, bString: String) =>
+      val a = ByteString(aString)
+      val b = ByteString(bString)
+
+      if (aString == bString)
+        a.hashWith(algo) should === (b.hashWith(algo))
+      else
+        a.hashWith(algo) should !== (b.hashWith(algo))
+    }
   }
 
 }
