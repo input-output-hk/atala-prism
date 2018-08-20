3abdea54c70be1799a4aab9e0bd455d43db96358
Shailesh Patil <shaileshp@gmail.com>
2018-08-20 08:02:19 +0100

    code reformat

--- cef/network/src/main/scala/io/iohk/cef/raft/akka/fsm/RaftActor.scala
+++ cef/network/src/main/scala/io/iohk/cef/raft/akka/fsm/RaftActor.scala
@@ -10,9 +10,13 @@ import io.iohk.cef.raft.akka.fsm.model._
 import scala.reflect._
 import config.RaftConfiguration
 
-
-abstract class RaftActor extends Actor with PersistentFSM[RaftState, StateData, DomainEvent]
-  with Follower with Candidate with Leader with InitialState {
+abstract class RaftActor
+    extends Actor
+    with PersistentFSM[RaftState, StateData, DomainEvent]
+    with Follower
+    with Candidate
+    with Leader
+    with InitialState {
 
   type Command
 
@@ -21,35 +25,37 @@ abstract class RaftActor extends Actor with PersistentFSM[RaftState, StateData, 
   protected val raftConfig = RaftConfiguration(context.system)
 
   private val ElectionTimeoutTimerName = "election-timer"
-  val heartbeatInterval: FiniteDuration =  raftConfig.heartbeatInterval
+  val heartbeatInterval: FiniteDuration = raftConfig.heartbeatInterval
 
   var replicatedLog = ReplicatedLog.empty[Command](raftConfig.defaultAppendEntriesBatchSize)
   var nextIndex = LogIndexMap.initialize(Set.empty, replicatedLog.nextIndex)
   var matchIndex = LogIndexMap.initialize(Set.empty, 0)
 
-
   override def domainEventClassTag: ClassTag[DomainEvent] = classTag[DomainEvent]
 
   override def persistenceId: String = "RaftActor-" + self.path.name
 
-  override def applyEvent(domainEvent: DomainEvent, sd: StateData):StateData = domainEvent match  {
-    case GoToFollowerEvent(term) => term.fold(sd.forFollower()){ t => sd.forFollower(t)}
-    case GoToLeaderEvent() => sd.forLeader
-    case StartElectionEvent() => sd.forNewElection
-    case KeepStateEvent() => sd
+  override def applyEvent(domainEvent: DomainEvent, sd: StateData): StateData = domainEvent match {
+    case GoToFollowerEvent(term) =>
+      term.fold(sd.forFollower()) { t =>
+        sd.forFollower(t)
+      }
+    case GoToLeaderEvent()       => sd.forLeader
+    case StartElectionEvent()    => sd.forNewElection
+    case KeepStateEvent()        => sd
     case VoteForEvent(candidate) => sd.withVoteFor(candidate)
-    case IncrementVoteEvent() => sd.incVote
-    case VoteForSelfEvent() => sd.incVote.withVoteFor(sd.self)
-    case UpdateTermEvent(term) => sd.withTerm(term)
+    case IncrementVoteEvent()    => sd.incVote
+    case VoteForSelfEvent()      => sd.incVote.withVoteFor(sd.self)
+    case UpdateTermEvent(term)   => sd.withTerm(term)
   }
 
   startWith(Init, StateData.initial(self))
 
   when(Init)(initialConfiguration)
 
-  when(Follower ,stateTimeout = nextElectionTime)(followerEvents)
+  when(Follower, stateTimeout = nextElectionTime)(followerEvents)
 
-  when(Candidate ,stateTimeout = nextElectionTime)(candidateEvents)
+  when(Candidate, stateTimeout = nextElectionTime)(candidateEvents)
 
   when(Leader)(leaderEvents)
 
@@ -69,7 +75,6 @@ abstract class RaftActor extends Actor with PersistentFSM[RaftState, StateData, 
       stopHeartbeat()
   }
 
-
 // Helper for timeout
   def nextElectionTime(): FiniteDuration = randomElectionTimeout(
     from = raftConfig.electionTimeoutMin,
@@ -86,28 +91,25 @@ abstract class RaftActor extends Actor with PersistentFSM[RaftState, StateData, 
   // End Helper for timeout
 
   /** Start a new election */
-  def beginElection(sd: StateData):PersistentFSMState= {
-      goto(Candidate) applying StartElectionEvent()
+  def beginElection(sd: StateData): PersistentFSMState = {
+    goto(Candidate) applying StartElectionEvent()
   }
 
   /** Stop being the Leader */
-  def stepDown(sd: StateData):PersistentFSMState = {
+  def stepDown(sd: StateData): PersistentFSMState = {
     goto(Follower) applying GoToFollowerEvent()
   }
 
   /** Stay in current state and reset the election timeout */
-  def acceptHeartbeat():PersistentFSMState = {
+  def acceptHeartbeat(): PersistentFSMState = {
     stay()
   }
 
   // sender aliases, for readability
-  @inline def follower():ActorRef = sender()
-  @inline def candidate():ActorRef = sender()
-  @inline def leader():ActorRef = sender()
-  @inline def voter():ActorRef = sender()
+  @inline def follower(): ActorRef = sender()
+  @inline def candidate(): ActorRef = sender()
+  @inline def leader(): ActorRef = sender()
+  @inline def voter(): ActorRef = sender()
   // end of sender aliases
 
 }
-
-
-
