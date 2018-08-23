ddb1a8d14e7247b2a473f6dd3be82e5370145445
Shailesh Patil <shaileshp@gmail.com>
2018-08-23 14:07:52 +0100

    compile issue

--- src/test/scala/io/iohk/cef/consensus/raft/FollowerTest.scala
+++ src/test/scala/io/iohk/cef/consensus/raft/FollowerTest.scala
@@ -38,7 +38,6 @@ class FollowerTest extends RaftSpec with BeforeAndAfterEach
 
 
   it should "reply with Vote if Candidate has later Term than it" in {
-    restartMember()
 
     // given
     subscribeBeginAsFollower()
