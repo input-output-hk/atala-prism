b97165c6e480a99b0572b0c8088d0a7c4a4c6647
Jeremy <jeremy.townson@gmail.com>
2018-09-02 21:42:24 +0200

    Adding a 'forgiving' sequence operation on Futures that removes failed Futures from the sequence result.

---
+++ src/test/scala/io/iohk/cef/consensus/raft/FutureOpsSpec.scala
@@ -0,0 +1,30 @@
+package io.iohk.cef.consensus.raft
+
+import io.iohk.cef.consensus.raft.FutureOps.sequenceForgiving
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers._
+import org.scalatest.concurrent.ScalaFutures._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.concurrent.Future.failed
+
+class FutureOpsSpec extends FlatSpec {
+
+  "sequenceForgiving" should "be equivalent to Future.seqence for successful futures" in {
+    val futures: Seq[Future[Int]] = Seq(Future[Int](1), Future[Int](2))
+
+    val futureSequence = Future.sequence(futures).futureValue
+    val forgivingSequence = sequenceForgiving(futures).futureValue
+
+    futureSequence shouldBe forgivingSequence
+  }
+
+  it should "remove failed futures from the result sequence without failing" in {
+    val futures: Seq[Future[Int]] = Seq(Future[Int](1), failed[Int](new Exception()))
+
+    val forgivingSequence = sequenceForgiving(futures).futureValue
+
+    forgivingSequence shouldBe Seq(1)
+  }
+}
