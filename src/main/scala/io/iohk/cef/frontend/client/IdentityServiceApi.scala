d59afde69fc04a11c02a2794c2f691a87a6009f7
Shailesh Patil <shaileshp@gmail.com>
2018-09-11 22:28:46 +0100

    code cleanup

--- src/main/scala/io/iohk/cef/frontend/client/IdentityServiceApi.scala
+++ src/main/scala/io/iohk/cef/frontend/client/IdentityServiceApi.scala
@@ -47,14 +47,8 @@ class IdentityServiceApi(createActor: ActorRef)(implicit executionContext: Execu
           onSuccess(responseHandler) { response =>
             complete(
               response.result match {
-                case Right(_) => {
-                  println(response.result)
-                  StatusCodes.Created
-                }
-                case Left(_) => {
-                  println(response.result)
-                  StatusCodes.BadRequest
-                }
+                case Right(_) => StatusCodes.Created
+                case Left(_) => StatusCodes.BadRequest
               }
             )
           }
