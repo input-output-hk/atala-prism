01af05c1b137eda2d1689851408a4253fbb34e46
Alexis Hernandez <alexis22229@gmail.com>
2018-09-12 07:07:20 -0600

    CE-254: Integrate the high-level crypto with the identity transactions (#70)
    * CE-254: Integrate the high-level crypto with the identity transactions
    
    * CE-254: Improve the Signature equals and hashCode

--- src/main/scala/io/iohk/cef/ledger/identity/storage/scalike/SqlSyntax.scala
+++ src/main/scala/io/iohk/cef/ledger/identity/storage/scalike/SqlSyntax.scala
@@ -1,8 +1,7 @@
 package io.iohk.cef.ledger.identity.storage.scalike
 
-import java.security.PublicKey
-
-import io.iohk.cef.crypto.low.decodePublicKey
+import akka.util.ByteString
+import io.iohk.cef.crypto._
 import scalikejdbc.{WrappedResultSet, _}
 
 //Identity Ledger State
@@ -11,6 +10,16 @@ case class IdentityLedgerStateTable(identity: String, publicKey: Array[Byte])
 object IdentityLedgerStateTable extends SQLSyntaxSupport[IdentityLedgerStateTable] {
   override val tableName = Schema.IdentityStateTableName
 
-  def apply(si: ResultName[IdentityLedgerStateTable])(rs: WrappedResultSet): LedgerStateEntry[String, PublicKey] =
-    new LedgerStateEntry(rs.string(si.identity), decodePublicKey(rs.bytes(si.publicKey)))
+  def apply(si: ResultName[IdentityLedgerStateTable])(
+      rs: WrappedResultSet): LedgerStateEntry[String, SigningPublicKey] = {
+    val result = for {
+      key <- SigningPublicKey.decodeFrom(ByteString(rs.bytes(si.publicKey)))
+    } yield LedgerStateEntry(rs.string(si.identity), key)
+
+    // TODO: What to do with error handling?
+    result.fold(
+      fa = e => throw new RuntimeException(s"Unable to decode signing public key: $e"),
+      fb = identity
+    )
+  }
 }
--- src/test/scala/io/iohk/cef/ledger/identity/storage/IdentityLedgerStateStorageDaoDbTest.scala
+++ src/test/scala/io/iohk/cef/ledger/identity/storage/IdentityLedgerStateStorageDaoDbTest.scala
@@ -1,6 +1,6 @@
 package io.iohk.cef.ledger.identity.storage
 
-import io.iohk.cef.builder.RSAKeyGenerator
+import io.iohk.cef.builder.SigningKeyPairs
 import io.iohk.cef.ledger.identity.storage.scalike.dao.IdentityLedgerStateStorageDao
 import io.iohk.cef.ledger.identity.{IdentityLedgerState, IdentityLedgerStateStorageFixture}
 import org.scalatest.{MustMatchers, OptionValues, fixture}
@@ -10,39 +10,32 @@ trait IdentityLedgerStateStorageDaoDbTest
     extends fixture.FlatSpec
     with AutoRollback
     with MustMatchers
-    with RSAKeyGenerator
+    with SigningKeyPairs
     with OptionValues
     with IdentityLedgerStateStorageFixture {
 
   behavior of "LedgerStateStorage"
 
   it should "execute a slice" in { implicit session =>
-    val keys = (1 to 4).map(_ => generateKeyPair._1)
     val list = List(
-      ("one", keys(0)),
-      ("two", keys(1))
+      ("one", alice.public),
+      ("two", bob.public)
     )
     insertPairs(list)
 
     val storage = new IdentityLedgerStateStorageDao
     val state = storage.slice(Set("one"))
     state.keys mustBe Set("one")
-    state.get("one").value mustBe Set(keys(0))
+    state.get("one").value mustBe Set(alice.public)
     state.get("two") mustBe None
   }
 
   it should "update a state" in { implicit session =>
-    val keys = (1 to 5).map(_ => generateKeyPair._1)
-    val (zero, _) = generateKeyPair
-    val (zeroh, _) = generateKeyPair
-    val (one, _) = generateKeyPair
-    val (two, _) = generateKeyPair
-    val (three, _) = generateKeyPair
     val list = List(
-      ("zero", zero),
-      ("zero", zeroh),
-      ("one", one),
-      ("two", two)
+      ("zero", alice.public),
+      ("zero", bob.public),
+      ("one", carlos.public),
+      ("two", daniel.public)
     )
     insertPairs(list)
 
@@ -50,16 +43,16 @@ trait IdentityLedgerStateStorageDaoDbTest
     val state = storage.slice(Set("one", "zero"))
     val newState = new IdentityLedgerState(
       Map(
-        ("one", Set(one)),
-        ("three", Set(three))
+        ("one", Set(carlos.public)),
+        ("three", Set(elena.public))
       ))
     storage.update(state, newState)
 
     val editedState = storage.slice(Set("one", "two", "three", "zero"))
     editedState.keys mustBe Set("one", "two", "three")
 
-    editedState.get("one").value mustEqual Set(one)
-    editedState.get("two").value mustEqual Set(two)
-    editedState.get("three").value mustEqual Set(three)
+    editedState.get("one").value mustEqual Set(carlos.public)
+    editedState.get("two").value mustEqual Set(daniel.public)
+    editedState.get("three").value mustEqual Set(elena.public)
   }
 }
