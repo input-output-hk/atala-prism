38c967d4327b1e76066ed81c7fee724cb8c2bd5e
Carlos R Vargas-Montero <carlos.vargas@iohk.io>
2018-10-02 08:49:10 -0600

    Style changes

--- src/main/scala/io/iohk/cef/main/IndentityTxMain.scala
+++ src/main/scala/io/iohk/cef/main/IndentityTxMain.scala
@@ -40,63 +40,60 @@ import scala.io.StdIn
 
 object IndentityTxMain extends App {
 
-    trait DefaultLedgerConfig extends LedgerConfigBuilder {
-      self: ConfigReaderBuilder =>
+  trait DefaultLedgerConfig extends LedgerConfigBuilder {
+    self: ConfigReaderBuilder =>
 
-      import io.iohk.cef.network.encoding.rlp.RLPImplicits._
-      import io.iohk.cef.network.encoding.rlp._
-      val discoveryMsgSerializer = implicitly[ByteStringSerializable[DiscoveryWireMessage]]
-      override val clock: Clock = Clock.systemUTC()
-      override val ledgerConfig: LedgerConfig = LedgerConfig(config)
+    import io.iohk.cef.network.encoding.rlp.RLPImplicits._
+    import io.iohk.cef.network.encoding.rlp._
+    val discoveryMsgSerializer = implicitly[ByteStringSerializable[DiscoveryWireMessage]]
+    override val clock: Clock = Clock.systemUTC()
+    override val ledgerConfig: LedgerConfig = LedgerConfig(config)
 
-      override val networkConfig = NetworkConfig(config.getConfig("network"))
-      override val nodeIdStr = config.getString("node.id")
-      override val nodeId: NodeId = NodeId(nodeIdStr)
-      val networkConfiguration = NetworkConfiguration(
-        Some(TcpTransportConfiguration(networkConfig.serverConfig.address)))
-      val peerInfo = PeerInfo(NodeId(nodeIdStr), networkConfiguration)
-      val capabilities = Capabilities(Hex.decode(config.getString("node.capabilities")).head)
-      val nodeInfo = NodeInfo(
-        peerInfo.nodeId.id,
-        networkConfig.discoveryConfig.discoveryAddress,
-        networkConfig.serverConfig.address,
-        capabilities)
-      val nodeState = NodeState(
-        nodeInfo.id,
-        ServerStatus.Listening(nodeInfo.serverAddress),
-        ServerStatus.Listening(nodeInfo.discoveryAddress),
-        capabilities)
+    override val networkConfig = NetworkConfig(config.getConfig("network"))
+    override val nodeIdStr = config.getString("node.id")
+    override val nodeId: NodeId = NodeId(nodeIdStr)
+    val networkConfiguration = NetworkConfiguration(Some(TcpTransportConfiguration(networkConfig.serverConfig.address)))
+    val peerInfo = PeerInfo(NodeId(nodeIdStr), networkConfiguration)
+    val capabilities = Capabilities(Hex.decode(config.getString("node.capabilities")).head)
+    val nodeInfo = NodeInfo(
+      peerInfo.nodeId.id,
+      networkConfig.discoveryConfig.discoveryAddress,
+      networkConfig.serverConfig.address,
+      capabilities)
+    val nodeState = NodeState(
+      nodeInfo.id,
+      ServerStatus.Listening(nodeInfo.serverAddress),
+      ServerStatus.Listening(nodeInfo.discoveryAddress),
+      capabilities)
 
-      private val discoveryBehavior =
-        DiscoveryManager.behaviour(
-          networkConfig.discoveryConfig,
-          new DummyKnownNodesStorage(clock) with InMemoryTelemetry,
-          nodeState,
-          clock,
-          discoveryMsgSerializer,
-          discoveryMsgSerializer,
-          listenerFactory(discoveryMsgSerializer, discoveryMsgSerializer),
-          new SecureRandom(),
-          InMemoryTelemetry.registry
-        )
+    private val discoveryBehavior =
+      DiscoveryManager.behaviour(
+        networkConfig.discoveryConfig,
+        new DummyKnownNodesStorage(clock) with InMemoryTelemetry,
+        nodeState,
+        clock,
+        discoveryMsgSerializer,
+        discoveryMsgSerializer,
+        listenerFactory(discoveryMsgSerializer, discoveryMsgSerializer),
+        new SecureRandom(),
+        InMemoryTelemetry.registry
+      )
 
-      private def listenerFactory(
-          encoder: Encoder[DiscoveryWireMessage, ByteString],
-          decoder: Decoder[ByteString, DiscoveryWireMessage])(
-          context: ActorContext[DiscoveryRequest]): ActorRef[DiscoveryListenerRequest] = {
+    private def listenerFactory(
+        encoder: Encoder[DiscoveryWireMessage, ByteString],
+        decoder: Decoder[ByteString, DiscoveryWireMessage])(
+        context: ActorContext[DiscoveryRequest]): ActorRef[DiscoveryListenerRequest] = {
 
-        context.spawn(
-          DiscoveryListener
-            .behavior(
-              networkConfig.discoveryConfig,
-              UDPBridge.creator(networkConfig.discoveryConfig, encoder, decoder)),
-          "DiscoveryListener")
-      }
-
-      override val transports: Transports = new Transports(peerInfo)
-      override val networkDiscovery: NetworkDiscovery =
-        new DiscoveryManagerAdapter(discoveryBehavior)
+      context.spawn(
+        DiscoveryListener
+          .behavior(networkConfig.discoveryConfig, UDPBridge.creator(networkConfig.discoveryConfig, encoder, decoder)),
+        "DiscoveryListener")
     }
+
+    override val transports: Transports = new Transports(peerInfo)
+    override val networkDiscovery: NetworkDiscovery =
+      new DiscoveryManagerAdapter(discoveryBehavior)
+  }
 
   trait DefaultRaftConsensusConfigBuilder[Command] extends RaftConsensusConfigBuilder[Command] {
     self: DefaultLedgerConfig with ConfigReaderBuilder =>
