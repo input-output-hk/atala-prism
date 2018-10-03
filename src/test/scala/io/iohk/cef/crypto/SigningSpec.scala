78c6b96431d9d68f36ae1bd6b23d8ab16c819843
Jeremy Townson <jeremy.townson@gmail.com>
2018-10-03 12:57:45 +0200

    Lower amount of keypair generation in SigningSpec.

--- src/test/scala/io/iohk/cef/crypto/SigningSpec.scala
+++ src/test/scala/io/iohk/cef/crypto/SigningSpec.scala
@@ -4,6 +4,7 @@ import akka.util.ByteString
 import io.iohk.cef.crypto.encoding.TypedByteStringDecodingError
 import io.iohk.cef.crypto.encoding.TypedByteStringDecodingError.NioDecoderFailedToDecodeTBS
 import io.iohk.cef.network.encoding.nio._
+import io.iohk.cef.network.transport.tcp.NetUtils
 import org.scalatest.Matchers._
 import org.scalatest.prop.PropertyChecks
 import org.scalatest.{EitherValues, MustMatchers, WordSpec}
@@ -11,43 +12,23 @@ import io.iohk.cef.test.ScalacheckExctensions
 
 class SigningSpec extends WordSpec with MustMatchers with PropertyChecks with EitherValues with ScalacheckExctensions {
 
+  private val keypair1 = generateSigningKeyPair()
+  private val keypair2 = generateSigningKeyPair()
+
   case class User(name: String, age: Int)
 
   "generateSigningKeyPair" should {
-    "always generate different key pairs" in {
-      var publicKeys = Set.empty[ByteString]
-      var privateKeys = Set.empty[ByteString]
-
-      forAll { _: Int =>
-        val result = generateSigningKeyPair()
-
-        val pub = result.public.toByteString
-        val priv = result.`private`.toByteString
-
-        publicKeys.contains(pub) must be(false)
-        privateKeys.contains(priv) must be(false)
-
-        publicKeys += pub
-        privateKeys += priv
-      }
+    "generate different key pairs" in {
+      keypair1 == keypair2 mustBe false
     }
   }
 
   "sign" should {
-    "generate a signature for any ByteString input" in {
-      forAll { input: ByteString =>
-        val keys = generateSigningKeyPair()
-        val result = sign(input, keys.`private`)
-
-        result.toByteString mustNot be(empty)
-      }
-    }
-
     "generate a signature for any Entity input" in {
       forAll { (name: String, age: Int) =>
-        val keys = generateSigningKeyPair()
         val entity = User(name, age)
-        val result = sign(entity, keys.`private`)
+
+        val result = sign(entity, keypair1.`private`)
 
         result.toByteString mustNot be(empty)
       }
@@ -55,170 +36,120 @@ class SigningSpec extends WordSpec with MustMatchers with PropertyChecks with Ei
   }
 
   "isValidSignature" should {
-    "verify the signature of any ByteString with the right key" in {
-      forAll { input: ByteString =>
-        val keys = generateSigningKeyPair()
-        val signature = sign(input, keys.`private`)
-        val result = isValidSignature(input, signature, keys.public)
+    "fail to verify the signature of an entity with the wrong key" in {
+      val input = User("name", 12)
 
-        result must be(true)
-      }
+      val signature1 = sign(input, keypair1.`private`)
+      val signature2 = sign(input, keypair2.`private`)
+
+      isValidSignature(input, signature1, keypair2.public) mustBe false
+      isValidSignature(input, signature2, keypair1.public) mustBe false
     }
 
-    "fail to verify the signature of any ByteString with the wrong key" in {
-      forAll { input: ByteString =>
-        val keys = generateSigningKeyPair()
-        val signature = sign(input, keys.`private`)
+    "verify the signature of an entity with the right key" in {
+      val entity = User("name", 12)
 
-        forAll { _: Int =>
-          val nested = generateSigningKeyPair().public
-          val result = isValidSignature(input, signature, nested)
-          result must be(false)
-        }
-      }
-    }
+      val signature = sign(entity, keypair1.`private`)
 
-    "verify the signature of any Entity with the right key" in {
-      forAll { (name: String, age: Int) =>
-        val keys = generateSigningKeyPair()
-        val entity = User(name, age)
-        val signature = sign(entity, keys.`private`)
-        val result = isValidSignature(entity, signature, keys.public)
-
-        result must be(true)
-      }
-    }
-
-    "fail to verify the signature of any Entity with the wrong key" in {
-      forAll { (name: String, age: Int) =>
-        val keys = generateSigningKeyPair()
-        val entity = User(name, age)
-        val signature = sign(entity, keys.`private`)
-
-        forAll { _: Int =>
-          val nested = generateSigningKeyPair().public
-          val result = isValidSignature(entity, signature, nested)
-
-          result must be(false)
-        }
-      }
+      isValidSignature(entity, signature, keypair1.public) must be(true)
     }
   }
 
   "Signature.decodeFrom" should {
     "decode valid signature" in {
-      forAll { input: ByteString =>
-        val keys = generateSigningKeyPair()
-        val signature = sign(input, keys.`private`)
-        val result = Signature.decodeFrom(signature.toByteString)
+      val input = User("name", 12)
 
-        result.right.value.toByteString.toArray mustNot be(empty)
-      }
+      val signature = sign(input, keypair1.`private`)
+      val result = Signature.decodeFrom(signature.toByteString)
+
+      result.right.value.toByteString.toArray mustNot be(empty)
     }
 
     "fail to decode invalid signatures" in {
-      pending
+      val bytes = ByteString(NetUtils.randomBytes(1024))
+      val expected =
+        SignatureDecodeError.DataExtractionError(TypedByteStringDecodingError.NioDecoderFailedToDecodeTBS)
 
-      forAll { bytes: ByteString =>
-        val result = Signature.decodeFrom(bytes)
-        val expected =
-          SignatureDecodeError.DataExtractionError(TypedByteStringDecodingError.NioDecoderFailedToDecodeTBS)
+      val result = Signature.decodeFrom(bytes)
 
-        result.left.value must be(expected)
-      }
+      result.left.value must be(expected)
     }
 
     "fail to decode signatures with unsupported algorithms" in {
       val algorithm = "SHA256withRSA".flatMap(_.toByte :: 0.toByte :: Nil).toArray
+      val input = ByteString(NetUtils.randomBytes(1024))
 
-      forAll { input: ByteString =>
-        val keys = generateSigningKeyPair()
-        val signature = sign(input, keys.`private`)
+      val signature = sign(input, keypair1.`private`)
 
-        val index = signature.toByteString.indexOfSlice(algorithm)
-        val corruptedBytes = signature.toByteString.updated(index, 'X'.toByte)
+      val index = signature.toByteString.indexOfSlice(algorithm)
+      val corruptedBytes = signature.toByteString.updated(index, 'X'.toByte)
+      val expected = SignatureDecodeError.UnsupportedAlgorithm("XHA256withRSA")
 
-        val result = Signature.decodeFrom(corruptedBytes)
-        val expected = SignatureDecodeError.UnsupportedAlgorithm("XHA256withRSA")
+      val result = Signature.decodeFrom(corruptedBytes)
 
-        result.left.value must be(expected)
-      }
+      result.left.value must be(expected)
     }
   }
 
   "SigningPublicKey.decodeFrom" should {
     "decode valid public key" in {
-      forAll { _: Int =>
-        val key = generateSigningKeyPair().public
-        val result = SigningPublicKey.decodeFrom(key.toByteString)
+      val key = keypair1.public
 
-        result.right.value.toByteString.toArray mustNot be(empty)
-      }
+      val result = SigningPublicKey.decodeFrom(key.toByteString)
+
+      result.right.value.toByteString.toArray mustNot be(empty)
     }
 
     "fail to decode invalid public key" in {
-      pending
+      val bytes = ByteString(NetUtils.randomBytes(1024))
+      val expected = SigningPublicKeyDecodeError.DataExtractionError(NioDecoderFailedToDecodeTBS)
 
-      forAll { bytes: ByteString =>
-        val result = SigningPublicKey.decodeFrom(bytes)
-        val expected = SigningPublicKeyDecodeError.DataExtractionError(NioDecoderFailedToDecodeTBS)
+      val result = SigningPublicKey.decodeFrom(bytes)
 
-        result.left.value must be(expected)
-      }
+      result.left.value must be(expected)
     }
 
     "fail to decode public keys with unsupported algorithms" in {
       val algorithm = "SHA256withRSA".flatMap(_.toByte :: 0.toByte :: Nil).toArray
+      val key = keypair1.public
+      val index = key.toByteString.indexOfSlice(algorithm)
+      val corruptedBytes = key.toByteString.updated(index, 'X'.toByte)
+      val expected = SigningPublicKeyDecodeError.UnsupportedAlgorithm("XHA256withRSA")
 
-      forAll { _: Int =>
-        val key = generateSigningKeyPair().public
+      val result = SigningPublicKey.decodeFrom(corruptedBytes)
 
-        val index = key.toByteString.indexOfSlice(algorithm)
-        val corruptedBytes = key.toByteString.updated(index, 'X'.toByte)
-
-        val result = SigningPublicKey.decodeFrom(corruptedBytes)
-        val expected = SigningPublicKeyDecodeError.UnsupportedAlgorithm("XHA256withRSA")
-
-        result.left.value must be(expected)
-      }
+      result.left.value must be(expected)
     }
   }
 
   "SigningPrivateKey.decodeFrom" should {
     "decode valid private key" in {
-      forAll { _: Int =>
-        val key = generateSigningKeyPair().`private`
-        val result = SigningPrivateKey.decodeFrom(key.toByteString)
+      val key = keypair1.`private`
 
-        result.right.value.toByteString.toArray mustNot be(empty)
-      }
+      val result = SigningPrivateKey.decodeFrom(key.toByteString)
+
+      result.right.value.toByteString.toArray mustNot be(empty)
     }
 
     "fail to decode invalid private key" in {
-      pending
+      val bytes = ByteString(NetUtils.randomBytes(1024))
 
-      forAll { bytes: ByteString =>
-        val result = SigningPrivateKey.decodeFrom(bytes)
-        val expected = SigningPrivateKeyDecodeError.DataExtractionError(NioDecoderFailedToDecodeTBS)
+      val result = SigningPrivateKey.decodeFrom(bytes)
+      val expected = SigningPrivateKeyDecodeError.DataExtractionError(NioDecoderFailedToDecodeTBS)
 
-        result.left.value must be(expected)
-      }
+      result.left.value must be(expected)
     }
 
     "fail to decode private keys with unsupported algorithms" in {
       val algorithm = "SHA256withRSA".flatMap(_.toByte :: 0.toByte :: Nil).toArray
+      val key = keypair1.`private`
+      val index = key.toByteString.indexOfSlice(algorithm)
+      val corruptedBytes = key.toByteString.updated(index, 'X'.toByte)
+      val expected = SigningPrivateKeyDecodeError.UnsupportedAlgorithm("XHA256withRSA")
 
-      forAll { _: Int =>
-        val key = generateSigningKeyPair().`private`
+      val result = SigningPrivateKey.decodeFrom(corruptedBytes)
 
-        val index = key.toByteString.indexOfSlice(algorithm)
-        val corruptedBytes = key.toByteString.updated(index, 'X'.toByte)
-
-        val result = SigningPrivateKey.decodeFrom(corruptedBytes)
-        val expected = SigningPrivateKeyDecodeError.UnsupportedAlgorithm("XHA256withRSA")
-
-        result.left.value must be(expected)
-      }
+      result.left.value must be(expected)
     }
   }
 }
