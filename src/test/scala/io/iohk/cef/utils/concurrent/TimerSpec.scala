69c034bb595217d479dafe21d9ce3cdcc59d3ba7
Jeremy Townson <jeremy.townson@gmail.com>
2018-10-03 17:09:23 +0200

    Add a better cef timer and testcase.

---
+++ src/test/scala/io/iohk/cef/utils/concurrent/TimerSpec.scala
@@ -0,0 +1,36 @@
+package io.iohk.cef.utils.concurrent
+
+import java.lang.System.currentTimeMillis
+
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers._
+import org.scalatest.concurrent.ScalaFutures._
+
+import scala.concurrent.{Await, TimeoutException}
+import scala.concurrent.duration._
+
+class TimerSpec extends FlatSpec {
+  behavior of "Timer"
+
+  it should "run a task after a delay" in {
+    val t1 = currentTimeMillis()
+
+    Timer
+      .schedule(50 millis) {
+        val t2 = currentTimeMillis()
+        (t2 - t1).toInt should be >= 50
+      }
+      .futureValue
+  }
+
+  it should "cancel a task" in {
+    val c = Timer
+      .schedule(10 millis) {
+        println("Task completed. Oh dear!")
+      }
+
+    c.cancel()
+
+    a[TimeoutException] should be thrownBy Await.result(c, 50 millis)
+  }
+}
