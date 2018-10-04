7a42125e3456224ebabc8cf527a1f192092940e7
Carlos R Vargas-Montero <carlos.vargas@iohk.io>
2018-10-04 00:35:40 -0600

    Modifying NodeId to be compatible with PeerInfo

--- src/main/scala/io/iohk/cef/network/NodeId.scala
+++ src/main/scala/io/iohk/cef/network/NodeId.scala
@@ -6,19 +6,16 @@ import io.iohk.cef.utils.HexStringCodec._
   * A node id represents an addressable identity on a cef network.
   */
 case class NodeId private[network] (id: ByteString) {
+  require(id.size == NodeId.nodeIdBytes, s"A node ID has to be of is ${NodeId.nodeIdBytes} bytes")
   override def toString: String = toHexString(id)
 }
 
 object NodeId {
   val nodeIdBytes: Int = 2
-  val nodeIdBits: Int = 2 * 8
+  val nodeIdBits: Int = nodeIdBytes * 8
 
-  def apply(idBytes: Seq[Byte]): NodeId = {
-    if (idBytes.length != 2)
-      throw new IllegalArgumentException("A node ID is 2 bytes")
-    else
+  def apply(idBytes: Seq[Byte]): NodeId =
       NodeId(ByteString(idBytes.toArray))
-  }
 
   def apply(idHex: String): NodeId =
     NodeId(fromHexString(idHex))
