9674445418fd488144ddd162b3a88f35eabcb25e
Carlos R Vargas-Montero <carlos.vargas@iohk.io>
2018-10-05 14:15:43 -0600

    Review comments

--- src/main/scala/io/iohk/cef/main/builder/NodeConfigurationBuilder.scala
+++
@@ -1,6 +0,0 @@
-package io.iohk.cef.main.builder
-import io.iohk.cef.LedgerId
-import io.iohk.cef.ledger.LedgerConfig
-trait NodeConfigurationBuilder {
-  val ledgersConfig: Map[LedgerId, LedgerConfig]
-}
