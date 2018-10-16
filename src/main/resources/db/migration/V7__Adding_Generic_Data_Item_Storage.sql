f4030c3b82860c2bbabcb395de49c4f06c55e5ef
Carlos R Vargas-Montero <carlos.vargas@iohk.io>
2018-10-16 00:59:14 -0600

    Fixing migration sql scripts

--- src/main/resources/db/migration/V7__Adding_Generic_Data_Item_Storage.sql
+++ src/main/resources/db/migration/V7__Adding_Generic_Data_Item_Storage.sql
@@ -5,15 +5,15 @@ create table if not exists cef.data_table (
     UNIQUE(table_id, data_item)
 );
 
-create table if not exists cef.data_item_signature {
+create table if not exists cef.data_item_signature (
   data_item_id bigint not null,
   signature varbinary(5096) not null,
   signing_public_key varbinary(5096) not null,
   foreign key (data_item_id) references cef.data_table(data_item_id)
-};
+);
 
-create table if not exists cef.data_item_owner {
+create table if not exists cef.data_item_owner (
   data_item_id bigint not null,
   signing_public_key varbinary(5096) not null,
   foreign key (data_item_id) references cef.data_table(data_item_id)
-};
+);
