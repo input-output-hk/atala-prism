d419b33518ce8974d220c915f7e8a50a8770747b
Carlos R Vargas-Montero <carlos.vargas@iohk.io>
2018-10-15 23:48:22 -0600

    Adding a validate method to DataItem

--- src/main/scala/io/iohk/cef/data/DataItem.scala
+++ src/main/scala/io/iohk/cef/data/DataItem.scala
@@ -1,7 +1,25 @@
 package io.iohk.cef.data
 import io.iohk.cef.crypto._
+import io.iohk.cef.error.ApplicationError
 
 trait DataItem {
-  def signatures: Seq[(SigningPublicKey, Signature)]
+
+  /**
+    * Users/entities that whitnessed this item and signed it
+    * @return
+    */
+  def whitnesses: Seq[(SigningPublicKey, Signature)]
+
+  /**
+    * Users/entities with permission to eliminate this data item
+    * @return
+    */
   def owners: Seq[SigningPublicKey]
+
+  /**
+    * Validates the data item and returns a specific error or nothing.
+    * Does it make sense to return something else?
+    * @return
+    */
+  def validate: Either[ApplicationError, Unit]
 }
