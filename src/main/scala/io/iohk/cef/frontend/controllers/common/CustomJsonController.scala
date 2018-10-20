8a67d7ef1ea5042c02288824022a1136df6f8592
Alexis Hernandez <alexis.hernandez@iohk.io>
2018-10-19 23:50:00 -0600

    CE-351: Add the playsonify skeleton

---
+++ src/main/scala/io/iohk/cef/frontend/controllers/common/CustomJsonController.scala
@@ -0,0 +1,41 @@
+package io.iohk.cef.frontend.controllers.common
+
+import akka.http.scaladsl.model.HttpRequest
+import com.alexitc.playsonify.akka._
+import com.alexitc.playsonify.core.FutureApplicationResult
+import com.alexitc.playsonify.models.{ErrorId, ServerError}
+import org.slf4j.LoggerFactory
+
+abstract class CustomJsonController extends AbstractJsonController(new CustomJsonController.CustomJsonComponents) {
+
+  protected val logger = LoggerFactory.getLogger(this.getClass)
+
+  override protected def onServerError(error: ServerError, id: ErrorId): Unit = {
+    error.cause
+      .orElse {
+        logger.error(s"Server error: $error, id = ${error.id}")
+        None
+      }
+      .foreach { cause =>
+        logger.error(s"Server error: $error, id = $id")
+      }
+  }
+}
+
+object CustomJsonController {
+
+  class CustomJsonComponents extends JsonControllerComponents[Nothing] {
+
+    override def i18nService: SingleLangService = new SingleLangService
+
+    override def publicErrorRenderer: PublicErrorRenderer = new PublicErrorRenderer
+
+    override def authenticatorService: AbstractAuthenticatorService[Nothing] =
+      new AbstractAuthenticatorService[Nothing] {
+
+        override def authenticate(request: HttpRequest): FutureApplicationResult[Nothing] = {
+          throw new RuntimeException("Authentication is not supported.")
+        }
+      }
+  }
+}
