3df4dad1276e10ea54ccfc1a534e811799c9c18d
Alexis Hernandez <alexis.hernandez@iohk.io>
2018-10-23 22:05:04 -0600

    CE-356: Add the data item generic frontend

---
+++ src/main/scala/io/iohk/cef/frontend/controllers/ItemsGenericController.scala
@@ -0,0 +1,30 @@
+package io.iohk.cef.frontend.controllers
+
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Route
+import akka.stream.Materializer
+import io.iohk.cef.data.DataItem
+import io.iohk.cef.frontend.controllers.common.CustomJsonController
+import org.scalactic.Good
+import play.api.libs.json.{Format, JsObject, JsString}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class ItemsGenericController(implicit ec: ExecutionContext, mat: Materializer) extends CustomJsonController {
+
+  import Context._
+
+  def routes[T <: DataItem](prefix: String)(implicit format: Format[T]): Route = {
+    pathPrefix(prefix) {
+      pathEnd {
+        post {
+          publicInput(StatusCodes.Created) { ctx: HasModel[T] =>
+            // TODO: complete it
+            val r = JsObject.empty + ("status" -> JsString("OK"))
+            Future.successful(Good(r))
+          }
+        }
+      }
+    }
+  }
+}
---
+++ src/test/scala/io/iohk/cef/frontend/controllers/ItemsGenericControllerSpec.scala
@@ -0,0 +1,61 @@
+package io.iohk.cef.frontend.controllers
+
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import de.heikoseeberger.akkahttpplayjson.PlayJsonSupport
+import io.iohk.cef.data.{DataItem, DataItemError, Owner, Witness}
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.{MustMatchers, WordSpec}
+import play.api.libs.json.{Format, JsValue, Json}
+
+class ItemsGenericControllerSpec
+    extends WordSpec
+    with MustMatchers
+    with ScalaFutures
+    with ScalatestRouteTest
+    with PlayJsonSupport {
+
+  import ItemsGenericControllerSpec._
+
+  implicit val executionContext = system.dispatcher
+
+  val controller = new ItemsGenericController()
+
+  "POST /certificates" should {
+
+    lazy val routes = controller.routes[BirthCertificate]("birth-certificates")
+
+    "create a valid key-pair" in {
+      val body =
+        """
+          |{
+          |  "date": "01/01/2015",
+          |  "name": "Input Output HK"
+          |}
+        """.stripMargin
+      val request = Post("/birth-certificates", HttpEntity(ContentTypes.`application/json`, body))
+
+      request ~> routes ~> check {
+        status must ===(StatusCodes.Created)
+
+        val json = responseAs[JsValue]
+        println(json)
+      }
+    }
+  }
+}
+
+object ItemsGenericControllerSpec {
+
+  case class BirthCertificate(date: String, name: String) extends DataItem {
+    override def id: String = "birth-certificate"
+
+    override def witnesses: Seq[Witness] = ???
+
+    override def owners: Seq[Owner] = ???
+
+    override def apply(): Either[DataItemError, Unit] = ???
+  }
+
+  implicit val format: Format[BirthCertificate] = Json.format[BirthCertificate]
+}
