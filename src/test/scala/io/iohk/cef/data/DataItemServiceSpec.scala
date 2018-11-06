05805a7678707f6cfbb989a70068f6762a02a865
Carlos R Vargas-Montero <carlos.vargas@iohk.io>
2018-11-06 11:36:42 -0500

    Style changes

--- src/test/scala/io/iohk/cef/data/DataItemServiceSpec.scala
+++ src/test/scala/io/iohk/cef/data/DataItemServiceSpec.scala
@@ -17,8 +17,12 @@ import org.scalatest.mockito.MockitoSugar._
 class DataItemServiceSpec extends FlatSpec with NetworkFixture {
 
   private val table = mock[Table]
-  private def newService[T](transports: Transports, networkDiscovery: NetworkDiscovery)(implicit encDec: NioEncDec[T], canValidate: CanValidate[DataItem[T]]) =
-    new DataItemService(table, transports, networkDiscovery)
+  private def newService[T](transports: Transports, networkDiscovery: NetworkDiscovery)(
+      implicit encDec: NioEncDec[T],
+      itemEncDec: NioEncDec[DataItem[T]],
+      actionEncDec: NioEncDec[DataItemAction[T]],
+      canValidate: CanValidate[DataItem[T]]) =
+    new DataItemService[T](table, transports, networkDiscovery)
   private implicit val dataItemSerializable = mock[NioEncDec[String]]
   private implicit val actionSerializable = mock[NioEncDec[DataItemAction[String]]]
   private implicit val canValidate = new CanValidate[DataItem[String]] {
@@ -33,7 +37,8 @@ class DataItemServiceSpec extends FlatSpec with NetworkFixture {
 //  private implicit val enc: NioEncoder[StringDataItem] = NioEncoder[StringDataItem]
 //  private implicit val dec: NioDecoder[StringDataItem] = NioDecoder[StringDataItem]
 
-  private def signature[T](dataItem: DataItem[T])(implicit nioEncDec: NioEncDec[DataItem[T]]) = sign(dataItem, keypair.`private`)
+  private def signature[T](dataItem: DataItem[T])(implicit nioEncDec: NioEncDec[DataItem[T]]) =
+    sign(dataItem, keypair.`private`)
 //  private implicit val codec: NioEncDec[DataItem[String]] = new NioEncDec[DataItem[String]] {
 //    override def decode(u: ByteBuffer) = Some(dataItem)
 //    override def encode(t: DataItem[String]) = ByteBuffer.
@@ -50,20 +55,22 @@ class DataItemServiceSpec extends FlatSpec with NetworkFixture {
   behavior of "DataItemService"
 
   val bootstrap = randomBaseNetwork(None)
-  it should "insert a data item" in networks(bootstrap, randomBaseNetwork(Some(bootstrap))) { networks => {
-    val dataItem: DataItem[String] = mock[DataItem[String]]
-    val service = newService[String](networks.head.transports, networks.head.networkDiscovery)
-    service.insert(envelopeAction.map(_.dataItem))
-    verify(table).insert(envelopeAction.containerId, dataItem)
-  }
+  it should "insert a data item" in networks(bootstrap, randomBaseNetwork(Some(bootstrap))) { networks =>
+    {
+      val dataItem: DataItem[String] = mock[DataItem[String]]
+      val service = newService[String](networks.head.transports, networks.head.networkDiscovery)
+      service.insert(envelopeAction.map(_.dataItem))
+      verify(table).insert(envelopeAction.containerId, dataItem)
+    }
   }
 
-  it should "delete a data item" in networks(bootstrap, randomBaseNetwork(Some(bootstrap))) { networks => {
-    val dataItem: DataItem[String] = mock[DataItem[String]]
-    val signature = mock[Signature]
-    val service = newService[String](networks.head.transports, networks.head.networkDiscovery)
-    service.delete(envelopeAction.map(_.dataItem), signature)
-    verify(table).delete(envelopeAction.containerId, envelopeAction.content.dataItem, signature)
-  }
+  it should "delete a data item" in networks(bootstrap, randomBaseNetwork(Some(bootstrap))) { networks =>
+    {
+      val dataItem: DataItem[String] = mock[DataItem[String]]
+      val signature = mock[Signature]
+      val service = newService[String](networks.head.transports, networks.head.networkDiscovery)
+      service.delete(envelopeAction.map(_.dataItem), signature)
+      verify(table).delete(envelopeAction.containerId, envelopeAction.content.dataItem, signature)
+    }
   }
 }
