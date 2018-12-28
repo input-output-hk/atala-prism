24975a42b49f30a8f4d49f03eef7ab312cfdd596
Jeremy <jeremy.townson@gmail.com>
2018-12-28 17:17:27 +0100

    Update after review comments.

---
+++ main/io/iohk/cef/agreements/AgreementsMessage.scala
@@ -0,0 +1,14 @@
+package io.iohk.cef.agreements
+
+import io.iohk.cef.agreements.AgreementsService.UserId
+
+sealed trait AgreementMessage[T]
+
+object AgreementsMessage {
+
+  case class Propose[T](correlationId: String, proposedBy: UserId, data: T) extends AgreementMessage[T]
+
+  case class Agree[T](correlationId: String, agreedBy: UserId, data: T) extends AgreementMessage[T]
+
+  case class Decline[T](correlationId: String, declinedBy: UserId) extends AgreementMessage[T]
+}
--- main/io/iohk/cef/agreements/AgreementsService.scala
+++ main/io/iohk/cef/agreements/AgreementsService.scala
@@ -4,30 +4,22 @@ import java.util.concurrent.Future
 import io.iohk.cef.codecs.nio._
 import io.iohk.cef.network.{MessageStream, NodeId, PeerConfig}
 
-object Agreements {
-  type UserId = NodeId
-}
-import Agreements._
-
-/* Message types for proposing data and agreeing to that data */
-sealed trait AgreementMessage[T]
-
-case class Propose[T](correlationId: String, proposedBy: UserId, data: T) extends AgreementMessage[T]
-
-case class Agree[T](correlationId: String, agreedBy: UserId, data: T) extends AgreementMessage[T]
-
-case class Decline[T](correlationId: String, declinedBy: UserId) extends AgreementMessage[T]
+import AgreementsService._
 
 class AgreementsService[T: NioCodec](peerConfig: PeerConfig) {
-  /* Send agreement to a list of userId who you wish to agree something */
-  /* Successful execution should guarantee that all parties have received the Proposal. */
+  // Send agreement to a list of userId who you wish to agree something
+  // Successful execution should guarantee that all parties have received the Proposal.
   def propose(correlationId: String, data: T, to: List[UserId]): Future[Unit] = ???
 
-  /* agree to a proposal */
-  /* return an Agreement to the proposer containing the data agreed to */
-  /* (NB: this might be different to the data in the proposal */
+  // agree to a proposal
+  // return an Agreement to the proposer containing the data agreed to
+  // (NB: this might be different to the data in the proposal
   def agree(correlationId: String, data: T): Future[Unit] = ???
 
-  /* receive notifications of proposals and agreements */
+  // receive notifications of proposals and agreements
   val agreementEvents: MessageStream[AgreementMessage[T]] = ???
+}
+
+object AgreementsService {
+  type UserId = NodeId
 }
