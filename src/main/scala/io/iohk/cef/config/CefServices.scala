395e573f0b1485775a979f178fe33acf6e7620cc
Shailesh Patil <shaileshp@gmail.com>
2019-01-03 14:58:57 +0530

    Added the Dataitem Service to Cefservices

--- src/main/scala/io/iohk/cef/config/CefServices.scala
+++ src/main/scala/io/iohk/cef/config/CefServices.scala
@@ -1,35 +1,36 @@
 package io.iohk.cef.config
-import java.nio.file.Files
+import java.nio.file.Path
 import java.time.Clock
+import java.util.concurrent.ConcurrentHashMap
 
-import io.iohk.cef.LedgerId
 import io.iohk.cef.codecs.nio._
-import io.iohk.cef.codecs.nio.auto._
-import io.iohk.cef.consensus.raft.node.OnDiskPersistentStorage
-import io.iohk.cef.consensus.{Consensus, raft}
-import io.iohk.cef.ledger.storage.mv.{MVLedgerStateStorage, MVLedgerStorage}
-import io.iohk.cef.transactionservice.raft.{RaftConsensusInterface, RaftRPCFactory}
-import io.iohk.cef.transactionservice.{Envelope, NodeTransactionService}
-import io.iohk.cef.ledger.storage.{Ledger, LedgerStorage}
-import io.iohk.cef.ledger.{Block, BlockHeader, Transaction}
+import io.iohk.cef.data.{DataItemService, TableId}
+import io.iohk.cef.ledger.Transaction
+import io.iohk.cef.network.NetworkServices
 import io.iohk.cef.network.transport.Transports
-import io.iohk.cef.network.{Network, NetworkServices}
-import io.iohk.cef.transactionpool.TransactionPoolInterface
+import io.iohk.cef.transactionservice.NodeTransactionService
 import org.slf4j.LoggerFactory
 
+import scala.collection.JavaConverters._
 import scala.concurrent.ExecutionContext
 import scala.reflect.runtime.universe._
 
 object CefServices {
+  private val services = new ConcurrentHashMap[CefConfig, CefServices]().asScala
+
   def cefTransactionServiceChannel[State, Tx <: Transaction[State]](cefConfig: CefConfig)(
       implicit stateCodec: NioCodec[State],
       stateTypeTag: TypeTag[State],
       txCodec: NioCodec[Tx],
       txTypeTag: TypeTag[Tx],
       ec: ExecutionContext): NodeTransactionService[State, Tx] = {
-
-    new CefServices(cefConfig).cefTransactionServiceChannel()
+    services.getOrElseUpdate(cefConfig, new CefServices(cefConfig)).cefTransactionServiceChannel()
   }
+  def cefDataItemServiceChannel[T: NioCodec: TypeTag](
+      cefConfig: CefConfig,
+      tableId: TableId,
+      storagePath: Path): DataItemService[T] =
+    services.getOrElseUpdate(cefConfig, new CefServices(cefConfig)).cefDataItemServiceChannel(tableId,storagePath)
 }
 
 private[config] class CefServices(cefConfig: CefConfig) {
@@ -49,86 +50,12 @@ private[config] class CefServices(cefConfig: CefConfig) {
       txCodec: NioCodec[Tx],
       txTypeTag: TypeTag[Tx],
       ec: ExecutionContext): NodeTransactionService[State, Tx] = {
-
-    new NodeTransactionService[State, Tx](
-      consensusMap[State, Tx],
-      txNetwork[State, Tx],
-      blockNetwork[State, Tx],
-      cefConfig.peerConfig.nodeId
-    )
+    new TransactionServiceConfig(cefConfig, log, clock, transports, networkDiscovery).cefTransactionServiceChannel()
   }
+  def cefDataItemServiceChannel[T: NioCodec: TypeTag](tableId: TableId, storagePath: Path): DataItemService[T] =
+    new DataItemServiceConfig(cefConfig, tableId, storagePath, clock, transports, networkDiscovery)
+      .cefDataItemServiceChannel()
 
-  private def consensusMap[State, Tx <: Transaction[State]]()(
-      implicit stateCodec: NioCodec[State],
-      stateTypeTag: TypeTag[State],
-      txCodec: NioCodec[Tx],
-      txTypeTag: TypeTag[Tx],
-      ec: ExecutionContext): Map[LedgerId, (TransactionPoolInterface[State, Tx], Consensus[State, Tx])] = {
-
-    val raftConfig = cefConfig.consensusConfig.raftConfig.get
-    val ledgerConfig = cefConfig.ledgerConfig
-
-    val headerGenerator: Seq[Transaction[State]] => BlockHeader = _ => BlockHeader(clock.instant())
-
-    val ledgerStateStoragePath = Files.createTempFile(s"state-storage-${ledgerConfig.id}", "").toAbsolutePath
-    val ledgerStateStorage = new MVLedgerStateStorage[State](ledgerConfig.id, ledgerStateStoragePath)
-
-    val ledgerStoragePath = Files.createTempFile(s"ledger-storage-${ledgerConfig.id}", "").toAbsolutePath
-    val ledgerStorage: LedgerStorage[State, Tx] = new MVLedgerStorage[State, Tx](ledgerConfig.id, ledgerStoragePath)
-
-    val ledger: Ledger[State, Tx] = Ledger(ledgerConfig.id, ledgerStorage, ledgerStateStorage)
-
-    val txPool = TransactionPoolInterface[State, Tx](
-      headerGenerator,
-      ledgerConfig.maxBlockSize,
-      ledgerStateStorage,
-      ledgerConfig.defaultTransactionExpiration)
-
-    val raftNode = raft.raftNode(
-      raftConfig.nodeId,
-      raftConfig.clusterMemberIds,
-      new RaftRPCFactory[Block[State, Tx]](networkDiscovery, transports),
-      raftConfig.electionTimeoutRange,
-      raftConfig.heartbeatTimeoutRange,
-      stateMachineCallback(ledger, txPool),
-      new OnDiskPersistentStorage[Block[State, Tx]](raftConfig.nodeId)
-    )
-
-    val raftConsensus: raft.RaftConsensus[Block[State, Tx]] = new raft.RaftConsensus(raftNode)
-
-    val consensus = new RaftConsensusInterface[State, Tx](cefConfig.ledgerConfig.id, raftConsensus)
-
-    Map(cefConfig.ledgerConfig.id -> (txPool, consensus))
-  }
-
-  private def stateMachineCallback[State, Tx <: Transaction[State]](
-      ledger: Ledger[State, Tx],
-      txPool: TransactionPoolInterface[State, Tx])(block: Block[State, Tx])(
-      implicit stateCodec: NioCodec[State],
-      stateTypeTag: TypeTag[State],
-      txCodec: NioCodec[Tx],
-      txTypeTag: TypeTag[Tx]): Unit = {
-    ledger(block) match {
-      case Left(error) =>
-        log.error(s"Could not apply block $block to the ledger with id ${ledger.ledgerId}. Error: $error")
-      case Right(()) =>
-        val result = txPool.removeBlockTransactions(block)
-        if (result.isLeft) {
-          log.error(s"Could not apply block $block to the ledger with id ${ledger.ledgerId}. Error: $result")
-        }
-    }
-  }
-
-  private def txNetwork[State, Tx <: Transaction[State]](
-      implicit
-      txCodec: NioCodec[Tx],
-      txTypeTag: TypeTag[Tx]): Network[Envelope[Tx]] =
-    Network[Envelope[Tx]](networkDiscovery, transports)
-
-  private def blockNetwork[State, Tx <: Transaction[State]](
-      implicit stateCodec: NioCodec[State],
-      stateTypeTag: TypeTag[State],
-      txCodec: NioCodec[Tx],
-      txTypeTag: TypeTag[Tx]): Network[Envelope[Block[State, Tx]]] =
-    Network[Envelope[Block[State, Tx]]](networkDiscovery, transports)
+  def shutdown(): Unit =
+    transports.shutdown()
 }
---
+++ src/main/scala/io/iohk/cef/config/DataItemServiceConfig.scala
@@ -0,0 +1,45 @@
+package io.iohk.cef.config
+
+import java.nio.file.Path
+import java.time.Clock
+import java.util.UUID
+
+import io.iohk.cef.codecs.nio._
+import io.iohk.cef.codecs.nio.auto._
+import io.iohk.cef.data._
+import io.iohk.cef.data.query.{QueryEngine, QueryRequest, QueryResponse}
+import io.iohk.cef.data.storage.mv.MVTableStorage
+import io.iohk.cef.error.ApplicationError
+import io.iohk.cef.network.Network
+import io.iohk.cef.network.discovery.NetworkDiscovery
+import io.iohk.cef.network.transport.Transports
+import io.iohk.cef.transactionservice.Envelope
+
+import scala.reflect.runtime.universe._
+
+private[config] class DataItemServiceConfig(
+    cefConfig: CefConfig,
+    tableId: TableId,
+    storagePath:Path,
+    clock: Clock,
+    transports: Transports,
+    networkDiscovery: NetworkDiscovery) {
+
+  def cefDataItemServiceChannel[T: NioCodec: TypeTag](): DataItemService[T] = {
+
+    implicit val canValidate = new CanValidate[DataItem[T]] {
+      override def validate(t: DataItem[T]): Either[ApplicationError, Unit] = Right(())
+    }
+
+    val tableStorage = new MVTableStorage[T](tableId,storagePath)
+    val table = new Table(tableId,tableStorage)
+    val network = Network[Envelope[DataItemAction[T]]](networkDiscovery, transports)
+    val requestNetwork = Network[Envelope[QueryRequest]](networkDiscovery, transports)
+    val responseNetwork = Network[Envelope[QueryResponse[T]]](networkDiscovery, transports)
+
+    val queryEngine =
+      new QueryEngine(cefConfig.peerConfig.nodeId, table, requestNetwork, responseNetwork, () => UUID.randomUUID().toString)
+
+    new DataItemService[T](table, network,queryEngine)
+  }
+}
