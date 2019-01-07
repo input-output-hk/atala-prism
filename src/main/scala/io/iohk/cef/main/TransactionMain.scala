3726abd4a32847d2d11163187fe88090c6ad922b
Carlos R Vargas-Montero <carlos.vargas@iohk.io>
2019-01-06 18:51:55 -0600

    Adding a constructor. Catching sigterm

--- src/main/scala/io/iohk/cef/main/TransactionMain.scala
+++ src/main/scala/io/iohk/cef/main/TransactionMain.scala
@@ -7,28 +7,47 @@ import akka.stream.ActorMaterializer
 import akka.util.Timeout
 import io.iohk.cef.config.{CefConfig, CefServices}
 import io.iohk.cef.ledger.Transaction
+import sun.misc.Signal
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.io.StdIn
 
-class TransactionMain[S, T <: Transaction[S]](route: Route,
-                                              frontendConfig: FrontendConfig,
-                                              cefConfig: CefConfig)
-                                             (implicit actorSystem: ActorSystem, executionContext: ExecutionContext, timeout: Timeout, materializer: ActorMaterializer) {
+class TransactionMain[S, T <: Transaction[S]] private (route: Route,
+                                                       frontendConfig: FrontendConfig,
+                                                       cefConfig: CefConfig,
+                                                       bindingFuture: Future[Http.ServerBinding])
+                                             (implicit executionContext: ExecutionContext) {
 
-  private def startup(): Future[Http.ServerBinding] = {
-    Http()(actorSystem)
-      .bindAndHandle(route, frontendConfig.bindAddress.getHostName, frontendConfig.bindAddress.getPort)
-  }
-  
-  private def shutdown(binding: Http.ServerBinding): Future[Unit] = {
-    binding.unbind().map(_ => CefServices.shutdown)
-  }
+  Signal.handle(new Signal("TERM"), onSigTerm)
 
   def run(): Future[Unit] = {
-    val binding = startup()
     println("Press enter to shutdown")
     StdIn.readLine() // let it run until user presses return
-    binding.map(shutdown)
+    shutdown()
+  }
+
+  private def onSigTerm(signal: Signal): Unit = {
+    shutdown()
+  }
+
+  private def shutdown(): Future[Unit] = {
+    for {
+      binding <- bindingFuture
+      _ <- binding.unbind()
+    } yield {
+      CefServices.shutdown
+    }
+  }
+}
+
+object TransactionMain {
+  def apply[S, T <: Transaction[S]](route: Route,
+            frontendConfig: FrontendConfig,
+            cefConfig: CefConfig)
+           (implicit actorSystem: ActorSystem, executionContext: ExecutionContext, timeout: Timeout, materializer: ActorMaterializer) = {
+
+    val binding = Http()(actorSystem)
+      .bindAndHandle(route, frontendConfig.bindAddress.getHostName, frontendConfig.bindAddress.getPort)
+    new TransactionMain[S, T](route, frontendConfig, cefConfig, binding)
   }
 }
