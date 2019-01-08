a096ee1e5a5dfef4ec12c28aa1b6e18ed0c18910
Carlos R Vargas-Montero <carlos.vargas@iohk.io>
2019-01-07 22:21:31 -0600

    Adding tests

--- src/main/scala/io/iohk/cef/query/ledger/identity/IdentityQueryService.scala
+++ src/main/scala/io/iohk/cef/query/ledger/identity/IdentityQueryService.scala
@@ -1,6 +1,6 @@
-package io.iohk.query.ledger.identity
+package io.iohk.cef.query.ledger.identity
 
-import io.iohk.query.ledger.LedgerQueryService
+import io.iohk.cef.query.ledger.LedgerQueryService
 
 object IdentityQueryService {
   def apply(engine: IdentityQueryEngine): IdentityQueryService =
