fb44e81da76c1b04ba193f3f42f963fb373845f8
Shailesh Patil <shaileshp@gmail.com>
2019-01-11 12:31:54 +0000

    Added decline and addres the PR comments

--- src/main/scala/io/iohk/cef/main/AgreementsMain.scala
+++ src/main/scala/io/iohk/cef/main/AgreementsMain.scala
@@ -4,9 +4,8 @@ import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.util.Timeout
 import com.typesafe.config.ConfigFactory
-import io.iohk.cef.agreements.AgreementsMessage.Agree
+import io.iohk.cef.agreements.AgreementsMessage.{Agree, Decline, Propose}
 import io.iohk.cef.network.NodeId
-import io.iohk.cef.agreements.AgreementsMessage.Propose
 import io.iohk.cef.codecs.nio.auto._
 import io.iohk.cef.config.ConfigReaderExtensions._
 import io.iohk.cef.config.{CefConfig, CefServices}
@@ -16,7 +15,6 @@ import pureconfig.generic.auto._
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 
-// Usage -Dconfig.resource=agreements-node-[1|2]-app.conf
 object AgreementsMain extends App {
   args.foreach(println)
   val cefConfig: CefConfig = pureconfig.loadConfigOrThrow[CefConfig](ConfigFactory.load(args(0)))
@@ -32,10 +30,10 @@ object AgreementsMain extends App {
   agreementsService.agreementEvents.foreach({
     case p: Propose[String] =>
       println(s"Node '${cefConfig.peerConfig.nodeId}' has received proposal '$p'")
-      //For testing without curl
-      if (p.data == "hello there") agreementsService.agree(p.correlationId, p.data)
     case a: Agree[String] =>
       println(s"Node '${cefConfig.peerConfig.nodeId}' has received agreements '$a'")
+    case a: Decline[String] =>
+      println(s"Node '${cefConfig.peerConfig.nodeId}' has declined proposal '$a'")
     case _ => ()
   })
 
@@ -45,6 +43,5 @@ object AgreementsMain extends App {
     serviceApi.routes(prefix, agreementsService),
     agreementsMainConfig
   )
-  Thread.sleep(2000)
-  agreementsService.propose("an-id", "hello there", List(NodeId("2222")))
+
 }
