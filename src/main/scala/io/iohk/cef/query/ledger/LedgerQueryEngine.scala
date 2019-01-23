2fd9a569b18a6a0f780b6f3d65bfec36fac73124
Carlos R Vargas-Montero <carlos.vargas@iohk.io>
2019-01-23 16:46:42 -0600

    Allowing for querying all currencies

--- src/main/scala/io/iohk/cef/query/ledger/LedgerQueryEngine.scala
+++ src/main/scala/io/iohk/cef/query/ledger/LedgerQueryEngine.scala
@@ -3,7 +3,8 @@ package io.iohk.cef.query.ledger
 import io.iohk.cef.ledger.storage.LedgerStateStorage
 import io.iohk.cef.query.QueryEngine
 
-case class LedgerQueryEngine[S](ledgerStateStorage: LedgerStateStorage[S]) extends QueryEngine {
+case class LedgerQueryEngine[S](private val ledgerStateStorage: LedgerStateStorage[S]) extends QueryEngine {
   def get(partitionId: String): Option[S] = ledgerStateStorage.slice(Set(partitionId)).get(partitionId)
   def contains(partitionId: String): Boolean = get(partitionId).isDefined
+  def keys(): Set[String] = ledgerStateStorage.keys
 }
