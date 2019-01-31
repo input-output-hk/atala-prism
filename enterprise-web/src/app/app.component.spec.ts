d26270b3d190c4f4405999a5ea688d099b926297
Alexis Hernandez <alexis.hernandez@iohk.io>
2019-01-30 18:41:57 -0700

    CE-555: Enterprise web demo skeleton
    This setups a project using Angular with Bootstrap 4, including:
    - A simple modular layout is created: navbar + current view + footer.
    - A simple navigation bar is rendered with some example options.
    - An empty home page.
    - Basic routing rules.

---
+++ enterprise-web/src/app/app.component.spec.ts
@@ -0,0 +1,33 @@
+import { NO_ERRORS_SCHEMA, } from '@angular/core';
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+      schemas: [
+        NO_ERRORS_SCHEMA
+      ]
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'enterprise-web'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('enterprise-web');
+  });
+});
