0ca3748464ab7f5dcd6c7eeb8bb70fc5d915bba8
Alexis Hernandez <alexis.hernandez@iohk.io>
2019-02-07 22:58:25 -0700

    CE-585: Update the identity ledger component spec to use the provided spies

--- enterprise-web/src/app/components/identity-ledger/identity-ledger.component.spec.ts
+++ enterprise-web/src/app/components/identity-ledger/identity-ledger.component.spec.ts
@@ -4,34 +4,18 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IdentityLedgerComponent } from './identity-ledger.component';
 import { FormBuilder } from '@angular/forms';
 
-import { ErrorService } from '../../services/error.service';
-import { IdentityLedgerService } from '../../services/identity-ledger.service';
-import { KeyPairsService } from '../../services/key-pairs.service';
-import { IdentityRepository } from '../../services/identity.repository';
+import { providers } from '../../testing/spies';
 
 describe('IdentityLedgerComponent', () => {
   let component: IdentityLedgerComponent;
   let fixture: ComponentFixture<IdentityLedgerComponent>;
-
-  const errorServiceSpy: jasmine.SpyObj<ErrorService> = jasmine.createSpyObj('ErrorService', [
-    'renderErrors',
-    'hasWrongValue',
-    'getFieldError']);
-  const identityLedgerServiceSpy: jasmine.SpyObj<IdentityLedgerService> = jasmine.createSpyObj('IdentityLedgerService', ['claim']);
-  const keyPairsServiceSpy: jasmine.SpyObj<KeyPairsService> = jasmine.createSpyObj('KeyPairsService', ['create']);
-  const identityRepositorySpy: jasmine.SpyObj<IdentityRepository> = jasmine.createSpyObj('IdentityRepository', [
-    'getUnconfirmedIdentities',
-    'getConfirmedIdentities']);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ IdentityLedgerComponent ],
       providers: [
         FormBuilder,
-        { provide: ErrorService, useValue: errorServiceSpy },
-        { provide: IdentityLedgerService, useValue: identityLedgerServiceSpy },
-        { provide: KeyPairsService, useValue: keyPairsServiceSpy },
-        { provide: IdentityRepository, useValue: identityRepositorySpy }
+        ...providers
       ],
       schemas: [
         NO_ERRORS_SCHEMA
