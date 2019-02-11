aecd93c671d9dcfc98ebc97f3392223064346f4d
Carlos R Vargas-Montero <carlos.vargas@iohk.io>
2019-02-11 15:12:29 -0600

    Fixing tests and main classes

--- main/io/iohk/cef/config/test/CefConfigSpec.scala
+++ main/io/iohk/cef/config/test/CefConfigSpec.scala
@@ -3,6 +3,7 @@ package io.iohk.cef.config.test
 import io.iohk.cef.config.CefConfig
 import org.scalatest.FlatSpec
 import pureconfig.generic.auto._
+import io.iohk.cef.config.ConfigReaderExtensions._
 
 class CefConfigSpec extends FlatSpec {
   behavior of "CefConfig"
