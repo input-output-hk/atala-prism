092d0356879d002920708f39af6155fede5040de
Ezequiel Postan <ezequiel.postan@iohk.io>
2019-05-23 14:13:35 -0300

    A-741: Add proper logging to the Blockchain (#303)
    * A-741: Add proper logging to the `Blockchain` class
    
    * A-741: Address PR reviews
    
    Changes:
    - Set proper use of `trace`, `debug` and `info` methods
    - Use structured logging

--- atala/obft/blockchain/Blockchain.scala
+++ atala/obft/blockchain/Blockchain.scala
@@ -3,6 +3,7 @@ package atala.obft.blockchain
 // format: off
 
 import atala.clock._
+import atala.logging.{AtalaLogging, Loggable}
 import atala.obft.blockchain.models._
 import atala.obft.blockchain.storage._
 import io.iohk.decco._
@@ -11,7 +12,10 @@ import io.iohk.multicrypto._
 
 import scala.annotation.tailrec
 
-class Blockchain[Tx: Codec](validator: SegmentValidator, private[blockchain] val storage: BlockStorage[Tx])(keys: List[SigningPublicKey], maxNumOfAdversaries: Int) { blockchain =>
+class Blockchain[Tx: Codec: Loggable](validator: SegmentValidator, private[blockchain] val storage: BlockStorage[Tx])
+                           (keys: List[SigningPublicKey], maxNumOfAdversaries: Int) extends AtalaLogging { blockchain =>
+
+  import Blockchain.LoggingFormat._
 
   private[blockchain] case class BlockPointer(
     at: Hash,
@@ -33,19 +37,29 @@ class Blockchain[Tx: Codec](validator: SegmentValidator, private[blockchain] val
   val genesisBlockHash = hash(genesisBlock)
 
   private[blockchain] var headPointer: BlockPointer = {
-    BlockPointer(hash(getLatestBlockFromStorage()), blockchainHeight())
+    logger.info("Retrieving head pointer from db")
+    val latestBlock = getLatestBlockFromStorage()
+    val latestBlockHash = hash(latestBlock)
+    val chainHeight = blockchainHeight()
+    logger.info("Head pointer retrieved successfully", "blockHash" -> latestBlockHash, "chainHeight" -> chainHeight)
+    BlockPointer(latestBlockHash, chainHeight)
   }
 
   private def head: AnyBlock[Tx] = headPointer.forceGetPointedBlockFromStorage
 
   private val finalisedDelta = (3 * maxNumOfAdversaries) + 1
   private def finalizedHeadPointer(now: TimeSlot): BlockPointer = {
+    logger.trace("Calculating block pointer for finalized transactions", "current time slot" -> now)
     val firstInvalidTimeSlot = (now - finalisedDelta) getOrElse TimeSlot.zero
     @scala.annotation.tailrec
     def findPointer(p: BlockPointer): BlockPointer =
       p.forceGetPointedBlockFromStorage match {
-        case GenesisBlock(_) => p
-        case Block(body, _) if body.timeSlot < firstInvalidTimeSlot => p
+        case GenesisBlock(_) =>
+          logger.debug(s"Block pointer computed", "block pointer hash" -> p.at, "pointer height" -> p.blockchainHeight)
+          p
+        case Block(body, _) if body.timeSlot < firstInvalidTimeSlot =>
+          logger.debug(s"Block pointer computed", "block pointer hash" -> p.at, "pointer height" -> p.blockchainHeight)
+          p
         case Block(body, _) =>
           val height: Height =
             p.blockchainHeight.below getOrElse { throw new RuntimeException("Fatal: Non genesis block with height zero")}
@@ -57,18 +71,22 @@ class Blockchain[Tx: Codec](validator: SegmentValidator, private[blockchain] val
   private def finalizedHead(now: TimeSlot): AnyBlock[Tx] = finalizedHeadPointer(now).forceGetPointedBlockFromStorage
 
   def unsafeRunAllTransactions[S](initialState: S, transactionExecutor: (S, Tx) => Option[S]): S = {
+    logger.trace("Running all transactions")
     run(headPointer, StateSnapshot(initialState, TimeSlot.zero), transactionExecutor)
   }
 
   def unsafeRunTransactionsFromPreviousStateSnapshot[S](snapshot: StateSnapshot[S], transactionExecutor: (S, Tx) => Option[S]): S = {
+    logger.trace("Running transactions from a given snapshot")
     run(headPointer, snapshot, transactionExecutor)
   }
 
   def runAllFinalizedTransactions[S](now: TimeSlot, initialState: S, transactionExecutor: (S, Tx) => Option[S]): S = {
+    logger.trace("Running all finalized transactions")
     run(finalizedHeadPointer(now), StateSnapshot(initialState, TimeSlot.zero), transactionExecutor)
   }
 
   def runFinalizedTransactionsFromPreviousStateSnapshot[S](now: TimeSlot, snapshot: StateSnapshot[S], transactionExecutor: (S, Tx) => Option[S]): S = {
+    logger.trace("Running finalized transactions from a given state snapshot")
     run(finalizedHeadPointer(now), snapshot, transactionExecutor)
   }
 
@@ -95,6 +113,7 @@ class Blockchain[Tx: Codec](validator: SegmentValidator, private[blockchain] val
 
   def add(chainSegment: ChainSegment[Tx]): Unit = {
 
+    logger.debug("Testing chain segment to be added", "segment" -> chainSegment)
     @tailrec
     def listBlocksToRemove(from: Hash, limit: Hash, accum: List[Hash]): List[Hash] =
       if (from == limit)
@@ -132,9 +151,13 @@ class Blockchain[Tx: Codec](validator: SegmentValidator, private[blockchain] val
         storage.update(blocksToRemove, blocksToAdd)
 
         val finalHeight = Height.from(s).get // `.get` is safe as s > 0.
+        logger.debug("Chain segment added to the chain")
         headPointer = BlockPointer(hash(chainSegment.mostRecentBlock.get), finalHeight)
+        logger.debug(s"New head pointer assigned", "head pointer hash" -> headPointer.at, "height" -> headPointer.blockchainHeight)
       }
     }
+
+    logger.debug("Chain segment processed", "segment" -> chainSegment)
   }
 
   def createBlockData(transactions: List[Tx], timeSlot: TimeSlot, key: SigningPrivateKey): Block[Tx] = {
@@ -145,6 +168,8 @@ class Blockchain[Tx: Codec](validator: SegmentValidator, private[blockchain] val
         timeSlot,
         sign(timeSlot, key)
       )
+
+    logger.debug(s"New block created", "block body" -> body)
 
     Block[Tx](
       body,
@@ -158,10 +183,17 @@ class Blockchain[Tx: Codec](validator: SegmentValidator, private[blockchain] val
   }
 
   private def getFromStorage(id: Hash): Option[AnyBlock[Tx]] = {
+    logger.trace("Retrieving block from storage", "block hash" -> id)
     if (genesisBlockHash == id) {
+      logger.trace(s"Returning the genesis block", "block hash" -> id)
       Some(genesisBlock)
     } else {
-      storage.get(id)
+      val response = storage.get(id)
+      response match {
+        case None => logger.trace("No block was found in storage", "block hash" -> id)
+        case Some(b) => logger.trace("Returning non genesis block", "block hash" -> id, "block" -> b)
+      }
+      response
     }
   }
 
@@ -170,7 +202,47 @@ class Blockchain[Tx: Codec](validator: SegmentValidator, private[blockchain] val
 
 object Blockchain {
 
-  def apply[Tx: Codec](keys: List[SigningPublicKey], maxNumOfAdversaries: Int, database: String, segmentValidator: SegmentValidator): Blockchain[Tx] = {
+  object LoggingFormat {
+
+    implicit val hashLoggable: Loggable[Hash] = Loggable.gen[Hash] { hash =>
+      val wholeHash = hash.toCompactString()
+      val firstSixChars = wholeHash.slice(1, 9)
+      val lasttSixChars = wholeHash.takeRight(8)
+      firstSixChars + " ... " + lasttSixChars
+    }
+
+    implicit val heightLoggable: Loggable[Height] = Loggable.gen[Height](_.toInt.toString)
+
+    implicit val timeSlotLoggable: Loggable[TimeSlot] = Loggable.gen[TimeSlot](_.toLong.toString)
+
+    implicit def blockBodyLoggable[Tx]: Loggable[BlockBody[Tx]] = Loggable.gen[BlockBody[Tx]] { bb =>
+      List(
+        s"previous hash -> ${hashLoggable.log(bb.previousHash)}",
+        s"number of transactions -> ${bb.delta.length}",
+        s"time slot -> ${timeSlotLoggable.log(bb.timeSlot)}"
+      ).mkString(", ")
+    }
+
+    implicit def blockLoggable[Tx: Loggable]: Loggable[Block[Tx]] = Loggable.gen[Block[Tx]]{ b =>
+      s"Block(${blockBodyLoggable.log(b.body)})"
+    }
+
+    implicit def chainSegmentLoggable[Tx: Loggable]: Loggable[ChainSegment[Tx]] = {
+      Loggable.gen[ChainSegment[Tx]] { cs =>
+        cs.blocks match {
+          case Nil => "< empty-segment >"
+          case List(b1) => s"< ${blockLoggable[Tx].log(b1)} >"
+          case List(b1, b2) => s"< ${blockLoggable[Tx].log(b1)}, ${blockLoggable[Tx].log(b2)} >"
+          case longerSegment =>
+            val h = longerSegment.head
+            val l = longerSegment.last
+            s"< ${blockLoggable[Tx].log(h)} ,..., ${blockLoggable[Tx].log(l)} > (segment length = ${longerSegment.length})"
+        }
+      }
+    }
+  }
+
+  def apply[Tx: Codec: Loggable](keys: List[SigningPublicKey], maxNumOfAdversaries: Int, database: String, segmentValidator: SegmentValidator): Blockchain[Tx] = {
     val storage = H2BlockStorage[Tx](database)
     new Blockchain[Tx](segmentValidator, storage)(keys, maxNumOfAdversaries)
   }
