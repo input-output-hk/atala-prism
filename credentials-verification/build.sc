fb09b5b2f69d8fba2f6906661fdfd7cd2f5d62d6
Marcin Kurczych <marcin.kurczych@iohk.io>
2019-09-13 17:08:09 +0200

    A-896 Connector API desing

--- credentials-verification/build.sc
+++ credentials-verification/build.sc
@@ -2,6 +2,10 @@ import mill._
 import mill.scalalib._
 import mill.contrib.scalapblib._
 
+import java.io.File
+import java.lang.reflect.Method
+import java.net.URLClassLoader
+
 import coursier.maven.MavenRepository
 
 object app extends ScalaModule {
@@ -114,14 +118,86 @@ object node extends `server-common` {
   object test extends `tests-common` {}
 }
 
+class FixedScalaPBWorker {
+  private var scalaPBInstanceCache = Option.empty[(Long, ScalaPBWorkerApi)]
+
+  private def scalaPB(scalaPBClasspath: Agg[os.Path]) = {
+    val classloaderSig = scalaPBClasspath.map(p => p.toString().hashCode + os.mtime(p)).sum
+    scalaPBInstanceCache match {
+      case Some((sig, instance)) if sig == classloaderSig => instance
+      case _ =>
+        val cl = new URLClassLoader(scalaPBClasspath.map(_.toIO.toURI.toURL).toArray)
+        val scalaPBCompilerClass = cl.loadClass("scalapb.ScalaPBC")
+        val mainMethod = scalaPBCompilerClass.getMethod("main", classOf[Array[java.lang.String]])
+
+        val instance = new ScalaPBWorkerApi {
+          override def compileScalaPB(source: File, scalaPBOptions: String, generatedDirectory: File) {
+            val opts = if (scalaPBOptions.isEmpty) "" else scalaPBOptions + ":"
+            mainMethod.invoke(
+              null,
+              Array(
+                "--throw",
+                s"--scala_out=${opts}${generatedDirectory.getCanonicalPath}",
+                s"--proto_path=${source.getParentFile.getCanonicalPath}",
+                source.getCanonicalPath
+              )
+            )
+          }
+        }
+        scalaPBInstanceCache = Some((classloaderSig, instance))
+        instance
+    }
+  }
+
+  def compile(scalaPBClasspath: Agg[os.Path], scalaPBSources: Seq[os.Path], scalaPBOptions: String, dest: os.Path)(
+      implicit ctx: mill.api.Ctx
+  ): mill.api.Result[PathRef] = {
+    val compiler = scalaPB(scalaPBClasspath)
+
+    def compileScalaPBDir(inputDir: os.Path) {
+      os.walk(inputDir)
+        .filter(_.last.matches(".*.proto"))
+        .foreach { proto =>
+          compiler.compileScalaPB(proto.toIO, scalaPBOptions, dest.toIO)
+        }
+    }
+
+    def compileScalaPB(input: os.Path): Unit = {
+      // ls throws if the path doesn't exist
+      if (input.toIO.exists) {
+        if (input.toIO.isDirectory) {
+          compileScalaPBDir(input)
+        } else {
+          compiler.compileScalaPB(input.toIO, scalaPBOptions, dest.toIO)
+        }
+      }
+    }
+
+    scalaPBSources.foreach(compileScalaPB)
+
+    mill.api.Result.Success(PathRef(dest))
+  }
+}
+
 object connector extends ScalaPBModule {
   def scalaVersion = "2.12.4"
   def scalaPBVersion = "0.9.0"
   def scalaPBGrpc = true
   override def mainClass = Some("io.iohk.connector.ServerApp")
 
+  override def scalaPBSources = T.sources {
+    millSourcePath / 'protobuf / "protos.proto"
+  }
+
+  override def compileScalaPB: T[PathRef] = T.persistent {
+    scalaPBSources().foreach(pathRef => println(pathRef.path))
+    new FixedScalaPBWorker()
+      .compile(scalaPBClasspath().map(_.path), scalaPBSources().map(_.path), scalaPBOptions(), T.ctx().dest)
+  }
+
   override def ivyDeps = Agg(
     ivy"io.grpc:grpc-netty:1.23.0",
+    ivy"com.thesamet.scalapb::scalapb-runtime:0.9.0;classifier=protobuf",
     ivy"com.thesamet.scalapb::scalapb-runtime-grpc:0.9.0"
   )
 }
--- credentials-verification/connector/src/io/iohk/connector/ServerApp.scala
+++ credentials-verification/connector/src/io/iohk/connector/ServerApp.scala
@@ -1,6 +1,6 @@
 package io.iohk.connector
 
-import io.grpc.{Server, ServerBuilder}
+import io.grpc.{Server, ServerBuilder, Status}
 import io.iohk.connector.protos._
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -24,7 +24,7 @@ class ServerApp(executionContext: ExecutionContext) { self =>
   private def start(): Unit = {
     server = ServerBuilder
       .forPort(ServerApp.port)
-      .addService(ConnectionsGrpc.bindService(new ConnectionsImpl, executionContext))
+      .addService(ConnectorServiceGrpc.bindService(new ConnectorServiceImpl, executionContext))
       .build()
       .start()
 
@@ -48,15 +48,124 @@ class ServerApp(executionContext: ExecutionContext) { self =>
     }
   }
 
-  private class ConnectionsImpl extends ConnectionsGrpc.Connections {
-    override def acceptConnection(request: AcceptConnectionRequest): Future[AcceptConnectionReply] = {
-      val reply = AcceptConnectionReply()
-      Future.successful(reply)
+  private class ConnectorServiceImpl extends ConnectorServiceGrpc.ConnectorService {
+    /** Get active connections for current participant
+      *
+      * Available to: Holder, Issuer, Validator
+      */
+    override def getConnectionsSince(request: GetConnectionsSinceRequest): Future[GetConnectionsSinceResponse] = {
+      Future.successful {
+        GetConnectionsSinceResponse(Nil)
+      }
     }
 
-    override def newConnectionCode(request: NewConnectionCodeRequest): Future[NewConnectionCodeReply] = {
-      val reply = NewConnectionCodeReply(code = "Hello ")
-      Future.successful(reply)
+    /** Return info about connection token such as creator info
+      *
+      * Available to: Holder
+      *
+      * Errors:
+      * Token does not exist (UNKNOWN)
+      */
+    override def getConnectionTokenInfo(request: GetConnectionTokenInfoRequest): Future[GetConnectionTokenInfoResponse] = {
+      Future.successful {
+        GetConnectionTokenInfoResponse(
+          ParticipantInfo(ParticipantInfo.Participant.Issuer(
+            IssuerInfo()
+          ))
+        )
+      }
+    }
+
+    /** Instantiate connection from connection token
+      *
+      * Available to: Holder
+      *
+      * Errors:
+      * Token does not exist (UNKNOWN)
+      */
+    override def addConnectionFromToken(request: AddConnectionFromTokenRequest): Future[AddConnectionFromTokenResponse] = {
+      Future.successful {
+        AddConnectionFromTokenResponse()
+      }
+    }
+
+    /** Delete active connection
+      *
+      * Available to: Holder, Issuer, Validator
+      *
+      * Errors:
+      * Connection does not exist (UNKNOWN)
+      */
+    override def deleteConnection(request: DeleteConnectionRequest): Future[DeleteConnectionResponse] = {
+      Future.successful {
+        DeleteConnectionResponse()
+      }
+    }
+
+    /** Bind DID to issuer
+      *
+      * Available to: Issuer
+      *
+      * Errors:
+      * Invalid DID (INVALID_ARGUMENT)
+      * Invalid DID document (INVALID_ARGUMENT)
+      * DID Document does not match DID (INVALID_ARGUMENT)
+      */
+    override def registerDID(request: RegisterDIDRequest): Future[RegisterDIDResponse] = {
+      Future.successful {
+        RegisterDIDResponse()
+      }
+    }
+
+    /** Change billing plan of participant who wants to generate connection tokens
+      *
+      * Available to: Issuer, Validator
+      *
+      * Errors:
+      * Unknown billing plan (UNKNOWN)
+      * User not allowed to set this billing plan (PERMISSION_DENIED)
+      */
+    override def changeBillingPlan(request: ChangeBillingPlanRequest): Future[ChangeBillingPlanResponse] = {
+      Future.successful {
+        ChangeBillingPlanResponse()
+      }
+    }
+
+    /** Generate connection token that can be used to instantiate connection
+      *
+      * Available to: Issuer, Validator
+      *
+      * Errors:
+      * Billing plan doesn't allow token generation (PERMISSION_DENIED)
+      */
+    override def generateConnectionToken(request: GenerateConnectionTokenRequest): Future[GenerateConnectionTokenResponse] = {
+      Future.failed {
+        Status.PERMISSION_DENIED.withDescription("Billing plan doesn't allow token generation").asException()
+      }
+    }
+
+    /** Return messages received after given time moment, sorted in ascending order by receive time
+      *
+      * Available to: Issuer, Holder, Validator
+      */
+    override def getMessagesSince(request: GetMessagesSinceRequest): Future[GetMessagesSinceResponse] = {
+      Future.successful {
+        GetMessagesSinceResponse(Nil)
+      }
+    }
+
+    /** Send message over a connection
+      *
+      * Available to: Issuer, Holder, Validator
+      *
+      * Errors:
+      * Unknown connection (UNKNOWN)
+      * Connection closed (FAILED_PRECONDITION)
+      */
+    override def sendMessage(request: SendMessageRequest): Future[SendMessageResponse] = {
+      Future.successful {
+        SendMessageResponse()
+      }
     }
   }
 }
