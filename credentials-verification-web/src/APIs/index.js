31fe8179c265328ed9c3277c0ae7c1a35967ecac
Tomas Najun <tnajun@atixlabs.com>
2019-11-28 16:43:21 -0300

    ATA-1864 - Integrated the wallet creation in the last registration step

--- credentials-verification-web/src/APIs/index.js
+++ credentials-verification-web/src/APIs/index.js
@@ -1,13 +1,14 @@
 import { mockApi } from './__mocks__';
 
 import { generateConnectionToken, getConnectionsPaginated } from './connector/connector';
-import { getDid } from './wallet/wallet';
+import { createWallet, getDid } from './wallet/wallet';
 import { getCredentials, createCredential } from './credentials/credentialsManager';
 
 export { mockApi };
 export const api = {
   generateConnectionToken,
   getConnectionsPaginated,
+  createWallet,
   getDid,
   getCredentials,
   createCredential
