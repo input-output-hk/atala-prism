ea1e75bdc730180a5f2e0f2704f029ffc4ea3595
Marcin Kurczych <marcin.kurczych@iohk.io>
2019-12-17 18:46:31 +0100

    ATA-2081 Fix graduationDate and enrollmentDate switch

--- credentials-verification/cmanager/src/io/iohk/cvp/cmanager/grpc/services/codecs/ProtoCodecs.scala
+++ credentials-verification/cmanager/src/io/iohk/cvp/cmanager/grpc/services/codecs/ProtoCodecs.scala
@@ -33,8 +33,8 @@ object ProtoCodecs {
       .withTitle(credential.title)
       .withIssuedBy(credential.issuedBy.value.toString)
       .withGroupName(credential.groupName)
-      .withEnrollmentDate(graduationDate)
-      .withGraduationDate(enrollmentDate)
+      .withEnrollmentDate(enrollmentDate)
+      .withGraduationDate(graduationDate)
   }
 
   def studentToProto(student: Student): protos.Student = {
