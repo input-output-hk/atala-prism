9d1e62c00d20a781fa1d9901b25a92dad96c08fe
Alexis Hernandez <alexis.hernandez@iohk.io>
2020-01-10 11:17:49 -0700

    ATA-2106: Add debug logs to the wallet
    These will be useful while testing on the environment for Georgia's demo.

--- credentials-verification/wallet/src/io/iohk/cvp/wallet/grpc/WalletServiceImpl.scala
+++ credentials-verification/wallet/src/io/iohk/cvp/wallet/grpc/WalletServiceImpl.scala
@@ -9,11 +9,15 @@ import io.iohk.cvp.wallet._
 import io.iohk.cvp.wallet.models.Wallet
 import io.iohk.cvp.wallet.protos.GetWalletStatusResponse.WalletStatus
 import io.iohk.cvp.wallet.protos.{ChangePassphraseResponse, _}
+import org.slf4j.LoggerFactory
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Success
 
 class WalletServiceImpl(wallet: Wallet)(implicit ec: ExecutionContext) extends protos.WalletServiceGrpc.WalletService {
+
+  private val logger = LoggerFactory.getLogger(this.getClass)
+
   private val cachedWallet: AtomicReference[Option[WalletData]] = new AtomicReference(None)
   private val walletSecurity = WalletSecurity()
   private val walletIO = WalletIO()
@@ -21,19 +25,24 @@ class WalletServiceImpl(wallet: Wallet)(implicit ec: ExecutionContext) extends p
   private val walletServiceOrchestrator = WalletServiceOrchestrator(walletSecurity, walletIO)
 
   override def getDID(request: protos.GetDIDRequest): Future[protos.GetDIDResponse] = {
+    logger.info(s"getDID - request = ${request.toProtoString}")
     val response = protos.GetDIDResponse(did = wallet.did)
+    logger.info(s"getDID - response = ${response.toProtoString}")
     Future.successful(response)
   }
 
   override def signMessage(request: SignMessageRequest): Future[SignMessageResponse] = {
+    logger.info(s"signMessage - request = ${request.toProtoString}")
     Future {
       val signature = ECSignature.sign(wallet.privateKey, request.message.toByteArray)
       val response = protos.SignMessageResponse(signature = ByteString.copyFrom(signature.toArray))
+      logger.info(s"signMessage - response = ${response.toProtoString}")
       response
     }
   }
 
   override def verifySignedMessage(request: VerifySignedMessageRequest): Future[VerifySignedMessageResponse] = {
+    logger.info(s"verifySignedMessage - request = ${request.toProtoString}")
     Future {
       val publicKey = toPublicKey(request.publicKey.getOrElse(throw new RuntimeException("Missing public key")))
       ECSignature.verify(
@@ -42,22 +51,27 @@ class WalletServiceImpl(wallet: Wallet)(implicit ec: ExecutionContext) extends p
         signature = request.signature.toByteArray.toVector
       )
       val response = protos.VerifySignedMessageResponse(verified = true)
+      logger.info(s"verifySignedMessage - response = ${response.toProtoString}")
       response
     }
   }
 
   override def createWallet(request: CreateWalletRequest): Future[CreateWalletResponse] = {
+    logger.info(s"createWallet - request = ${request.toProtoString}")
     walletServiceOrchestrator
       .createNewWallet(request.passphrase, request.role, request.organisationName, request.logo.toByteArray)
       .map { data =>
         cachedWallet.set(Some(data))
 
-        protos.CreateWalletResponse(Some(toSignedAtalaOperation(data.keyPair)))
+        val response = protos.CreateWalletResponse(Some(toSignedAtalaOperation(data.keyPair)))
+        logger.info(s"createWallet - response = ${response.toProtoString}")
+        response
       }
   }
 
   override def getWalletStatus(request: GetWalletStatusRequest): Future[GetWalletStatusResponse] = {
-    Future {
+    logger.info(s"getWalletStatus - request = ${request.toProtoString}")
+    val result = Future {
       cachedWallet.get()
     }.flatMap {
       case Some(_) => Future.successful(GetWalletStatusResponse(WalletStatus.Unlocked))
@@ -66,12 +80,17 @@ class WalletServiceImpl(wallet: Wallet)(implicit ec: ExecutionContext) extends p
           case Some(_) => GetWalletStatusResponse(WalletStatus.Locked)
           case None => GetWalletStatusResponse(WalletStatus.Missing)
         }
-
     }
 
+    result.foreach { response =>
+      logger.info(s"getWalletStatus - response = ${response.toProtoString}")
+    }
+
+    result
   }
 
   override def unlockWallet(request: UnlockWalletRequest): Future[UnlockWalletResponse] = {
+    logger.info(s"unlockWallet - request = ${request.toProtoString}")
     val walletFuture = cachedWallet.get() match {
       case Some(wallet) => Future.successful(wallet)
       case None =>
@@ -87,21 +106,32 @@ class WalletServiceImpl(wallet: Wallet)(implicit ec: ExecutionContext) extends p
     }
 
     walletFuture.map { wallet =>
-      UnlockWalletResponse()
+      val response = UnlockWalletResponse()
         .withOrganisationName(wallet.organisationName)
         .withRole(wallet.role)
         .withLogo(wallet.logo)
+
+      logger.info(s"unlockWallet - response = ${response.toProtoString}")
+      response
     }
   }
 
   override def lockWallet(request: LockWalletRequest): Future[LockWalletResponse] = {
-    Future {
+    logger.info(s"lockWallet - request = ${request.toProtoString}")
+    val result = Future {
       cachedWallet.set(None)
     }.map(_ => LockWalletResponse())
+
+    result.foreach { response =>
+      logger.info(s"lockWallet - response = ${response.toProtoString}")
+    }
+
+    result
   }
 
   override def changePassphrase(request: ChangePassphraseRequest): Future[ChangePassphraseResponse] = {
-    walletServiceOrchestrator.loadWallet(request.currentPassphrase).flatMap {
+    logger.info(s"changePassphrase - request = ${request.toProtoString}")
+    val result = walletServiceOrchestrator.loadWallet(request.currentPassphrase).flatMap {
       case Some(data) => {
         val walletData = protos.WalletData.parseFrom(data)
         walletServiceOrchestrator.save(request.newPassphrase, walletData).map { _ =>
@@ -111,17 +141,29 @@ class WalletServiceImpl(wallet: Wallet)(implicit ec: ExecutionContext) extends p
       }
       case None => throw new RuntimeException("Wallet cannot be Unlocked")
     }
+
+    result.foreach { response =>
+      logger.info(s"changePassphrase - response = ${response.toProtoString}")
+    }
+
+    result
   }
 
   override def generateDID(request: GenerateDIDRequest): Future[GenerateDIDResponse] = {
-    Future {
+    logger.info(s"generateDID - request = ${request.toProtoString}")
+    val result = Future {
       cachedWallet.get() match {
-        case Some(data) => {
+        case Some(data) =>
           GenerateDIDResponse(Some(toSignedAtalaOperation(data.keyPair)))
-        }
+
         case None => throw new RuntimeException("Wallet is locked")
       }
     }
-  }
 
+    result.foreach { response =>
+      logger.info(s"generateDID - response = ${response.toProtoString}")
+    }
+
+    result
+  }
 }
