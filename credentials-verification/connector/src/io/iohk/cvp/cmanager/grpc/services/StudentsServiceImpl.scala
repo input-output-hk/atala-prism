7d979a364d3f42818b513dabe8b5df94759b3e19
Alexis Hernandez <alexis.hernandez@iohk.io>
2020-01-27 07:19:52 -0700

    ATA-2210: Add logs when creating a student

--- credentials-verification/connector/src/io/iohk/cvp/cmanager/grpc/services/StudentsServiceImpl.scala
+++ credentials-verification/connector/src/io/iohk/cvp/cmanager/grpc/services/StudentsServiceImpl.scala
@@ -3,7 +3,7 @@ package io.iohk.cvp.cmanager.grpc.services
 import java.util.UUID
 
 import io.iohk.cvp.cmanager.grpc.services.codecs.ProtoCodecs._
-import io.iohk.cvp.cmanager.models.{IssuerGroup, Student}
+import io.iohk.cvp.cmanager.models.{Issuer, IssuerGroup, Student}
 import io.iohk.cvp.cmanager.models.requests.CreateStudent
 import io.iohk.cvp.cmanager.protos
 import io.iohk.cvp.cmanager.protos.{
@@ -18,31 +18,56 @@ import io.iohk.cvp.cmanager.protos.{
 }
 import io.iohk.cvp.cmanager.repositories.{CredentialsRepository, StudentsRepository}
 import io.scalaland.chimney.dsl._
+import org.slf4j.LoggerFactory
 
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 
 class StudentsServiceImpl(studentsRepository: StudentsRepository, credentialsRepository: CredentialsRepository)(
     implicit ec: ExecutionContext
 ) extends protos.StudentsServiceGrpc.StudentsService {
 
-  override def createStudent(request: protos.CreateStudentRequest): Future[protos.CreateStudentResponse] = {
-    val issuerId = getIssuerId()
-    val model = request
-      .into[CreateStudent]
-      .withFieldConst(_.issuer, issuerId)
-      .enableUnsafeOption
-      .transform
+  private val logger = LoggerFactory.getLogger(this.getClass)
 
-    studentsRepository
-      .create(model)
-      .map(studentToProto)
-      .map(protos.CreateStudentResponse().withStudent)
-      .value
-      .map {
-        case Right(x) => x
-        case Left(e) => throw new RuntimeException(s"FAILED: $e")
-      }
+  override def createStudent(request: protos.CreateStudentRequest): Future[protos.CreateStudentResponse] = {
+    def f(issuerId: Issuer.Id) = {
+      val model = request
+        .into[CreateStudent]
+        .withFieldConst(_.issuer, issuerId)
+        .enableUnsafeOption
+        .transform
+
+      studentsRepository
+        .create(model)
+        .map(studentToProto)
+        .map(protos.CreateStudentResponse().withStudent)
+        .value
+        .map {
+          case Right(x) => x
+          case Left(e) => throw new RuntimeException(s"FAILED: $e")
+        }
+    }
+
+    Try {
+      getIssuerId()
+    } match {
+      case Failure(ex) =>
+        logger.info(s"createStudent - missing userId, request = ${request.toProtoString}", ex)
+        Future.failed(throw new RuntimeException("Missing userId"))
+
+      case Success(issuerId) =>
+        val result = f(issuerId)
+        result.onComplete {
+          case Success(response) =>
+            logger.info(
+              s"createStudent, userId = $issuerId, request = ${request.toProtoString}, response = ${response.toProtoString}"
+            )
+
+          case Failure(ex) =>
+            logger.info(s"createStudent failed, userId = $issuerId, request = ${request.toProtoString}", ex)
+        }
+        result
+    }
   }
 
   override def getStudents(request: GetStudentsRequest): Future[GetStudentsResponse] = {
