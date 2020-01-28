09b1e8da3000609d4e1f8eb21bf2f745d8efc934
Gonzalo Hernandorena <ghernandorena@atixlabs.com>
2020-01-28 09:55:27 -0300

    Deleted hardcoded true at logged validation

--- credentials-verification-web/src/components/providers/withLoggedValidation.jsx
+++ credentials-verification-web/src/components/providers/withLoggedValidation.jsx
@@ -51,7 +51,7 @@ const withLoggedValidationComponent = (Component, validRoles) => props => {
   // If is either public and no user is logger or meets the requeriments, the
   // wanted component is returned.
   // if (canRender()) return <Component {...props} />;
-  if (true || canRender()) return <Component {...props} />;
+  if (canRender()) return <Component {...props} />;
 
   // If the previous validation turns out to fail, because it is not public and
   // the wallet is locked or the role is not one with access to this route, the
