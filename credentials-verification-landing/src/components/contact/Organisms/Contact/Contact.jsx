5dd896fef8f999e9333674fc0e759110696e678b
Nicolas Stupfler <58951950+Estufa-Atix@users.noreply.github.com>
2020-05-23 02:36:41 -0300

    ATA-2591 integrate mailchimp suscription (#840)
    Co-authored-by: Alexis Hernandez <alexis.hernandez@iohk.io>

--- credentials-verification-landing/src/components/contact/Organisms/Contact/Contact.jsx
+++ credentials-verification-landing/src/components/contact/Organisms/Contact/Contact.jsx
@@ -1,11 +1,16 @@
 import React, { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
+import jsonp from 'jsonp';
+import queryString from 'query-string';
 import { message } from 'antd';
 import { useTranslation } from 'react-i18next';
 import { withApi } from '../../../providers/withApi';
 import ContactForm from '../ContactInformation/ContactInformation';
+import { config } from '../../../../APIs/configs';
 
-const Contact = ({ api }) => {
+const { mailchimpURL, mailchimpU, mailchimpID } = config;
+
+const Contact = () => {
   const { t } = useTranslation();
 
   const contactInfoRef = useRef();
@@ -20,10 +25,23 @@ const Contact = ({ api }) => {
           if (!consent) return message.error(t('errors.consent'));
           if (errors) return;
 
-          // TODO integrate with Backend Service when exist
           console.log('fields=', fullName, email, formMessage, consent);
+          subscribeToNewsLetter({ EMAIL: email, FULLNAME: fullName, MESSAGE: formMessage });
         }
       );
+  };
+
+  const subscribeToNewsLetter = formData => {
+    jsonp(
+      `${mailchimpURL}/subscribe/post-json?u=${mailchimpU}&amp;id=${mailchimpID}&${queryString.stringify(
+        formData
+      )}`,
+      { param: 'c' },
+      (err, data) => {
+        console.log('err:', err);
+        console.log('data:', data);
+      }
+    );
   };
 
   return (
@@ -35,12 +53,4 @@ const Contact = ({ api }) => {
   );
 };
 
-Contact.propTypes = {
-  api: PropTypes.shape({
-    getConnectionToken: PropTypes.func,
-    startSubjectStatusStream: PropTypes.func,
-    SetPersonalData: PropTypes.func
-  }).isRequired
-};
-
-export default withApi(Contact);
+export default Contact;
