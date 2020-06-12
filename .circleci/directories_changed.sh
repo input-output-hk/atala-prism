1a1ecc8638c7d47bb4b7df63a7dd0c9d0d1985a2
Moises Osorio <moises.osorio@iohk.io>
2020-06-12 16:42:00 -0600

    ATA-2683: Skip CircleCI jobs when components are not affected. (#895)
    * ATA-2683: Skip CircleCI jobs when components are not affected.
    
    For simplicity, a bash script is added to detect whether a branch
    deploys an image, because when it does, all tests should run to
    guarantee the image quality.
    
    For other branches, it detects all the changes since the last develop
    fork (need to check whether *not* rebasing works properly) and ignores
    some files (e.g., documentation).
    
    When one of the directory parameters have been detected as changed, the
    script returns a non-zero exit code. A zero return code means no
    directory changed and testing can be skipped.
    
    Note that changes outside a component directory won't be detected, but I
    think this is OK? LMK otherwise.
    
    This is all encapsulated in a new CircleCI command, which is called from
    jobs that can be skipped. Because the job does run, it should become
    "green" on GitHub when it's skipped.
    
    I put the CV component as a dependency to all other components, but LMK
    if you'd want me to streamline this.
    
    Once this is merged, I will try to watch closely all runs in CircleCI to
    detect any issues.
    
    For the record, there are ways to completely skip the jobs, but we would
    need to heavily modify the workflow and write a script to launch the
    workflow with dynamic params depending on the affected directories. I
    think this PR is way simpler and easier to understand and may get us
    where we want to be.
    
    It should be important to note that all testing will still be done to
    PRs when merged to develop, and that authors should receive an email if
    their PR failed to pass tests in develop, so as long develop is green,
    this work is doing what it has to do :)
    
    * Remove dep of android with backend.
    
    Changing the backend doesn't really affect the Android side, not today,
    maybe tomorrow once protos are used directly, perhaps.
    
    * Tighten script style.
    
    * Fix the strict issues caused in a previous commit.
    
    Had to disable pipefail checks for `head` as it's expected :?

---
+++ .circleci/directories_changed.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# This script detects whether at least one of the directories given as arguments have changed and exits with a non-zero
+# code in that case, or zero if nothing has changed. If the current branch should always be tested, this script will
+# always exit with a non-zero code, independently of directory changes.
+# This script is used by CircleCI (.circle/config.yml), in order to detect whether a component or its dependencies have
+# changed and test it.
+# $CIRCLE_* variables should always be set in CircleCI, the below checks for emptiness are to aid local testing, but
+# running `CIRCLE_BRANCH=develop ./.circleci/directories_changed.sh`, and alike, can be used as well.
+
+# Precautionary measure: if the script fails, testing should be performed.
+set -euo pipefail
+
+echo "Detecting if any of directories '$*' changed"
+
+branch=${CIRCLE_BRANCH:-}
+if [ -z "$branch" ] ; then
+  # Use head branch
+  branch=$(git rev-parse --abbrev-ref HEAD)
+fi
+
+echo "Current branch is '$branch'"
+
+# Branches that deploy an image should be fully tested
+ALWAYS_TEST_BRANCH_REGEX="^(develop|test|geud-test)"
+if [[ $branch =~ $ALWAYS_TEST_BRANCH_REGEX ]] ; then
+    echo "Branch '$branch' should always be tested"
+    exit 1
+fi
+
+current_commit=${CIRCLE_SHA1:-}
+if [ -z "$current_commit" ] ; then
+  # Use head commit
+  current_commit=$(git rev-parse HEAD)
+fi
+
+echo "Current commit is $current_commit"
+# Get the last commit shared by both develop and the current branch
+set +o pipefail # Disable pipefail check as head causes it but it's OK
+base_commit=$(git rev-list origin/develop | grep "$(git rev-list HEAD)" | head -1)
+set -o pipefail # Re-enable the pipefail check
+
+echo "Base commit is $base_commit"
+
+# Detect changed files, ignoring doc files (.md and everything inside a docs directory) and other files not affecting
+# builds or tests
+changed_files=$(
+  git diff "$base_commit".."$current_commit" --name-only | \
+      grep -v "\.md$" | \
+      grep -v "/docs/" | \
+      grep -v "\.gitignore$")
+for dir in "$@" ; do
+  # Matches a line starting with the directory name, followed by a slash
+  dir_regex="[^\n]$dir/"
+  if [[ $changed_files =~ $dir_regex ]] ; then
+    echo "Directory '$dir' has changed"
+    exit 1
+  fi
+done
+
+echo "None of directories '$*' has changed"
