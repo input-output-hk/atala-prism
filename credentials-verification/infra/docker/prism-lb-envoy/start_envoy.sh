4f5a1cab9370f0f1304e02794a9a49104ff9030e
Jeremy Townson <jtownson@users.noreply.github.com>
2020-06-30 12:28:03 +0200

    Update the grpc hostname to grpc-<env>.atalaprism.io. (#958)

--- credentials-verification/infra/docker/prism-lb-envoy/start_envoy.sh
+++ credentials-verification/infra/docker/prism-lb-envoy/start_envoy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-envsubst < /root/envoy.yaml > /etc/envoy/envoy.yaml && /usr/local/bin/envoy -c /etc/envoy/envoy.yaml --log-path /dev/stdout --log-level trace
+envsubst < /root/envoy.yaml > /etc/envoy/envoy.yaml && /usr/local/bin/envoy -c /etc/envoy/envoy.yaml --log-path /dev/stdout
