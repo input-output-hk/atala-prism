7aae3d436b0d6f419ee8bd6f889e731706287ff5
Miguel Machado <miguel.machado@iohk.io>
2020-09-10 17:50:07 -0600

    ATA-3058 Android Fix - verifiers names are empty in contacts list (#1139)

--- credentials-verification-android/app/src/main/java/io/iohk/cvp/data/local/db/mappers/ContactMapper.java
+++ credentials-verification-android/app/src/main/java/io/iohk/cvp/data/local/db/mappers/ContactMapper.java
@@ -8,22 +8,33 @@ import io.iohk.prism.protos.AddConnectionFromTokenResponse;
 import io.iohk.prism.protos.ConnectionInfo;
 import io.iohk.prism.protos.HolderInfo;
 import io.iohk.prism.protos.IssuerInfo;
+import io.iohk.prism.protos.VerifierInfo;
 
 public class ContactMapper {
 
-     static public Contact mapToContact(ConnectionInfo connectionInfo, String keyDerivationPath) {
+    static public Contact mapToContact(ConnectionInfo connectionInfo, String keyDerivationPath) {
 
         Contact contact = new Contact();
         IssuerInfo issuerInfo = connectionInfo.getParticipantInfo().getIssuer();
         HolderInfo holderInfo = connectionInfo.getParticipantInfo().getHolder();
+        VerifierInfo verifierInfo = connectionInfo.getParticipantInfo().getVerifier();
 
         contact.connectionId = connectionInfo.getConnectionId();
         contact.dateCreated = System.currentTimeMillis();
-        contact.did = issuerInfo.getDID() != null ? issuerInfo.getDID() : holderInfo.getDID();
-        contact.name = issuerInfo.getName() != null ? issuerInfo.getName() : holderInfo.getName();
+        if (!issuerInfo.getDID().isEmpty()) {
+            contact.did = issuerInfo.getDID();
+            contact.name = issuerInfo.getName();
+            contact.logo = issuerInfo.getLogo().toByteArray();
+        } else if (!verifierInfo.getDID().isEmpty()) {
+            contact.did = verifierInfo.getDID();
+            contact.name = verifierInfo.getName();
+            contact.logo = verifierInfo.getLogo().toByteArray();
+        } else if (!holderInfo.getDID().isEmpty()) {
+            contact.did = holderInfo.getDID();
+            contact.name = holderInfo.getName();
+        }
         contact.token = connectionInfo.getToken();
         contact.keyDerivationPath = keyDerivationPath;
-        contact.logo = issuerInfo.getLogo().toByteArray();
         return contact;
     }
 
