c37e5e0af474495569b481936afd4475593ddcf6
Miguel Machado <miguel.machado@iohk.io>
2020-09-22 11:36:32 -0600

    ATA-3206 Android - Cast all inputs texts to lowercase in the Phrase Verification view (#1163)

--- credentials-verification-android/app/src/main/java/io/iohk/cvp/neo/ui/onboarding/phraseverification/PhraseVerificationFragment.kt
+++ credentials-verification-android/app/src/main/java/io/iohk/cvp/neo/ui/onboarding/phraseverification/PhraseVerificationFragment.kt
@@ -13,6 +13,7 @@ import com.google.firebase.analytics.FirebaseAnalytics
 import io.iohk.cvp.R
 import io.iohk.cvp.databinding.NeoFragmentPhraseVerificationBinding
 import io.iohk.cvp.neo.common.EventWrapperObserver
+import io.iohk.cvp.neo.common.lowerCaseInputFilter
 import io.iohk.cvp.utils.FirebaseAnalyticsEvents
 
 class PhraseVerificationFragment : Fragment() {
@@ -34,6 +35,7 @@ class PhraseVerificationFragment : Fragment() {
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
         setObservers()
+        configureInputsViews()
         return binding.root
     }
 
@@ -49,5 +51,11 @@ class PhraseVerificationFragment : Fragment() {
         bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, FirebaseAnalyticsEvents.VERIFY_RECOVERY_PHRASE_SUCCESS)
         firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
         findNavController().navigate(R.id.action_phraseVerificationFragment_to_accountCreatedFragment)
+    }
+
+    private fun configureInputsViews() {
+        // cast all text to lowercase
+        binding.editText1.filters = arrayOf(lowerCaseInputFilter)
+        binding.editText2.filters = arrayOf(lowerCaseInputFilter)
     }
 }
