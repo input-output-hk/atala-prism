04deea3479b204d098ff089bb0647d9948c5dae6
Ezequiel Postan <ezequiel.postan@iohk.io>
2020-10-07 19:11:46 -0300

    Add some utility methods (#1232)
    Before implementing the node resolution for long form DIDs, we found the
    need for some utility methods to handle DIDs. This commit adds the
    methods we believe cover the needs we have

--- prism-sdk/identity/shared/src/main/scala/io/iohk/atala/identity/DID.scala
+++ prism-sdk/identity/shared/src/main/scala/io/iohk/atala/identity/DID.scala
@@ -6,7 +6,65 @@ import com.google.protobuf.ByteString
 import io.iohk.atala.crypto.{ECConfig, ECPublicKey, SHA256Digest}
 import io.iohk.prism.protos.node_models
 
+import scala.util.matching.Regex
+
 object DID {
+
+  case class ValidatedLongForm(stateHash: String, encodedState: String, initialState: node_models.AtalaOperation)
+
+  sealed trait DIDFormat
+  object DIDFormat {
+    val longFormRegex: Regex = "^did:prism:[0-9a-f]{64}:[A-Za-z0-9_-]+$".r
+    val shortFormRegex: Regex = "^did:prism:[0-9a-f]{64}$".r
+
+    case class Canonical(suffix: String) extends DIDFormat
+    case class LongForm(stateHash: String, encodedState: String) extends DIDFormat {
+      def validate: Option[ValidatedLongForm] = {
+        val atalaOperationBytes = Base64.getUrlDecoder.decode(encodedState)
+        if (stateHash == SHA256Digest.compute(atalaOperationBytes).hexValue) {
+          node_models.AtalaOperation
+            .validate(atalaOperationBytes)
+            .toOption
+            .map(ValidatedLongForm(stateHash, encodedState, _))
+        } else {
+          None
+        }
+      }
+
+      def getInitialState: Option[node_models.AtalaOperation] = validate.map(_.initialState)
+    }
+    case object Unknown extends DIDFormat
+  }
+
+  def isLongForm(did: String): Boolean = {
+    DIDFormat.longFormRegex.findFirstMatchIn(did).nonEmpty
+  }
+
+  def isCanonicalForm(did: String): Boolean = {
+    DIDFormat.shortFormRegex.findFirstMatchIn(did).nonEmpty
+  }
+
+  def getFormat(did: String): DIDFormat = {
+    if (isLongForm(did)) {
+      DIDFormat.LongForm(
+        did.stripPrefix("did:prism:").takeWhile(_ != ':'),
+        did.stripPrefix("did:prism:").dropWhile(_ != ':').tail
+      )
+    } else if (isCanonicalForm(did)) {
+      DIDFormat.Canonical(did.stripPrefix("did:prism:").takeWhile(_ != ':'))
+    } else {
+      DIDFormat.Unknown
+    }
+  }
+
+  def getCanonicalSuffix(did: String): Option[String] = {
+    getFormat(did) match {
+      case DIDFormat.Canonical(suffix) => Some(suffix)
+      case DIDFormat.LongForm(stateHash, _) => Some(stateHash)
+      case DIDFormat.Unknown => None
+    }
+  }
+
   def createUnpublishedDID(masterKey: ECPublicKey): String = {
     val createDidOp = node_models.CreateDIDOperation(
       didData = Some(
@@ -27,12 +85,12 @@ object DID {
     val atalaOp = node_models.AtalaOperation(operation = node_models.AtalaOperation.Operation.CreateDid(createDidOp))
     val operationBytes = atalaOp.toByteArray
     val operationHash = SHA256Digest.compute(operationBytes)
-    val didSuffix = operationHash.hexValue
+    val didCanonicalSuffix = operationHash.hexValue
     val encodedOperation = Base64.getUrlEncoder.encodeToString(operationBytes)
-    s"did:prism:$didSuffix:$encodedOperation"
+    s"did:prism:$didCanonicalSuffix:$encodedOperation"
   }
 
-  private def publicKeyToProto(key: ECPublicKey): node_models.ECKeyData = {
+  private[identity] def publicKeyToProto(key: ECPublicKey): node_models.ECKeyData = {
     val point = key.getCurvePoint
     node_models.ECKeyData(
       curve = ECConfig.CURVE_NAME,
--- prism-sdk/identity/shared/src/test/scala/io/iohk/atala/identity/DIDSpecBase.scala
+++ prism-sdk/identity/shared/src/test/scala/io/iohk/atala/identity/DIDSpecBase.scala
@@ -1,6 +1,9 @@
 package io.iohk.atala.identity
 
 import io.iohk.atala.crypto.ECTrait
+import io.iohk.atala.identity.DID.{DIDFormat, getFormat, publicKeyToProto}
+import io.iohk.prism.protos.node_models
+import org.scalatest.OptionValues.convertOptionToValuable
 import org.scalatest.matchers.must.Matchers.convertToAnyMustWrapper
 import org.scalatest.wordspec.AnyWordSpec
 
@@ -20,6 +23,91 @@ abstract class DIDSpecBase(val ec: ECTrait) extends AnyWordSpec {
         "did:prism:0f753f41e0f3488ba56bd581d153ae9b3c9040cbcc7a63245b4644a265eb3b77:CmEKXxJdCgdtYXN0ZXIwEAFCUAoJc2VjcDI1NmsxEiAel_7KEiez4s_e0u8DyJwLkUnVmUHBuWU-0h01nerSNRohAJlR51Vbk49vagehAwQkFvW_fvyM1qa4ileIEYkXs4pF"
 
       DID.createUnpublishedDID(masterKey) mustBe expectedDID
+
+      DID.getFormat(expectedDID) match {
+        case longForm @ DIDFormat.LongForm(_, _) => longForm.validate.isEmpty mustBe false
+        case _ => fail("unexpected format for long DID")
+      }
+    }
+
+    val canonicalSuffix = "0f753f41e0f3488ba56bd581d153ae9b3c9040cbcc7a63245b4644a265eb3b77"
+    val encodedStateUsed =
+      "CmEKXxJdCgdtYXN0ZXIwEAFCUAoJc2VjcDI1NmsxEiAel_7KEiez4s_e0u8DyJwLkUnVmUHBuWU-0h01nerSNRohAJlR51Vbk49vagehAwQkFvW_fvyM1qa4ileIEYkXs4pF"
+
+    val short = s"did:prism:$canonicalSuffix"
+    val long = s"did:prism:$canonicalSuffix:$encodedStateUsed"
+    val wrong = "did:prism:wrong"
+
+    "get the correct canonical suffix" in {
+      DID.getCanonicalSuffix(short).value mustBe canonicalSuffix
+      DID.getCanonicalSuffix(long).value mustBe canonicalSuffix
+      DID.getCanonicalSuffix(wrong) mustBe None
+    }
+
+    "tell if the DID is in canonical form" in {
+      DID.isCanonicalForm(short) mustBe true
+      DID.isCanonicalForm(long) mustBe false
+      DID.isCanonicalForm(wrong) mustBe false
+    }
+
+    "tell if the DID is in long form" in {
+      DID.isLongForm(short) mustBe false
+      DID.isLongForm(long) mustBe true
+      DID.isLongForm(wrong) mustBe false
+    }
+
+    "get the correct format" in {
+      DID.getFormat(short) match {
+        case DID.DIDFormat.Canonical(suffix) =>
+          suffix mustBe canonicalSuffix
+        case _ => fail("unexpected format for canonical DID")
+      }
+      DID.getFormat(long) match {
+        case DID.DIDFormat.LongForm(stateHash, encodedState) =>
+          stateHash mustBe canonicalSuffix
+          encodedState mustBe encodedStateUsed
+        case _ => fail("unexpected format for long DID")
+      }
+      DID.getFormat(wrong) match {
+        case DID.DIDFormat.Unknown => // do nothing, the test would fail on the other cases
+        case _ => fail("unexpected format for unknown DID")
+      }
+    }
+
+    "properly validate a long form DID" in {
+      // bytes extracted from a randomly generated key
+      val xBytes = Array[Byte](30, -105, -2, -54, 18, 39, -77, -30, -49, -34, -46, -17, 3, -56, -100, 11, -111, 73, -43,
+        -103, 65, -63, -71, 101, 62, -46, 29, 53, -99, -22, -46, 53)
+      val yBytes = Array[Byte](0, -103, 81, -25, 85, 91, -109, -113, 111, 106, 7, -95, 3, 4, 36, 22, -11, -65, 126, -4,
+        -116, -42, -90, -72, -118, 87, -120, 17, -119, 23, -77, -118, 69)
+      val masterKey = ec.toPublicKey(xBytes, yBytes)
+
+      val expectedInitialState =
+        node_models.AtalaOperation(
+          operation = node_models.AtalaOperation.Operation.CreateDid(
+            node_models.CreateDIDOperation(
+              didData = Some(
+                node_models.DIDData(
+                  publicKeys = Seq(
+                    node_models.PublicKey(
+                      id = s"master0",
+                      usage = node_models.KeyUsage.MASTER_KEY,
+                      keyData = node_models.PublicKey.KeyData.EcKeyData(
+                        publicKeyToProto(masterKey)
+                      )
+                    )
+                  )
+                )
+              )
+            )
+          )
+        )
+
+      getFormat(long) match {
+        case validated @ DIDFormat.LongForm(_, _) =>
+          validated.validate.value.initialState mustBe expectedInitialState
+        case _ => fail("Long form DID with unexpected format")
+      }
     }
   }
 }
