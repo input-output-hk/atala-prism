2f9f60b2f53589cee8b2bee8c6377b02ea931a48
zejaco <jacek.kurkowski@iohk.io>
2020-11-23 14:43:07 +0100

    Enable e2e tests (#1460)

--- trigger_tests.py
+++ trigger_tests.py
@@ -81,8 +81,7 @@ def check_jobs_results(results):
     if all(result == "success" for result in results):
         print("All jobs ended up successfully!")
     else:
-        # TODO(ATA-3042): Re-enable failing here, to properly denote the workflow has failed.
-        print("ERROR: A least one of the jobs did not return in success! See the logs above.")
+        raise ValueError("ERROR: A least one of the jobs did not return in success! See the logs above.")
 
 
 token = get_env_variable("CIRCLE_CI_TOKEN")
