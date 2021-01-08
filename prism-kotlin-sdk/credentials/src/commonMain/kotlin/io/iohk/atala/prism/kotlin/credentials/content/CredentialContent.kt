c1ad0f54a252ba43b7e0642ea9825e777a9cd6ec
Mariusz Ołownia <mariusz.olownia@scalac.io>
2021-01-08 10:45:28 +0100

    ATA-4130: Kotlin SDK: Credential content (#1620)

--- prism-kotlin-sdk/credentials/src/commonMain/kotlin/io/iohk/atala/prism/kotlin/credentials/content/CredentialContent.kt
+++ prism-kotlin-sdk/credentials/src/commonMain/kotlin/io/iohk/atala/prism/kotlin/credentials/content/CredentialContent.kt
@@ -1,3 +1,57 @@
 package io.iohk.atala.prism.kotlin.credentials.content
 
-class CredentialContent
+import kotlinx.serialization.json.*
+
+/**
+ * Credential content JSON representation,
+ * this class allows to access nested fields
+ * with 'dot' notation like: credentialSubject.fieldName
+ *
+ * Example:
+ * {{{
+ *   import kotlinx.serialization.json.*
+ *   val credentialContent = buildCredentialContent {
+ *     put("issuerDid", "did")
+ *     putJsonObject("credentialSubject") {
+ *       put("fieldName", 123)
+ *     }
+ *   }
+ *   credentialContent.getString("issuerDid") == "did"
+ *   credentialContent.getInt("credentialSubject.fieldName") == 123
+ * }}}
+ */
+data class CredentialContent(val fields: JsonObject) {
+
+    fun getString(field: String): String? =
+        getField(field)?.jsonPrimitive?.contentOrNull
+
+    fun getInt(field: String): Int? =
+        getField(field)?.jsonPrimitive?.int
+
+    fun getBoolean(field: String): Boolean? =
+        getField(field)?.jsonPrimitive?.boolean
+
+    fun getArray(field: String): JsonArray? =
+        getField(field)?.jsonArray
+
+    /**
+     * Get nested json primitives with 'dot' notation like: credentialSubject.fieldName
+     */
+    fun getField(fieldName: String): JsonElement? {
+        val path = fieldName.split('.')
+        return path.drop(1).fold(fields[path[0]]) { field, key ->
+            field?.jsonObject?.get(key)
+        }
+    }
+
+    companion object {
+        fun fromString(value: String): CredentialContent? =
+            CredentialContent(Json.parseToJsonElement(value)?.jsonObject)
+    }
+}
+
+/**
+ * Create a new CredentialContent instance using buildJsonObject syntax.
+ */
+inline fun buildCredentialContent(builderAction: JsonObjectBuilder.() -> Unit) =
+    CredentialContent(buildJsonObject(builderAction))
--- prism-kotlin-sdk/credentials/src/commonTest/kotlin/io/iohk/atala/prism/kotlin/credentials/content/CredentialContentTest.kt
+++ prism-kotlin-sdk/credentials/src/commonTest/kotlin/io/iohk/atala/prism/kotlin/credentials/content/CredentialContentTest.kt
@@ -1,11 +1,105 @@
 package io.iohk.atala.prism.kotlin.credentials.content
 
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import kotlinx.serialization.json.*
+import kotlin.test.*
 
 class CredentialContentTest {
+    private val credential = CredentialContent(
+        buildJsonObject {
+            putJsonArray("credentialType") {
+                add(1)
+                add(2)
+                add(3)
+                add("test")
+            }
+            put("issuerDid", "did")
+            put("issuanceKeyId", 123)
+            put("issuance", false)
+            putJsonObject("credentialSubject") {
+                put("fieldName", true)
+            }
+        }
+    )
+
+    private val credentialString =
+        """{"credentialType":[1,2,3,"test"],"issuerDid":"did","issuanceKeyId":123,"issuance":false,"credentialSubject":{"fieldName":true}}"""
+
     @Test
-    fun exampleTest() {
-        assertEquals(true, true)
+    fun testEncoding() {
+        assertEquals(credentialString, credential.fields.toString())
+    }
+
+    @Test
+    fun testDecoding() {
+        assertEquals(credential, CredentialContent.fromString(credentialString))
+    }
+
+    @Test
+    fun testAccessToString() {
+        assertEquals("did", credential.getString("issuerDid"))
+    }
+
+    @Test
+    fun testAccessToInt() {
+        assertEquals(123, credential.getInt("issuanceKeyId"))
+    }
+
+    @Test
+    fun testAccessToBoolean() {
+        assertEquals(false, credential.getBoolean("issuance"))
+    }
+
+    @Test
+    fun testAccessToArray() {
+        val array = buildJsonArray {
+            add(1)
+            add(2)
+            add(3)
+            add("test")
+        }
+        assertEquals(array, credential.getArray("credentialType"))
+    }
+
+    @Test
+    fun testAccessToNonexistentValue() {
+        assertNull(credential.getString("nonexistent"))
+    }
+
+    @Test
+    fun testAccessToEmptyField() {
+        assertNull(credential.getString(""))
+    }
+
+    @Test
+    fun testAccessToNestedPrimitive() {
+        val nestedCredential = CredentialContent(
+            buildJsonObject {
+                putJsonObject("credentialSubject") {
+                    put("fieldName", true)
+                    putJsonObject("nested") {
+                        put("key1", "value")
+                        put("key2", 123)
+                    }
+                }
+            }
+        )
+        assertEquals(true, nestedCredential.getBoolean("credentialSubject.fieldName"))
+        assertEquals("value", nestedCredential.getString("credentialSubject.nested.key1"))
+        assertEquals(123, nestedCredential.getInt("credentialSubject.nested.key2"))
+    }
+
+    @Test
+    fun testAccessToNonexistentNestedValue() {
+        assertNull(credential.getString("credentialSubject.nonexistent"))
+        assertNull(credential.getString("nonexistent.nonexistent2.nonexistent3"))
+    }
+
+    @Test
+    fun testBuilderSyntax() {
+        val credentialContent = buildCredentialContent {
+            put("key", "value")
+        }
+
+        assertEquals("value", credentialContent.getString("key"))
     }
 }
