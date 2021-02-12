eb77ffbd7d804fbef2ccb6b7035cd1d2d9962dbb
Ana Clara Alleva <52926991+ana-alleva@users.noreply.github.com>
2021-02-12 16:46:13 -0300

    resolve pr comments (#1738)

--- prism-management-console-web/src/components/landing/Landing.jsx
+++ prism-management-console-web/src/components/landing/Landing.jsx
@@ -27,19 +27,24 @@ const Landing = () => {
   return (
     <div className="LandingContainer">
       <div className="LandingCard">
-        <PreloginLogo className="imgLogo" alt={t('landing.logoAlt')} width={200} height={120} />
-        <PreloginImg className="loginImg" alt={t('landing.logoAlt')} width={185} height={120} />
-        <div className="WelcomeText">
-          <h3>{t('landing.welcome')}</h3>
-          <h2>{t('landing.description')}</h2>
+        <div className="imgLogo">
+          <PreloginLogo className="imgLogo" alt={t('landing.logoAlt')} width={200} height={120} />
         </div>
-        <div className="LandingOptions">
-          <CustomButton
-            buttonProps={{ className: 'theme-secondary', onClick: handleLogin }}
-            buttonText={t('landing.login')}
-            loading={loading}
-          />
+        <div>
+          <PreloginImg className="loginImg" alt={t('landing.logoAlt')} width={185} height={120} />
+          <div className="WelcomeText">
+            <h3>{t('landing.welcome')}</h3>
+            <h2>{t('landing.description')}</h2>
+          </div>
+          <div className="LandingOptions">
+            <CustomButton
+              buttonProps={{ className: 'theme-secondary', onClick: handleLogin }}
+              buttonText={t('landing.login')}
+              loading={loading}
+            />
+          </div>
         </div>
+
         <div className="browserWalletOption">
           <p>
             {t('landing.getWallet')}
