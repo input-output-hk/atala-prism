16ffb4822084083d636cb0d6d997176fec081917
Marcin Kurczych <marcin.kurczych@iohk.io>
2021-03-01 16:50:32 +0100

    ATA-4375 Replace script obtaining wallet variables

--- .circleci/generate_cardano_variables.sh
+++ .circleci/generate_cardano_variables.sh
@@ -2,18 +2,15 @@
 
 set -euo pipefail
 
-current_dir=$(pwd)
-
-pushd $(dirname $0)/../prism-backend/infra/stage/base/cardano/ > /dev/null
 temp_file=$(mktemp)
-./cardano.sh -s > $temp_file
+aws s3 cp s3://atala-cvp/infra/stage/cardano/prism-test/terraform.tfstate $temp_file &> /dev/stderr
 
-echo "export NODE_CARDANO_DB_SYNC_HOST=\"$(cat $temp_file | sed -n -e 's/psql_host = "\(.*\)"/\1/p')\""
-echo "export NODE_CARDANO_DB_SYNC_DATABASE=\"$(cat $temp_file | sed -n -e 's/psql_database = "\(.*\)"/\1/p')\""
-echo "export NODE_CARDANO_DB_SYNC_USERNAME=\"$(cat $temp_file | sed -n -e 's/psql_username = "\(.*\)"/\1/p')\""
-echo "export NODE_CARDANO_DB_SYNC_PASSWORD=\"$(cat $temp_file | sed -n -e 's/psql_password = "\(.*\)"/\1/p')\""
+echo "export NODE_CARDANO_DB_SYNC_HOST=\"$(cat $temp_file | jq -r '.outputs.psql_host.value')\""
+echo "export NODE_CARDANO_DB_SYNC_DATABASE=\"$(cat $temp_file | jq -r '.outputs.psql_database.value')\""
+echo "export NODE_CARDANO_DB_SYNC_USERNAME=\"$(cat $temp_file | jq -r '.outputs.psql_username.value')\""
+echo "export NODE_CARDANO_DB_SYNC_PASSWORD=\"$(cat $temp_file | jq -r '.outputs.psql_password.value')\""
 
-echo "export NODE_CARDANO_WALLET_API_HOST=\"$(cat $temp_file | sed -n -e 's/wallet_host = "\(.*\)"/\1/p')\""
-echo "export NODE_CARDANO_WALLET_API_PORT=\"$(cat $temp_file | sed -n -e 's/wallet_port = \([[:digit:]]*\).*$/\1/p')\""
+echo "export NODE_CARDANO_WALLET_API_HOST=\"$(cat $temp_file | jq -r '.outputs.wallet_host.value')\""
+echo "export NODE_CARDANO_WALLET_API_PORT=\"$(cat $temp_file | jq -r '.outputs.wallet_port.value')\""
 
-popd > /dev/null
+rm $temp_file
