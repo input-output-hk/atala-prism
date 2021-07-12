97f9d720ac3b539c0cd2eda81a4afdba37a07fd6
dbrosio3 <dbrosio@atixlabs.com>
2021-07-12 15:15:35 -0300

    ATA-4982: splitting files with multiple components

--- prism-management-console-web/src/components/credentialTemplateCreation/Atoms/DesignTemplateStep/ColorPicker.jsx
+++ prism-management-console-web/src/components/credentialTemplateCreation/Atoms/DesignTemplateStep/ColorPicker.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Form, Radio } from 'antd';
-import { CheckOutlined } from '@ant-design/icons';
-import Avatar from 'antd/lib/avatar/avatar';
-import contrast from 'get-contrast';
+import ColorOption from './ColorOption';
 
 const ColorPicker = ({ colors, selected, ...formItemProps }) => (
   <Form.Item {...formItemProps} rules={[{ required: true }]}>
@@ -14,32 +12,6 @@ const ColorPicker = ({ colors, selected, ...formItemProps }) => (
     </Radio.Group>
   </Form.Item>
 );
-
-const ColorOption = ({ color, showCheckmark }) => {
-  const contrastVsWhite = contrast.ratio('#FFFFFF', color);
-  const threshold = contrast.ratio('#FFFFFF', '#D8D8D8');
-  const isBrightColor = contrastVsWhite < threshold;
-
-  const circleStyle = {
-    backgroundColor: color,
-    ...(isBrightColor && { border: '1px solid #D8D8D8' })
-  };
-  const checkmarkStyle = { fontSize: '16px', ...(isBrightColor && { color: '#000000' }) };
-
-  return (
-    <Radio value={color}>
-      <Avatar
-        style={circleStyle}
-        icon={showCheckmark && <CheckOutlined style={checkmarkStyle} />}
-      />
-    </Radio>
-  );
-};
-
-ColorOption.propTypes = {
-  color: PropTypes.string.isRequired,
-  showCheckmark: PropTypes.bool.isRequired
-};
 
 ColorPicker.defaultProps = {
   colors: []
--- prism-management-console-web/src/components/credentialTemplateCreation/Atoms/DesignTemplateStep/SortableAttributes.jsx
+++ prism-management-console-web/src/components/credentialTemplateCreation/Atoms/DesignTemplateStep/SortableAttributes.jsx
@@ -1,31 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Space } from 'antd';
-import { DeleteOutlined, SwapOutlined } from '@ant-design/icons';
-import { SortableContainer, SortableElement, SortableHandle } from 'react-sortable-hoc';
-import FixedTextInput from './FixedTextInput';
-import DynamicAttributeInput from './DynamicAttributeInput';
-
-const DragHandle = SortableHandle(() => (
-  <div className="dragHandleWrapper">
-    <SwapOutlined rotate={90} />
-  </div>
-));
-
-const SortableItem = SortableElement(({ value, remove }) => {
-  const { key, name, isFixedText } = value;
-  return (
-    <div className="sortable">
-      <DragHandle />
-      <Space key={key} style={{ display: 'flex', marginBottom: 8 }} align="baseline">
-        <div className="firstGroupInputContainer">
-          {isFixedText ? <FixedTextInput value={value} /> : <DynamicAttributeInput value={value} />}
-          <Button icon={<DeleteOutlined />} onClick={() => remove(name)} />
-        </div>
-      </Space>
-    </div>
-  );
-});
+import { SortableContainer } from 'react-sortable-hoc';
+import SortableItem from './SortableItem';
 
 const SortableList = SortableContainer(({ children }) => <div className="row">{children}</div>);
 
