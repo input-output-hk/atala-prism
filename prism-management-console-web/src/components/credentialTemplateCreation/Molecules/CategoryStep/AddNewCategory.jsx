9fe5d130f947f82dc4af1219745189f7b77ed3f6
Ana Clara Alleva <aalleva@atixlabs.com>
2021-08-02 17:58:42 -0300

    fix select category buttons

--- prism-management-console-web/src/components/credentialTemplateCreation/Molecules/CategoryStep/AddNewCategory.jsx
+++ prism-management-console-web/src/components/credentialTemplateCreation/Molecules/CategoryStep/AddNewCategory.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 import { Col, Row } from 'antd';
-import addNewCategoryIcon from '../../../../images/addNewCategoryIcon.svg';
 
 const AddNewCategory = ({ onClick }) => {
   const { t } = useTranslation();
   return (
-    <Col type="flex" align="middle" className="TypeCard shadow" onClick={onClick}>
+    <Col type="flex" align="middle" className="TypeCard2 shadow" onClick={onClick}>
       <Row className="header-name">{t('credentialTemplateCreation.actions.addCategory')}</Row>
-      <div className="line" />
-      <img className="img-credential" src={addNewCategoryIcon} alt="addNewCategoryIcon" />
     </Col>
   );
 };
