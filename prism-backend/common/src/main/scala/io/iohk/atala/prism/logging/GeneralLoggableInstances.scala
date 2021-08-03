06157019141abc49fb31d60a8d40336bf1fbb459
Kamil Babenkov <kamil.babenkov@scalac.io>
2021-08-03 15:26:13 +0800

    ATA-5209: common: removed unused SHA256DigestLoggable

--- prism-backend/common/src/main/scala/io/iohk/atala/prism/logging/GeneralLoggableInstances.scala
+++ prism-backend/common/src/main/scala/io/iohk/atala/prism/logging/GeneralLoggableInstances.scala
@@ -1,6 +1,5 @@
 package io.iohk.atala.prism.logging
 
-import io.iohk.atala.prism.crypto.SHA256Digest
 import io.iohk.atala.prism.identity.DID
 import tofu.logging._
 
@@ -9,15 +8,7 @@ import tofu.logging._
   */
 object GeneralLoggableInstances {
 
-  implicit val SHA256DigestLoggable: DictLoggable[SHA256Digest] = new DictLoggable[SHA256Digest] {
-    override def fields[I, V, R, S](a: SHA256Digest, i: I)(implicit r: LogRenderer[I, V, R, S]): R = {
-      r.addString("SHA256Digest", a.hexValue, i)
-    }
-
-    override def logShow(a: SHA256Digest): String = s"{SHA256Digest=${a.hexValue}}"
-  }
-
-  implicit val userLoggable: DictLoggable[DID] = new DictLoggable[DID] {
+  implicit val didLoggable: DictLoggable[DID] = new DictLoggable[DID] {
     override def fields[I, V, R, S](a: DID, i: I)(implicit r: LogRenderer[I, V, R, S]): R = {
       r.addString("DID", a.value, i)
     }
