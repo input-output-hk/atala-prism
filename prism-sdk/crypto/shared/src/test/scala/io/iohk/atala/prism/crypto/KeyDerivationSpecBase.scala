09fb556f7fc03bedfba796435fd34e4fd65fc0bd
Shota Jolbordi <shota.jolbordi@iohk.io>
2021-08-08 04:28:00 +0400

    POC

--- prism-sdk/crypto/shared/src/test/scala/io/iohk/atala/prism/crypto/KeyDerivationSpecBase.scala
+++ prism-sdk/crypto/shared/src/test/scala/io/iohk/atala/prism/crypto/KeyDerivationSpecBase.scala
@@ -7,6 +7,11 @@ import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks._
 
 import scala.collection.mutable
+import javax.crypto._
+import javax.crypto.spec._
+import java.security._
+import java.nio.charset.StandardCharsets
+import java.util.Base64
 
 abstract class KeyDerivationSpecBase(val keyDerivation: KeyDerivationTrait) extends AnyWordSpec {
 
@@ -16,6 +21,122 @@ abstract class KeyDerivationSpecBase(val keyDerivation: KeyDerivationTrait) exte
   val pathGen = Gen
     .containerOf[Vector, Int](arbitrary[Int])
     .map(axes => DerivationPath(axes.map(axis => new DerivationAxis(axis))))
+
+  private val encryptionAlgorithm = "AES/CBC/PKCS5Padding"
+  private val keyGenerationAlgorithm = "AES"
+
+  private def md5(bytes: Array[Byte]): Array[Byte] = {
+    val md5Hash = MessageDigest.getInstance("MD5")
+    md5Hash.update(bytes)
+    md5Hash.digest
+  }
+
+  private def AESEncrypt(data: Array[Byte], key: SecretKey, iv: IvParameterSpec): Array[Byte] = {
+    val cipher = Cipher.getInstance(encryptionAlgorithm)
+    cipher.init(Cipher.ENCRYPT_MODE, key, iv)
+    cipher.doFinal(data)
+  }
+
+  private def AESDecrypt(data: Array[Byte], key: SecretKey, iv: IvParameterSpec): Array[Byte] = {
+    val cipher = Cipher.getInstance(encryptionAlgorithm)
+    cipher.init(Cipher.DECRYPT_MODE, key, iv)
+    cipher.doFinal(data)
+  }
+
+  "Proof of concept" should {
+    "Derive private and public keys from mnemonic and use then as AES key and IV" in {
+      val mnemonic = keyDerivation.randomMnemonicCode()
+      val initialSeed = keyDerivation.binarySeed(mnemonic, BIP39TestVectors.password)
+      // I'll generate an extended key with some arbitrary path m/1'/2'
+      val extendedKey = keyDerivation.deriveKey(
+        initialSeed,
+        DerivationPath(
+          Vector(
+            DerivationAxis.hardened(1),
+            DerivationAxis.hardened(2)
+          )
+        )
+      )
+
+      // got public and private keys (as byte arrays)
+      val ecPrivateKeyBytes = extendedKey.privateKey.getEncoded
+      val ecPublicKeyBytes = extendedKey.publicKey.getEncoded
+
+      /**
+        * Here I'm just using EC private key as a Secret Key for AES algorithm, since EC
+        * secret key is just a big integer, and AES secret key can be any sequence of bytes
+        * that is either 128, 192, or 256 bits long, EC private can be used as AES secret key
+        * assuming it has been derived from sufficient amount of entropy
+        */
+      val key = new SecretKeySpec(ecPrivateKeyBytes, keyGenerationAlgorithm)
+
+      /**
+        * Initialization vector does not have any special requirements other then that it
+        * has to be 128 bits long, here I'm using the public key as an IV since it can be deterministically
+        * derived from the mnemonic and derivation path, it is easy to associate IV to the key since it is
+        * a public key of its private key. In this case this key is not really a "key", it is just some random bytes
+        * that can be deterministically derived, and it is not really "public", as leaking this key to the outside world
+        * would be a problem. I'm using md5 to make it 128 bits, I could have just taken first 128 bits as well since it
+        * is 65 bytes long.
+        */
+      val iv = new IvParameterSpec(md5(ecPublicKeyBytes))
+
+      val secretMessage = "Connector message that should be encrypted"
+      println(s"The original message is: ${secretMessage}")
+
+      val cipherTextBytes = AESEncrypt(secretMessage.getBytes, key, iv)
+      val cipherTextStr = new String(cipherTextBytes, StandardCharsets.UTF_8)
+      println(s"Encrypted text is: ${cipherTextStr}")
+
+      val decryptedTextBytes = AESDecrypt(cipherTextBytes, key, iv)
+      val decryptedTextStr = new String(decryptedTextBytes, StandardCharsets.UTF_8)
+      println(s"Decrypted text is: ${decryptedTextStr}")
+
+      secretMessage mustBe decryptedTextStr
+
+    }
+
+    "derive the key and decrypt encrypted message from seed and derivation path" in {
+      // only knowing word list and key index path, I should be able to derive the key
+      // and decrypt messages that have been encrypted with that key using AES
+
+      val originalText = "Connector message that should be encrypted"
+      val base64EncodedCypherText = "yr5HBjwFcIZeoRLOaeHQmENiJIvPZEfaYozHfzUUYxeK1YnrJIxUb4HUYk6Ouv1l"
+      val wordList = List(
+        "option",
+        "width",
+        "fitness",
+        "weasel",
+        "luxury",
+        "picture",
+        "question",
+        "subject",
+        "prize",
+        "chaos",
+        "twelve",
+        "dove"
+      )
+
+      val indexPath = "m/1'/2'"
+
+      val mnemonicCode = MnemonicCode(wordList)
+      val initialSeed = keyDerivation.binarySeed(mnemonicCode, BIP39TestVectors.password)
+      val derivationPath = DerivationPath(indexPath)
+
+      val extendedKey = keyDerivation.deriveKey(initialSeed, derivationPath)
+      val ecPrivateKeyBytes = extendedKey.privateKey.getEncoded
+      val ecPublicKeyBytes = extendedKey.publicKey.getEncoded
+
+      val key = new SecretKeySpec(ecPrivateKeyBytes, keyGenerationAlgorithm)
+      val iv = new IvParameterSpec(md5(ecPublicKeyBytes))
+
+      val decoded = AESDecrypt(Base64.getDecoder.decode(base64EncodedCypherText), key, iv)
+      val stringDecoded = new String(decoded, StandardCharsets.UTF_8)
+
+      stringDecoded mustBe originalText
+
+    }
+  }
 
   "randomMnemonicCode" should {
     "generate 12-word mnemonics" in {
