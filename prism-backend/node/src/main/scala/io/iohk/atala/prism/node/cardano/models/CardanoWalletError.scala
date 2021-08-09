755af3d95621e9ab658af3a3dbb36499812197b1
Vlad <vlad107@users.noreply.github.com>
2021-08-09 10:07:34 +0300

    ATA-5325: Add error classes in accordance with cardano-wallet documentation (#30)

---
+++ prism-backend/node/src/main/scala/io/iohk/atala/prism/node/cardano/models/CardanoWalletError.scala
@@ -0,0 +1,51 @@
+package io.iohk.atala.prism.node.cardano.models
+import enumeratum._
+
+case class CardanoWalletError(message: String, code: CardanoWalletErrorCode)
+    extends RuntimeException(f"Status [${code.entryName}]. $message")
+
+object CardanoWalletError {
+  private val undefinedErrorDescription: String = "Undefined internal error in Cardano Wallet occurred."
+
+  def fromString(message: String, errorCode: String): CardanoWalletError = {
+    val errorCodeMaybe = CardanoWalletErrorCode.fromString(errorCode)
+    errorCodeMaybe.fold {
+      val errorDescription = f"$undefinedErrorDescription [$errorCode]"
+      CardanoWalletError(errorDescription, CardanoWalletErrorCode.UndefinedCardanoWalletError)
+    } { code =>
+      CardanoWalletError(message, code)
+    }
+  }
+}
+
+sealed trait CardanoWalletErrorCode extends EnumEntry.Snakecase
+object CardanoWalletErrorCode extends Enum[CardanoWalletErrorCode] {
+  val values: IndexedSeq[CardanoWalletErrorCode] = findValues
+
+  // Errors 403 Forbidden
+  case object InvalidWalletType extends CardanoWalletErrorCode
+  case object AlreadyWithdrawing extends CardanoWalletErrorCode
+  case object UtxoTooSmall extends CardanoWalletErrorCode
+  case object CannotCoverFee extends CardanoWalletErrorCode
+  case object NotEnoughMoney extends CardanoWalletErrorCode
+  case object TransactionIsTooBig extends CardanoWalletErrorCode
+  case object NoRootKey extends CardanoWalletErrorCode
+  case object WrongEncryptionPassphrase extends CardanoWalletErrorCode
+  case object TransactionAlreadyInLedger extends CardanoWalletErrorCode
+
+  // Errors 404 Not Found
+  case object NoSuchWallet extends CardanoWalletErrorCode
+  case object NoSuchTransaction extends CardanoWalletErrorCode
+
+  // Errors 406 Not Acceptable
+  case object NotAcceptable extends CardanoWalletErrorCode
+
+  // Errors 415 Not Supported Media Type
+  case object UnsupportedMediaType extends CardanoWalletErrorCode
+
+  // Errors 400 Undefined
+  case object UndefinedCardanoWalletError extends CardanoWalletErrorCode
+
+  def fromString(errorCode: String): Option[CardanoWalletErrorCode] =
+    CardanoWalletErrorCode.withNameInsensitiveOption(errorCode)
+}
