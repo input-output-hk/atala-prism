de45f82c010f63dd805b79bd79e0be831ed1e869
Nursultan Esenbaev <nursultan.esenbaev@scalac.io>
2021-08-18 13:41:45 +0300

    proto conversion fix

--- prism-backend/common/src/main/scala/io/iohk/atala/prism/interop/toScalaSDK.scala
+++ prism-backend/common/src/main/scala/io/iohk/atala/prism/interop/toScalaSDK.scala
@@ -1,5 +1,4 @@
 package io.iohk.atala.prism.interop
-import com.google.protobuf.ByteString
 import io.iohk.atala.prism.kotlin.crypto.keys.{ECKeyPair, ECPrivateKey, ECPublicKey}
 import io.iohk.atala.prism.crypto.{
   JvmECPrivateKey,
@@ -12,9 +11,8 @@ import io.iohk.atala.prism.kotlin.crypto.{MerkleInclusionProof, MerkleRoot, SHA2
 import io.iohk.atala.prism.crypto.MerkleTree.{MerkleRoot => MerkleRootScalaSDK}
 import io.iohk.atala.prism.crypto.{SHA256Digest => SHA256DigestScalaSDK}
 import io.iohk.atala.prism.crypto.MerkleTree.{MerkleInclusionProof => MerkleInclusionProofScalaSDK}
-import io.iohk.atala.prism.kotlin.protos.{AtalaOperation, DIDData, KeyUsage, PublicKey, UpdateDIDAction}
 import io.iohk.atala.prism.protos.node_models
-import io.iohk.atala.prism.protos.node_models.{DIDData => DIDDAtaScalaSDK}
+import pbandk.MessageKt
 
 import scala.jdk.CollectionConverters._
 
@@ -61,160 +59,14 @@ object toScalaSDK {
     }
   }
 
-  implicit class DIDDataInterop(private val v: DIDData) extends AnyVal {
-    def asScala: DIDDAtaScalaSDK = {
-      DIDDAtaScalaSDK(v.getId, v.getPublicKeys.asScala.toList.map(_.asScala))
-    }
-  }
-
-  implicit class LedgerDataInterop(private val v: io.iohk.atala.prism.kotlin.protos.LedgerData) extends AnyVal {
-    def asScala: node_models.LedgerData = {
-      node_models.LedgerData(
-        v.getTransactionId,
-        ledger = v.getLedger.asScala,
-        timestampInfo = Option(v.getTimestampInfo).map(_.asScala)
-      )
-    }
-  }
-
-  implicit class LedgerInterop(private val v: io.iohk.atala.prism.kotlin.protos.Ledger) extends AnyVal {
-    def asScala: io.iohk.atala.prism.protos.common_models.Ledger =
-      io.iohk.atala.prism.protos.common_models.Ledger.fromValue(v.getValue)
-  }
-
-  implicit class ProtosPublicKeyInterop(private val v: io.iohk.atala.prism.kotlin.protos.PublicKey) extends AnyVal {
-    def asScala: node_models.PublicKey = {
-      io.iohk.atala.prism.protos.node_models
-        .PublicKey(
-          id = v.getId,
-          usage = v.getUsage.asScala,
-          addedOn = Option(v.getAddedOn).map(_.asScala),
-          revokedOn = Option(v.getRevokedOn).map(_.asScala),
-          keyData = v.getKeyData.asScala
-        )
+  implicit class DIDDataInterop(private val v: io.iohk.atala.prism.kotlin.protos.DIDData) extends AnyVal {
+    def asScala: node_models.DIDData = {
+      node_models.DIDData.parseFrom(MessageKt.encodeToByteArray(v))
     }
   }
 
   implicit class AtalaOperationInterop(private val v: io.iohk.atala.prism.kotlin.protos.AtalaOperation) extends AnyVal {
-    def asScala: node_models.AtalaOperation = {
-      val op = v.getOperation match {
-        case op: AtalaOperation.Operation.CreateDid =>
-          node_models.AtalaOperation.Operation.CreateDid(op.getValue.asScala)
-        case op: AtalaOperation.Operation.IssueCredentialBatch =>
-          node_models.AtalaOperation.Operation.IssueCredentialBatch(op.getValue.asScala)
-        case op: AtalaOperation.Operation.RevokeCredentials =>
-          node_models.AtalaOperation.Operation.RevokeCredentials(op.getValue.asScala)
-        case op: AtalaOperation.Operation.UpdateDid =>
-          node_models.AtalaOperation.Operation.UpdateDid(op.getValue.asScala)
-        case _ => node_models.AtalaOperation.Operation.Empty
-      }
-
-      node_models.AtalaOperation(op)
-    }
-  }
-
-  implicit class UpdateDIDOperationInterop(private val v: io.iohk.atala.prism.kotlin.protos.UpdateDIDOperation)
-      extends AnyVal {
-    def asScala: node_models.UpdateDIDOperation =
-      node_models.UpdateDIDOperation(ByteString.copyFrom(v.getPreviousOperationHash.getArray), v.getId)
-  }
-
-  implicit class IssueCredentialBatchInterop(
-      private val v: io.iohk.atala.prism.kotlin.protos.IssueCredentialBatchOperation
-  ) extends AnyVal {
-    def asScala: node_models.IssueCredentialBatchOperation =
-      node_models.IssueCredentialBatchOperation(Option(v.getCredentialBatchData).map(_.asScala))
-  }
-
-  implicit class UpdateDIDActionInterop(private val v: io.iohk.atala.prism.kotlin.protos.UpdateDIDAction)
-      extends AnyVal {
-    def asScala: node_models.UpdateDIDAction.Action =
-      v.getAction match {
-        case key: UpdateDIDAction.Action.AddKey => node_models.UpdateDIDAction.Action.AddKey(key.getValue.asScala)
-        case key: UpdateDIDAction.Action.RemoveKey => node_models.UpdateDIDAction.Action.RemoveKey(key.getValue.asScala)
-        case _ => node_models.UpdateDIDAction.Action.Empty
-      }
-  }
-
-  implicit class AddKeyActionInterop(private val v: io.iohk.atala.prism.kotlin.protos.AddKeyAction) extends AnyVal {
-    def asScala: node_models.AddKeyAction = node_models.AddKeyAction(key = Option(v.getKey).map(_.asScala))
-  }
-
-  implicit class RemoveKeyActionInterop(private val v: io.iohk.atala.prism.kotlin.protos.RemoveKeyAction)
-      extends AnyVal {
-    def asScala: node_models.RemoveKeyAction = node_models.RemoveKeyAction(keyId = v.getKeyId)
-  }
-
-  implicit class RevokeCredentialsOperationInterop(
-      private val v: io.iohk.atala.prism.kotlin.protos.RevokeCredentialsOperation
-  ) extends AnyVal {
-    def asScala: node_models.RevokeCredentialsOperation =
-      node_models.RevokeCredentialsOperation(
-        ByteString.copyFrom(v.getPreviousOperationHash.getArray),
-        v.getCredentialBatchId,
-        v.getCredentialsToRevoke.asScala.map(arr => ByteString.copyFrom(arr.getArray)).toList
-      )
-  }
-
-  implicit class CredentialBatchDataInterop(private val v: io.iohk.atala.prism.kotlin.protos.CredentialBatchData)
-      extends AnyVal {
-    def asScala: node_models.CredentialBatchData =
-      node_models.CredentialBatchData(v.getIssuerDid, ByteString.copyFrom(v.getMerkleRoot.getArray))
-  }
-
-  implicit class CreateDIDOperationInterop(private val v: io.iohk.atala.prism.kotlin.protos.CreateDIDOperation)
-      extends AnyVal {
-    def asScala: node_models.CreateDIDOperation = node_models.CreateDIDOperation(Option(v.getDidData).map(_.asScala))
-  }
-
-  implicit class ProtosKeyDataInterop(private val v: io.iohk.atala.prism.kotlin.protos.PublicKey.KeyData[_])
-      extends AnyVal {
-    def asScala: node_models.PublicKey.KeyData = {
-      v match {
-        case data: PublicKey.KeyData.EcKeyData =>
-          io.iohk.atala.prism.protos.node_models.PublicKey.KeyData.EcKeyData(
-            io.iohk.atala.prism.protos.node_models.ECKeyData(
-              data.getValue.getCurve,
-              ByteString.copyFrom(data.getValue.getX.getArray),
-              ByteString.copyFrom(data.getValue.getY.getArray)
-            )
-          )
-        case _ => io.iohk.atala.prism.protos.node_models.PublicKey.KeyData.Empty
-      }
-    }
-  }
-
-  implicit class ProtosTimestampInfoInterop(private val v: io.iohk.atala.prism.kotlin.protos.TimestampInfo)
-      extends AnyVal {
-    def asScala: node_models.TimestampInfo = {
-      io.iohk.atala.prism.protos.node_models
-        .TimestampInfo(
-          v.getBlockSequenceNumber,
-          v.getOperationSequenceNumber,
-          Option(v.getBlockTimestamp).map(_.asScala)
-        )
-    }
-  }
-
-  implicit class ProtosTimestampInterop(private val v: pbandk.wkt.Timestamp) extends AnyVal {
-    def asScala: com.google.protobuf.timestamp.Timestamp = {
-      com.google.protobuf.timestamp.Timestamp(v.getSeconds, v.getNanos)
-    }
-  }
-
-  implicit class ProtosKeyUsageInterop(private val v: io.iohk.atala.prism.kotlin.protos.KeyUsage) extends AnyVal {
-    def asScala: node_models.KeyUsage = {
-      v match {
-        case _: KeyUsage.UNKNOWN_KEY => io.iohk.atala.prism.protos.node_models.KeyUsage.UNKNOWN_KEY
-        case _: KeyUsage.MASTER_KEY => io.iohk.atala.prism.protos.node_models.KeyUsage.MASTER_KEY
-        case _: KeyUsage.AUTHENTICATION_KEY =>
-          io.iohk.atala.prism.protos.node_models.KeyUsage.AUTHENTICATION_KEY
-        case _: KeyUsage.ISSUING_KEY => io.iohk.atala.prism.protos.node_models.KeyUsage.ISSUING_KEY
-        case unrecognized: KeyUsage.UNRECOGNIZED =>
-          io.iohk.atala.prism.protos.node_models.KeyUsage.Unrecognized(unrecognized.getValue)
-        case _: KeyUsage.COMMUNICATION_KEY =>
-          io.iohk.atala.prism.protos.node_models.KeyUsage.COMMUNICATION_KEY
-      }
-    }
+    def asScala: node_models.AtalaOperation =
+      node_models.AtalaOperation.parseFrom(MessageKt.encodeToByteArray(v))
   }
 }
