98e10cfea3766aa9e23208c8d7a7ac99472ae058
Atix <nicolas.bianco@iohk.io>
2021-08-19 15:20:15 -0300

    ATA-5343 Enable blog dynamic search

--- prism-interactive-demo-web/src/components/searchbar/searchbar.js
+++ prism-interactive-demo-web/src/components/searchbar/searchbar.js
@@ -1,14 +1,72 @@
-import React from 'react';
-import { Input } from 'antd';
-import { navigate } from 'gatsby';
+import React, { useState, useEffect } from 'react';
+import { Input, AutoComplete } from 'antd';
+import { graphql, navigate, useStaticQuery } from 'gatsby';
+import keywordExtractor from 'keyword-extractor';
+import _ from 'lodash';
+
+import './style.scss';
 
 const { Search } = Input;
 
-const Searchbar = () => (
-  <div className="group">
-    <h3>Search Blogs</h3>
-    <Search onSearch={value => navigate(`/search?query=${value}`)} style={{ width: 250 }} />
-  </div>
-);
+const searchBlogs = value => navigate(`/search?query=${value}`);
+
+const Searchbar = () => {
+  const {
+    posts: { nodes: posts }
+  } = useStaticQuery(
+    graphql`
+      query {
+        posts: allMarkdownRemark {
+          nodes {
+            html
+            frontmatter {
+              title
+              description
+            }
+          }
+        }
+      }
+    `
+  );
+
+  const [wordList, setWordList] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+
+  useEffect(() => {
+    const postsContent = posts.reduce(
+      (accum, { frontmatter: { title, description }, html }) =>
+        accum.concat(title, ' ', description, ' ', html.replace(/(<([^>]+)>)/gi, ''), ' '),
+      ''
+    );
+
+    const newWordList = keywordExtractor.extract(postsContent, {
+      language: 'english',
+      remove_digits: true,
+      return_changed_case: true,
+      remove_duplicates: true
+    });
+
+    const sortedWordList = _.sortBy(newWordList, ['length']);
+
+    setWordList(sortedWordList);
+  }, [posts]);
+
+  return (
+    <div className="group">
+      <h3>Search Blogs</h3>
+      <AutoComplete
+        className="searchbar"
+        value={searchValue}
+        onSelect={searchBlogs}
+        onChange={setSearchValue}
+        dataSource={
+          searchValue ? wordList.filter(word => _.startsWith(word, searchValue.toLowerCase())) : []
+        }
+      >
+        <Search style={{ width: 250 }} onSearch={searchBlogs} />
+      </AutoComplete>
+    </div>
+  );
+};
 
 export default Searchbar;
