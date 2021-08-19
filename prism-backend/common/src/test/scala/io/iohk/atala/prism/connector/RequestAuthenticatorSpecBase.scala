10ab125913543020ce16f4749da150f4d663342a
Nursultan Esenbaev <nursultan.esenbaev@scalac.io>
2021-08-19 08:38:30 +0300

    tests

---
+++ prism-backend/common/src/test/scala/io/iohk/atala/prism/connector/RequestAuthenticatorSpecBase.scala
@@ -0,0 +1,38 @@
+package io.iohk.atala.prism.connector
+
+import io.iohk.atala.prism.crypto.{ECSignature, ECTrait}
+import org.scalatest.matchers.must.Matchers._
+import org.scalatest.wordspec.AnyWordSpec
+
+import java.util.Base64
+import scala.util.Random
+
+abstract class RequestAuthenticatorSpecBase(ec: ECTrait) extends AnyWordSpec {
+  private val requestAuthenticator = new RequestAuthenticator(ec)
+  private val request = Array.ofDim[Byte](128)
+  new Random().nextBytes(request)
+
+  "signConnectorRequest" should {
+    "return the encoded signature and nonce" in {
+      val keyPair = ec.generateKeyPair()
+
+      val signedRequest = requestAuthenticator.signConnectorRequest(request, keyPair.privateKey)
+
+      val signature = Base64.getUrlDecoder.decode(signedRequest.encodedSignature)
+      signedRequest.signature must contain theSameElementsInOrderAs signature
+      val requestNonce = Base64.getUrlDecoder.decode(signedRequest.encodedRequestNonce)
+      signedRequest.requestNonce must contain theSameElementsInOrderAs requestNonce
+      val requestWithNonce = requestNonce ++ request
+      ec.verify(requestWithNonce, keyPair.publicKey, ECSignature(signature)) mustBe true
+    }
+
+    "return random nonces" in {
+      val keyPair = ec.generateKeyPair()
+
+      val signedRequest1 = requestAuthenticator.signConnectorRequest(request, keyPair.privateKey)
+      val signedRequest2 = requestAuthenticator.signConnectorRequest(request, keyPair.privateKey)
+
+      signedRequest1.encodedRequestNonce must not be signedRequest2.encodedRequestNonce
+    }
+  }
+}
