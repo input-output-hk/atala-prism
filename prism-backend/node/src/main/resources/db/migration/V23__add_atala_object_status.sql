e6033a1404f81d9655c4013c09009b09aa23dc0b
Vladislav Podtelkin <vladislav.podtelkin@iohk.io>
2021-08-26 14:50:51 +0300

    ATA-5425: Parametrize waiting time for retry and submission

--- prism-backend/node/src/main/resources/db/migration/V23__add_atala_object_status.sql
+++ prism-backend/node/src/main/resources/db/migration/V23__add_atala_object_status.sql
@@ -8,5 +8,3 @@ UPDATE atala_objects
   WHERE processed = true;
 
 ALTER TABLE atala_objects DROP COLUMN processed;
-
-CREATE INDEX atala_objects_processed_index ON atala_objects((1)) WHERE atala_object_status = 'PENDING';
