d2be0a6faab9f43c2edaf655d0dc037fb9a9b3b8
Nursultan Esenabev <80749770+esenese@users.noreply.github.com>
2021-08-26 11:30:45 +0600

    docker image fix (#68)
    * docker image fix

--- prism-backend/project/PrismBuild.scala
+++ prism-backend/project/PrismBuild.scala
@@ -46,6 +46,13 @@ object PrismBuild {
           val argsString = args.mkString(" ")
           ("testOnly " + argsString) :: ("testOnlyUntilFailed " + argsString) :: state
         },
+        assembly / assemblyExcludedJars := {
+          val cp = (assembly / fullClasspath).value
+
+          val excludeLibs = Set("protobuf-javalite", "kotlinx-coroutines-core", "pbandk-protos")
+
+          cp.filter{ path => excludeLibs.exists( lib => path.data.getName.startsWith(lib)) }
+        },
         assembly / assemblyMergeStrategy := {
           // Merge service files, otherwise GRPC client doesn't work: https://github.com/grpc/grpc-java/issues/5493
           case PathList("META-INF", "services", _*) => MergeStrategy.concat
