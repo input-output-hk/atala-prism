9ffdb630f25d615f65fba6de69e9a57782100744
Atix <nicolas.bianco@iohk.io>
2021-09-02 13:04:55 -0300

    ATA-4985 Add Product Tour introduction

--- prism-management-console-web/src/APIs/config.js
+++ prism-management-console-web/src/APIs/config.js
@@ -1,9 +1,31 @@
 const { REACT_APP_GRPC_CLIENT } = window._env_;
 
+const INITIAL_TUTORIAL_PROGRESS = {
+  started: false,
+  paused: false,
+  finished: false,
+  basicSteps: 0,
+  contacts: 0,
+  groups: 0,
+  credentials: 0
+};
+
 export const config = {
-  grpcClient: getFromLocalStorage('backendUrl') || REACT_APP_GRPC_CLIENT
+  grpcClient: getFromLocalStorage('backendUrl') || REACT_APP_GRPC_CLIENT,
+  tutorialProgress: getTutorialProgress(),
+  saveTutorialProgress
 };
 
 function getFromLocalStorage(key) {
   return window.localStorage.getItem(key);
+}
+
+function getTutorialProgress() {
+  const tutorialProgress = getFromLocalStorage('tutorialProgress');
+  if (tutorialProgress) return JSON.parse(tutorialProgress);
+  return INITIAL_TUTORIAL_PROGRESS;
+}
+
+function saveTutorialProgress(tutorialProgress) {
+  window.localStorage.setItem('tutorialProgress', JSON.stringify(tutorialProgress));
 }
