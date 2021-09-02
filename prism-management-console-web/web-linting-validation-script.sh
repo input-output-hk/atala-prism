18825e9846f03edf2f7ea18aeb73b503d7f80343
Milos Backonja <35807060+milosbackonja@users.noreply.github.com>
2021-09-02 17:43:56 +0200

    UI build fixes (#118)
    * Testing flow and changes on test branches without provision
    
    * Adding back copyEnvVarsToWindow.sh
    
    * Removing copyEnvVarsToWindow.sh, and adjusting Dockerfile to match
    
    * Change from plan to apply for rebuild-prism-env job

--- prism-management-console-web/web-linting-validation-script.sh
+++ prism-management-console-web/web-linting-validation-script.sh
@@ -3,6 +3,8 @@
 set -e
 # Delete all dependencies so it won't use space needlessly #
 rm -rf node_modules/ package-lock.json yarn.lock;
+# Don't generate a lockfile and fail if an update is needed
+yarn --frozen-lockfile
 # Install dependencies #
 yarn install;
 # Run all test cases #
