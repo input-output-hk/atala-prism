18e05f27f98fe62585bc765e652323f0c6a579e3
dbrosio3 <dbrosio@atixlabs.com>
2021-10-12 12:09:36 -0300

    ATA-5547: reverted tempalte creation button removal

--- prism-management-console-web/src/components/credentialTemplates/Molecules/Headers/ActionsHeader.jsx
+++ prism-management-console-web/src/components/credentialTemplates/Molecules/Headers/ActionsHeader.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
 import TemplateFiltersContainer from '../Filters/TemplateFiltersContainer';
+import CreateTemplateButton from '../../Atoms/Buttons/CreateTemplateButton';
 import './_style.scss';
 
 const ActionsHeader = observer(() => (
   <div className="ActionsHeader flex">
     <TemplateFiltersContainer showDateFilters />
+    <CreateTemplateButton />
   </div>
 ));
 
