2b3f1503ceaf2b938dbe7d8efc5951294f7a73d8
Ana Clara Alleva <aalleva@atixlabs.com>
2021-10-13 16:10:50 -0300

    add link to pictures on brochure. fix responsive

--- prism-interactive-demo-web/src/pages/resources.js
+++ prism-interactive-demo-web/src/pages/resources.js
@@ -97,13 +97,13 @@ const Resources = ({ data }) => {
                 </a>
               </div>
             </div>
-            <img className="thumbnail" src={featuredBrochure.thumbnail} alt="Thumbnail" />
+            <a className="thumbnail" href={featuredBrochure.url} target="_blank"><img src={featuredBrochure.thumbnail} alt="Thumbnail" /></a>
           </div>
           <div className="brochures-container">
             {otherBrochures.map(({ title, description, url, thumbnail }) => (
               <div className="brochure-container">
                 <div className="brochure-info-container">
-                  <img className="thumbnail" src={thumbnail} alt="Thumbnail" />
+                  <a href={url} target="_blank"><img className="thumbnail" src={thumbnail} alt="Thumbnail" /></a>
                   <h2>{title}</h2>
                   <p>{description}</p>
                 </div>
