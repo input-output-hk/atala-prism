c15ebc5c23349463c9a409c9567327a4dcdcef45
itegulov <itegulov@users.noreply.github.com>
2021-10-20 02:36:17 +0000

    Bump SDK version to 1.3.0-build-3-2653397d

--- prism-backend/project/Dependencies.scala
+++ prism-backend/project/Dependencies.scala
@@ -37,7 +37,7 @@ object versions {
   val twirl = "1.5.1"
   val typesafeConfig = "1.4.1"
   val http4s = "0.21.7"
-  val prismSdk = "0.1.0-72fe0fe7"
+  val prismSdk = "1.3.0-build-3-2653397d"
 }
 
 object Dependencies {
