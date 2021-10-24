e7649a494841a166a59ad31713f7eecbb1ed9fe2
Atala Dev <atala.dev@iohk.io>
2021-10-24 00:15:48 +0000

    Update mockito-scala, ... to 1.16.46 in master

--- prism-backend/project/Dependencies.scala
+++ prism-backend/project/Dependencies.scala
@@ -20,7 +20,7 @@ object versions {
   val logback = "1.2.6"
   val logbackLogstash = "6.6"
   val jaxb = "2.3.1"
-  val mockito = "1.16.0"
+  val mockito = "1.16.46"
   val monix = "3.2.2"
   val osLib = "0.7.8"
   val playJson = "2.9.1"
