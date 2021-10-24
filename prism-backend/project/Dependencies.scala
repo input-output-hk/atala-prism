f57299b2dda65f06c3baf72abf663dd2715fc865
Atala Dev <atala.dev@iohk.io>
2021-10-24 00:21:59 +0000

    Update bitcoin-lib to 0.19 in 1-2-x

--- prism-backend/project/Dependencies.scala
+++ prism-backend/project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object versions {
   val awsSdk = "2.11.8"
-  val bitcoinLib = "0.18"
+  val bitcoinLib = "0.19"
   val bouncycastle = "1.62"
   val braintree = "3.2.0"
   val catsScalatest = "3.0.5"
