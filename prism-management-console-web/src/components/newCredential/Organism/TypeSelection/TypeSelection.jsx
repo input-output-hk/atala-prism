48d44c79f513914e056daa20f634f1eaf159ccb7
Ana Clara Alleva <52926991+ana-alleva@users.noreply.github.com>
2021-10-25 16:35:45 -0300

    ATA-5547: fix credential cards (#174)
    Co-authored-by: dbrosio3 <dbrosio@atixlabs.com>

--- prism-management-console-web/src/components/newCredential/Organism/TypeSelection/TypeSelection.jsx
+++ prism-management-console-web/src/components/newCredential/Organism/TypeSelection/TypeSelection.jsx
@@ -1,61 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { message } from 'antd';
+import { observer } from 'mobx-react-lite';
 import { useTranslation } from 'react-i18next';
-import TypeCard from '../../Molecules/TypeCard/TypeCard';
-import { credentialTypesShape, templateCategoryShape } from '../../../../helpers/propShapes';
 import SimpleLoading from '../../../common/Atoms/SimpleLoading/SimpleLoading';
-import {
-  CREDENTIAL_TYPE_STATUSES,
-  VALID_CREDENTIAL_TYPE_STATUSES
-} from '../../../../helpers/constants';
+import { CONFIRMED } from '../../../../helpers/constants';
+import { useTemplateStore, useTemplateUiState } from '../../../../hooks/useTemplateStore';
+import TemplatesList from './TemplatesList';
+import CreateTemplateButton from '../../../credentialTemplates/Atoms/Buttons/CreateTemplateButton';
+import EmptyComponent from '../../../common/Atoms/EmptyComponent/EmptyComponent';
+import noTemplatesPicture from '../../../../images/noTemplates.svg';
+import { useSession } from '../../../../hooks/useSession';
 
 import './_style.scss';
 
-// TODO: refactor this page based on new mockups (ATA-5547)
-// eslint-disable-next-line no-unused-vars
-const TypeSelection = ({ credentialTypes, templateCategories, selectedType, onTypeSelection }) => {
+const TypeSelection = observer(({ selectedType, onTypeSelection }) => {
   const { t } = useTranslation();
+  const { accountStatus } = useSession();
+  const { isLoading } = useTemplateStore();
+  const { filteredTemplates, hasFiltersApplied } = useTemplateUiState();
 
-  const isValidState = state => VALID_CREDENTIAL_TYPE_STATUSES.includes(state);
-  const isMockedState = state => state === CREDENTIAL_TYPE_STATUSES.MOCKED;
+  const noTemplates = !filteredTemplates?.length;
 
-  const showWarning = () => message.warn(t('templates.messages.customTypeWarning'));
+  const emptyProps = {
+    photoSrc: noTemplatesPicture,
+    model: t('templates.title'),
+    isFilter: hasFiltersApplied,
+    button: noTemplates && accountStatus === CONFIRMED ? <CreateTemplateButton /> : null
+  };
+
+  if (isLoading) return <SimpleLoading size="md" />;
+  if (noTemplates) return <EmptyComponent {...emptyProps} />;
 
   return (
     <div className="TypeSelectionWrapper">
       <div className="TypeSelectionContainer">
         <div className="TypeSelection">
-          {!credentialTypes.length ? (
-            <SimpleLoading size="md" />
-          ) : (
-            credentialTypes
-              .filter(({ state }) => isValidState(state))
-              .map(ct => (
-                <TypeCard
-                  credentialType={ct}
-                  typeKey={ct.id}
-                  key={ct.id}
-                  isSelected={selectedType === ct.id}
-                  onClick={isMockedState(ct.state) ? showWarning : onTypeSelection}
-                  logo={ct.icon}
-                  sampleImage={ct.sampleImage}
-                />
-              ))
-          )}
+          <TemplatesList selectedType={selectedType} onTypeSelection={onTypeSelection} />
         </div>
       </div>
     </div>
   );
-};
+});
 
 TypeSelection.defaultProps = {
   selectedType: ''
 };
 
 TypeSelection.propTypes = {
-  credentialTypes: credentialTypesShape.isRequired,
-  templateCategories: PropTypes.arrayOf(templateCategoryShape).isRequired,
   selectedType: PropTypes.string,
   onTypeSelection: PropTypes.func.isRequired
 };
