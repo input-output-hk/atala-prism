3a3d32134a8600b81452369f3695a04b98195c57
dbrosio3 <dbrosio@atixlabs.com>
2021-10-29 13:04:53 -0300

    ATA-5466: renamed boolean to partiallyMatches

--- prism-management-console-web/src/helpers/filterHelpers.js
+++ prism-management-console-web/src/helpers/filterHelpers.js
@@ -27,14 +27,15 @@ export const filterByNewerDate = (filter, field) => !filter || filter < field;
 
 export const filterByUnixDate = (filter, field) => !filter || filter === dateFormat(field);
 
+export const filterByManyFields = (toFilter, filterValue, keys) =>
+  toFilter.filter(item => filterByMultipleKeys(filterValue, item, keys));
+
 // filters by a series of keys:
 // where only the items that include the `filterValue` on any of it's keys will return true
 export const filterByMultipleKeys = (filterValue, item, keys) =>
-  keys.reduce((matches, key) => matches || filterByInclusion(filterValue, item[key]), false);
-
-export const filterByManyFields = (toFilter, filterValue, keys) =>
-  toFilter.filter(item =>
-    keys.reduce((matches, key) => matches || filterByInclusion(filterValue, item[key]), false)
+  keys.reduce(
+    (partiallyMatches, key) => partiallyMatches || filterByInclusion(filterValue, item[key]),
+    false
   );
 
 export const exactValueExists = (list, filter, key) => list.some(item => item[key] === filter);
