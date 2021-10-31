b18d725ed9026711db2eb605030fd6f4e870dc57
Atala Dev <atala.dev@iohk.io>
2021-10-31 00:11:47 +0000

    Update prism-api-jvm, ... to 1.3.0-build-9-6840800f in master

--- prism-backend/project/Dependencies.scala
+++ prism-backend/project/Dependencies.scala
@@ -33,7 +33,7 @@ object versions {
   val twirl = "1.5.1"
   val typesafeConfig = "1.4.1"
   val http4s = "0.21.7"
-  val prismSdk = "1.3.0-build-8-a84ebb4a"
+  val prismSdk = "1.3.0-build-9-6840800f"
 }
 
 object Dependencies {
