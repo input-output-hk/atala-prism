0b5d635076e797d2e02c67de909658bf9a6cba02
Atala Dev <atala.dev@iohk.io>
2021-10-31 00:11:52 +0000

    Update kamon-bundle, kamon-prometheus to 2.3.1 in master

--- prism-backend/project/Dependencies.scala
+++ prism-backend/project/Dependencies.scala
@@ -15,7 +15,7 @@ object versions {
   val enumeratumDoobie = "1.7.0"
   val flyway = "7.15.0"
   val grpc = "1.41.0"
-  val kamon = "2.1.21"
+  val kamon = "2.3.1"
   val logback = "1.2.6"
   val logbackLogstash = "6.6"
   val jaxb = "2.3.1"
