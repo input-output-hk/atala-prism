5330c37db8a52b2a88fe97052d7d16c5657b0f76
Milos Dzepina <milos.dzepina@iohk.io>
2021-11-05 11:24:56 +0100

    ATA-5641: update validation script and remove unused plugins

--- prism-management-console-web/web-linting-validation-script.sh
+++ prism-management-console-web/web-linting-validation-script.sh
@@ -2,12 +2,10 @@
 # Stop script if something fails #
 set -e
 # Delete all dependencies so it won't use space needlessly #
-rm -rf node_modules/ package-lock.json yarn.lock;
-# Don't generate a lockfile and fail if an update is needed
-yarn --frozen-lockfile
-# Install dependencies #
-yarn install;
-# Run all test cases #
-yarn run eslint;
+rm -rf node_modules/;
+# Install dependencies (Don't generate a lockfile and fail if an update is needed)
+yarn install --frozen-lockfile;
+# Run validations. build script runs eslint checks too, so it 2-in-1 validation. TODO: run jest tests? #
+yarn build;
 # Delete all dependencies so it won't use space needlessly #
-rm -rf node_modules/;                      
+rm -rf node_modules/;
