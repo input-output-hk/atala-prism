f2a60e91433439aa2046437b9b38a5112e73bd2b
Milos Dzepina <milos.dzepina@iohk.io>
2021-11-05 11:45:55 +0100

    ATA-5641: remove unused code for cancelable promises

--- prism-management-console-web/src/helpers/promises.js
+++ prism-management-console-web/src/helpers/promises.js
@@ -1,37 +1,3 @@
-// TODO: remove if not used
-// const promiseWasCancelled = 'Promise was cancelled';
-//
-// export class PromiseCancellationError extends Error {
-//   constructor(message) {
-//     super();
-//     this.message = message;
-//     this.name = promiseWasCancelled;
-//   }
-// }
-//
-// const promiseCancellationError = new PromiseCancellationError();
-//
-// export const makeCancelable = promise => {
-//   let hasCanceled_ = false;
-//   const wrappedPromise = new Promise((resolve, reject) => {
-//     promise
-//       .then(val => (hasCanceled_ ? reject(promiseCancellationError) : resolve(val)))
-//       .catch(err => {
-//         const error = new PromiseCancellationError();
-//         error.message = err.message;
-//         error.stack = err.stack;
-//         return reject(error);
-//       });
-//   });
-//
-//   return {
-//     promise: wrappedPromise,
-//     cancel() {
-//       hasCanceled_ = true;
-//     }
-//   };
-// };
-
 const RETRY_COUNT_DEFAULT = 5;
 const RETRY_INTERVAL_DEFAULT = 1000;
 
