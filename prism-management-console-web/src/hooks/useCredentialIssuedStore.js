17969ea219f7fd2adc4177c0f9ed47dfaf00ec4c
dbrosio3 <dbrosio@atixlabs.com>
2021-11-08 15:01:47 -0300

    ATA-5468: improved useCredentialsIssuedStore

--- prism-management-console-web/src/hooks/useCredentialIssuedStore.js
+++ prism-management-console-web/src/hooks/useCredentialIssuedStore.js
@@ -1,5 +1,4 @@
-import { reaction } from 'mobx';
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect } from 'react';
 import { PrismStoreContext } from '../stores/domain/PrismStore';
 import { UiStateContext } from '../stores/ui/UiState';
 
@@ -20,57 +19,34 @@ export const useCredentialIssuedStore = ({ fetch, reset } = { fetch: false, rese
 
 export const useCredentialIssuedUiState = ({ reset } = { reset: false }) => {
   const { credentialIssuedUiState } = useContext(UiStateContext);
-  const { triggerSearch, resetState } = credentialIssuedUiState;
+  const {
+    triggerSearch,
+    resetState,
+    nameFilter,
+    credentialTypeFilter,
+    credentialStatusFilter,
+    connectionStatusFilter,
+    dateFilter,
+    sortingKey,
+    sortDirection
+  } = credentialIssuedUiState;
 
   useEffect(() => {
     if (reset) resetState();
   }, [reset, resetState]);
 
   useEffect(() => {
-    reaction(() => credentialIssuedUiState.nameFilter, () => triggerSearch());
-  }, [credentialIssuedUiState.nameFilter, triggerSearch]);
-
-  useEffect(() => {
-    reaction(() => credentialIssuedUiState.credentialTypeFilter, () => triggerSearch());
-  }, [credentialIssuedUiState.credentialTypeFilter, triggerSearch]);
-
-  useEffect(() => {
-    reaction(() => credentialIssuedUiState.credentialStatusFilter, () => triggerSearch());
-  }, [credentialIssuedUiState.credentialStatusFilter, triggerSearch]);
-
-  useEffect(() => {
-    reaction(() => credentialIssuedUiState.connectionStatusFilter, () => triggerSearch());
-  }, [credentialIssuedUiState.connectionStatusFilter, triggerSearch]);
-
-  useEffect(() => {
-    reaction(() => credentialIssuedUiState.dateFilter, () => triggerSearch());
-  }, [credentialIssuedUiState.dateFilter, triggerSearch]);
-
-  useEffect(() => {
-    reaction(() => credentialIssuedUiState.sortingKey, () => triggerSearch());
-  }, [credentialIssuedUiState.sortingKey, triggerSearch]);
-
-  useEffect(() => {
-    reaction(() => credentialIssuedUiState.sortDirection, () => triggerSearch());
-  }, [credentialIssuedUiState.sortDirection, triggerSearch]);
+    triggerSearch();
+  }, [
+    nameFilter,
+    credentialTypeFilter,
+    credentialStatusFilter,
+    connectionStatusFilter,
+    dateFilter,
+    sortingKey,
+    sortDirection,
+    triggerSearch
+  ]);
 
   return credentialIssuedUiState;
-};
-
-export const useAllCredentials = () => {
-  const [allCredentials, setAllCredentials] = useState([]);
-  const { fetchAllCredentials, isFetching } = useCredentialIssuedStore();
-
-  useEffect(() => {
-    const triggerFetch = async () => {
-      const fetchedCredentials = await fetchAllCredentials();
-      setAllCredentials(fetchedCredentials);
-    };
-    triggerFetch();
-  }, [fetchAllCredentials]);
-
-  return {
-    isLoading: isFetching,
-    allCredentials
-  };
 };
