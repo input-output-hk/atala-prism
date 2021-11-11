7fb3e44a917cefe45f363bb164f532605782d171
Atix <nicolas.bianco@iohk.io>
2021-11-11 14:24:27 -0300

    ATA-5678 Abstract blog entry component

--- prism-interactive-demo-web/src/pages/search.js
+++ prism-interactive-demo-web/src/pages/search.js
@@ -1,16 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { Link, graphql } from 'gatsby';
+import { graphql } from 'gatsby';
 import queryString from 'query-string';
-import Highlighter from 'react-highlight-words';
 import HeaderBlog from '../components/headerBlog/headerBlog';
 import FooterBlog from '../components/footer/BlogFooter';
-import calendarIcon from '../images/calendar.svg';
-import authorIcon from '../images/author.svg';
-import clockIcon from '../images/clock.svg';
 import SEO from '../components/seo/seo';
 import Sidebar from '../components/sidebar/sidebar';
-import { getTextToHighlight } from '../helpers/textFormatter';
+import BlogEntry from '../components/blogEntry/BlogEntry';
 import { buildSearchIndex } from '../helpers/searchIndex';
 import { groupedPostsShape, postsShape, recentPostsShape } from '../helpers/propTypes';
 
@@ -50,82 +46,9 @@ const Search = ({ data, location }) => {
         <div className="SectionContainer">
           <div className="containerEntry">
             {!filteredPosts.length && <p>No results found. Try searching for full words.</p>}
-            {filteredPosts.map(post => {
-              const title = post.frontmatter.title || post.fields.slug;
-
-              const displayBody = getTextToHighlight(post.html, search.query);
-
-              return (
-                <div className="mainSectionContainer">
-                  <div className="articlesContainer">
-                    <article
-                      className="post-list-item"
-                      itemScope
-                      itemType="https://schema.org/Article"
-                      key={post.fields.slug}
-                    >
-                      <header className="entryHeader">
-                        <Link to={post.fields.slug} itemProp="url">
-                          <h2 className="h2" itemProp="headline">
-                            <Highlighter
-                              highlightClassName="Highlighted"
-                              searchWords={[search.query]}
-                              textToHighlight={title}
-                              autoEscape
-                            />
-                          </h2>
-                        </Link>
-                      </header>
-                      <div className="blog-post-container">
-                        <section className="blog-post">
-                          <p className="copete" itemProp="description">
-                            <Highlighter
-                              highlightClassName="Highlighted"
-                              searchWords={[search.query]}
-                              textToHighlight={post.frontmatter.description}
-                              autoEscape
-                            />
-                          </p>
-                          <div className="postInfoContainer">
-                            <div className="postInfo">
-                              <div className="postInfoImgContainer">
-                                <img src={calendarIcon} alt="date" />
-                              </div>
-                              <div>
-                                <p>{post.frontmatter.date}</p>
-                              </div>
-                            </div>
-                            <div className="postInfo">
-                              <div className="postInfoImgContainer">
-                                <img src={authorIcon} alt="author" />
-                              </div>
-                              <div>
-                                <p>{post.frontmatter.author}</p>
-                              </div>
-                            </div>
-                            <div className="postInfo">
-                              <div className="postInfoImgContainer">
-                                <img src={clockIcon} alt="readingTime" />
-                              </div>
-                              <div>
-                                <p>{post.frontmatter.readingTime} mins read</p>
-                              </div>
-                            </div>
-                          </div>
-                          <Highlighter
-                            highlightClassName="Highlighted"
-                            searchWords={[search.query]}
-                            textToHighlight={displayBody}
-                            autoEscape
-                          />
-                        </section>
-                        <section className="sideBar" />
-                      </div>
-                    </article>
-                  </div>
-                </div>
-              );
-            })}
+            {filteredPosts.map(post => (
+              <BlogEntry post={post} query={search.query} />
+            ))}
           </div>
         </div>
         <Sidebar recentPosts={recentPosts} postsPerYear={postsPerYear} />
