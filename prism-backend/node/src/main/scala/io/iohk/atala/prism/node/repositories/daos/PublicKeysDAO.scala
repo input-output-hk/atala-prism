edbed236b667e5d420005022175d43997b35496b
Alexis Hernandez <alexis.hernandez@iohk.io>
2021-11-10 19:22:53 -0700

    ATA-5705: Fix findKey DAO method from node (#410)
    The SQL query was retrieving the same values for the addedOn and
    the revokedOn models, which is fixed.

--- prism-backend/node/src/main/scala/io/iohk/atala/prism/node/repositories/daos/PublicKeysDAO.scala
+++ prism-backend/node/src/main/scala/io/iohk/atala/prism/node/repositories/daos/PublicKeysDAO.scala
@@ -30,9 +30,9 @@ object PublicKeysDAO {
 
   def find(didSuffix: DidSuffix, keyId: String): ConnectionIO[Option[DIDPublicKeyState]] = {
     sql"""
-         |SELECT did_suffix, key_id, key_usage, curve, compressed, added_on, added_on_absn, added_on_osn, added_on_transaction_id, ledger,
-         |       added_on, added_on_absn, added_on_osn, revoked_on_transaction_id, ledger,
-         |       revoked_on, revoked_on_absn, revoked_on_osn
+         |SELECT did_suffix, key_id, key_usage, curve, compressed,
+         |       added_on, added_on_absn, added_on_osn, added_on_transaction_id, ledger,
+         |       revoked_on, revoked_on_absn, revoked_on_osn, revoked_on_transaction_id, ledger
          |FROM public_keys
          |WHERE did_suffix = $didSuffix AND key_id = $keyId
        """.stripMargin.query[DIDPublicKeyState].option
@@ -40,9 +40,9 @@ object PublicKeysDAO {
 
   def findAll(didSuffix: DidSuffix): ConnectionIO[List[DIDPublicKeyState]] = {
     sql"""
-         |SELECT did_suffix, key_id, key_usage, curve, compressed, added_on, added_on_absn, added_on_osn, added_on_transaction_id, ledger,
-         |       added_on, added_on_absn, added_on_osn, revoked_on_transaction_id, ledger,
-         |       revoked_on, revoked_on_absn, revoked_on_osn
+         |SELECT did_suffix, key_id, key_usage, curve, compressed,
+         |       added_on, added_on_absn, added_on_osn, added_on_transaction_id, ledger,
+         |       revoked_on, revoked_on_absn, revoked_on_osn, revoked_on_transaction_id, ledger
          |FROM public_keys
          |WHERE did_suffix = $didSuffix
        """.stripMargin.query[DIDPublicKeyState].to[List]
