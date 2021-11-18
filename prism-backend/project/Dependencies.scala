0e2916df29735091c6166bd450afdfd37948fe46
Vladislav Podtelkin <vladislav.podtelkin@iohk.io>
2021-11-18 19:46:39 +0200

    Correct hash for SDK dependency, add jetbrans space resolver

--- prism-backend/project/Dependencies.scala
+++ prism-backend/project/Dependencies.scala
@@ -31,7 +31,7 @@ object versions {
   val twirl = "1.5.1"
   val typesafeConfig = "1.4.1"
   val http4s = "0.21.7"
-  val prismSdk = "1.3.0-build-19-38b990b6"
+  val prismSdk = "1.3.0-build-19-e6500860"
 }
 
 object Dependencies {
