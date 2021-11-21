639b16268a45c50682d43bbfad8d4e734b2140d7
Atala Dev <atala.dev@iohk.io>
2021-11-21 00:12:08 +0000

    Update client3:async-http-client-backend-cats, ... to 3.3.17 in master

--- prism-backend/project/Dependencies.scala
+++ prism-backend/project/Dependencies.scala
@@ -25,7 +25,7 @@ object versions {
   val scalatestplus = s"$scalatest.0"
   val scalapb = "0.11.6"
   val slf4j = "1.7.32"
-  val sttp = "3.3.16"
+  val sttp = "3.3.17"
   val tofu = "0.10.6"
   val tofuDerevo = "0.12.7"
   val twirl = "1.5.1"
