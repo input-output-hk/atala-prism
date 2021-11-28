78c21039d8483f5fea8f19459e9a7dd5780f044e
Atala Dev <atala.dev@iohk.io>
2021-11-28 00:12:56 +0000

    Update vault-api-jvm to 0.1.0-build-2-96cc137d in master

--- prism-backend/project/Dependencies.scala
+++ prism-backend/project/Dependencies.scala
@@ -32,7 +32,7 @@ object versions {
   val typesafeConfig = "1.4.1"
   val http4s = "0.21.7"
   val prismSdk = "1.3.0-build-26-52d80580"
-  val vaultSdk = "0.1.0-build-1-d914b564"
+  val vaultSdk = "0.1.0-build-2-96cc137d"
 }
 
 object Dependencies {
