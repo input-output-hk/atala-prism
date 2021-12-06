059b898c0905497c0545a8556a3b423d5c81cac7
Milos Dzepina <milos.dzepina@iohk.io>
2021-12-06 14:28:09 +0100

    cleanup: remove unused group and contacts stores

--- prism-management-console-web/src/hooks/useGroupStore.js
+++ prism-management-console-web/src/hooks/useGroupStore.js
@@ -1,39 +1,5 @@
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import { GlobalStateContext } from '../stores';
-
-import { useUpdateEffect } from './useUpdateEffect';
-
-// TODO: delete when all usage is refactored
-export const useGroupStore = () => {
-  const { groupStore } = useContext(GlobalStateContext);
-
-  return groupStore;
-};
-
-// TODO: delete when all usage is refactored
-export const useGroupUiState = ({ reset } = { reset: false }) => {
-  const { groupUiState } = useGroupStore();
-  const {
-    triggerSearch,
-    resetState,
-    nameFilter,
-    dateFilter,
-    sortingKey,
-    sortDirection
-  } = groupUiState;
-
-  useEffect(() => {
-    if (reset) resetState();
-  }, [reset, resetState]);
-
-  const sortingAndFiltersDependencies = [nameFilter, dateFilter, sortingKey, sortDirection];
-
-  useUpdateEffect(() => {
-    triggerSearch();
-  }, [...sortingAndFiltersDependencies, triggerSearch]);
-
-  return groupUiState;
-};
 
 /**
  *
