19d518d338bde160c87c44559b9842912e3cc4a2
Ana Clara Alleva <aalleva@atixlabs.com>
2021-12-09 10:06:14 -0300

    fix: align inputs on template content tab

--- prism-management-console-web/src/components/contactDetail/atoms/row/Row.jsx
+++ prism-management-console-web/src/components/contactDetail/atoms/row/Row.jsx
@@ -11,33 +11,32 @@ const Row = ({ theme, groupName, contacts, groupId, contactId, onDelete }) => {
   const classname = `RowInfo ${theme}`;
   return (
     <div className={classname}>
+      <div className="contactsTitle">
       <div className="groupNameContainer">
         <div className="img">
           <img className="IconUniversity" src={IdIcon} alt="Icon University" />
         </div>
+      </div>
         <div className="groupTitle">
           <p>{t('contacts.detail.dataTitle')}</p>
           <span>{groupName}</span>
-        </div>
-      </div>
-      <div className="contactsTitle">
-        <div>
-          <img className="img" src={peopleIcon} alt="Icon Group" />
         </div>
         <div className="contactsContainer">
           <p>{contacts}</p>
           <span style={{ textTransform: 'uppercase' }}>{t('contacts.detail.contacts')}</span>
         </div>
       </div>
-      {onDelete && (
-        <CustomButton
-          buttonProps={{
-            onClick: () => onDelete(groupId, [contactId]),
-            className: 'theme-link'
-          }}
-          buttonText={t('actions.delete')}
-        />
-      )}
+      <div>
+        {onDelete && (
+          <CustomButton
+            buttonProps={{
+              onClick: () => onDelete(groupId, [contactId]),
+              className: 'theme-link'
+            }}
+            buttonText={t('actions.delete')}
+          />
+        )}
+      </div>
     </div>
   );
 };
