cb8ed6a73b554a43b11774525bc730b1632160cd
Shota Jolbordi <shota.jolbordi@iohk.io>
2021-12-16 08:02:54 +0400

    Add created_at

--- prism-backend/management-console/src/main/resources/db/migration/V19__add_institution_id_to_credential_type_categories.sql
+++ prism-backend/management-console/src/main/resources/db/migration/V19__add_institution_id_to_credential_type_categories.sql
@@ -1,4 +1,5 @@
 ALTER TABLE credential_type_categories
+    ADD COLUMN created_at timestamp with time zone not null default current_timestamp,
     ADD COLUMN institution_id UUID,
     ADD CONSTRAINT credential_type_categories_institution_id
         FOREIGN KEY (institution_id)
