1ab060de4df6198f4878f1e78d161f31ea397397
Atala Dev <atala.dev@iohk.io>
2021-12-19 00:13:22 +0000

    Update grpc-context, grpc-netty, ... to 1.43.0 in master

--- prism-backend/project/Dependencies.scala
+++ prism-backend/project/Dependencies.scala
@@ -13,7 +13,7 @@ object versions {
   val enumeratum = "1.7.0"
   val enumeratumDoobie = "1.7.0"
   val flyway = "8.2.1"
-  val grpc = "1.42.1"
+  val grpc = "1.43.0"
   val kamon = "2.4.2"
   val logback = "1.2.7"
   val logbackLogstash = "7.0.1"
