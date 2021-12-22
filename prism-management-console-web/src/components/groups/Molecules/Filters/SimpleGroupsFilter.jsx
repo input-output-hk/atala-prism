c12700449baaf69138fcc694eac811e3652b3e86
dbrosio3 <dbrosio@atixlabs.com>
2021-12-22 16:11:45 -0300

    ATA-5987: removing unnecesary observer tag

--- prism-management-console-web/src/components/groups/Molecules/Filters/SimpleGroupsFilter.jsx
+++ prism-management-console-web/src/components/groups/Molecules/Filters/SimpleGroupsFilter.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
-import { observer } from 'mobx-react-lite';
 import SearchBar from '../../../common/Atoms/SearchBar/SearchBar';
 import './_style.scss';
 
-const SimpleGroupsFilter = observer(({ filterSortingProps }) => {
+const SimpleGroupsFilter = ({ filterSortingProps }) => {
   const { t } = useTranslation();
 
   const { setFilterValue } = filterSortingProps;
@@ -20,12 +19,12 @@ const SimpleGroupsFilter = observer(({ filterSortingProps }) => {
       </div>
     </div>
   );
-});
+};
 
 SimpleGroupsFilter.propTypes = {
   filterSortingProps: PropTypes.shape({
     setFilterValue: PropTypes.func.isRequired
-  })
+  }).isRequired
 };
 
 export default SimpleGroupsFilter;
