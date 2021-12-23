bdd2d451d60ead7bbb631c8a07396ee7c84ebea8
Shailesh <shailesh.patil@iohk.io>
2021-12-23 12:15:19 +0000

    HikariConfig
    Signed-off-by: Shailesh <shailesh.patil@iohk.io>

--- prism-backend/common/src/main/scala/io/iohk/atala/prism/repositories/TransactorFactory.scala
+++ prism-backend/common/src/main/scala/io/iohk/atala/prism/repositories/TransactorFactory.scala
@@ -1,23 +1,18 @@
 package io.iohk.atala.prism.repositories
 
-import cats.effect.{Async, Resource, Sync}
+import java.util.concurrent.Executors
+import cats.effect.{Async, Blocker, ContextShift, Resource, Sync}
 import com.zaxxer.hikari.HikariConfig
 import doobie.hikari.HikariTransactor
-import doobie.util.ExecutionContexts
 import org.flywaydb.core.Flyway
+
+import scala.concurrent.ExecutionContext
 
 object TransactorFactory {
 
-  case class Config(
-      username: String,
-      password: String,
-      jdbcUrl: String,
-      awaitConnectionThreads: Int = 8
-  )
+  case class Config(username: String, password: String, jdbcUrl: String, awaitConnectionThreads: Int = 8)
 
-  def transactorConfig(
-      globalConfig: com.typesafe.config.Config
-  ): TransactorFactory.Config = {
+  def transactorConfig(globalConfig: com.typesafe.config.Config): TransactorFactory.Config = {
     val dbConfig = globalConfig.getConfig("db")
 
     val url = dbConfig.getString("url")
@@ -32,30 +27,39 @@ object TransactorFactory {
     )
   }
 
-  def transactor[A[_]: Async](
-      config: Config
-  ): Resource[A, HikariTransactor[A]] = {
+  // We need a ContextShift[A] before we can construct a Transactor[A]. The passed ExecutionContext
+  // is where nonblocking operations will be executed.
+  def transactor[A[_]: Async: ContextShift](config: Config): Resource[A, HikariTransactor[A]] = {
+    // Threads for awaiting on database connection. After some local performance tests it turned out
+    // that the best performance can be achieved when this is equal to number of logical processor cores
+    val awaitConnectionExecutor =
+      ExecutionContext.fromExecutor(Executors.newFixedThreadPool(config.awaitConnectionThreads))
 
-    // https://github.com/brettwooldridge/HikariCP/wiki/About-Pool-Sizing
-    val poolSize = (config.awaitConnectionThreads * 2) + 1
+    // Threads for executing blocking JDBC operations. After some local performance tests it turned out
+    // that the best performance can be achieved when cached thread pool is used.
+    val executeJdbcBlocker =
+      Blocker.liftExecutionContext(ExecutionContext.fromExecutor(Executors.newCachedThreadPool()))
+
+    val poolSize = (config.awaitConnectionThreads * 3) + 1
     val hikariConfig = new HikariConfig()
     hikariConfig.setJdbcUrl(config.jdbcUrl)
     hikariConfig.setUsername(config.username)
     hikariConfig.setPassword(config.password)
     hikariConfig.setAutoCommit(false)
-    hikariConfig.setLeakDetectionThreshold(300000)
+    hikariConfig.setLeakDetectionThreshold(60000)
     hikariConfig.setMinimumIdle(poolSize)
     hikariConfig.setMaximumPoolSize(poolSize) // Both Pool size amd Minimum Idle should same and is recommended
     hikariConfig.setDriverClassName("org.postgresql.Driver")
     hikariConfig.addDataSourceProperty("cachePrepStmts", "true")
     hikariConfig.addDataSourceProperty("prepStmtCacheSize", "250")
     hikariConfig.addDataSourceProperty("prepStmtCacheSqlLimit", "2048")
-    for {
-      // Resource yielding a transactor configured with a bounded connect EC and an unbounded
-      // transaction EC. Everything will be closed and shut down cleanly after use.
-      ce <- ExecutionContexts.fixedThreadPool[A](config.awaitConnectionThreads) // our connect EC
-      xa <- HikariTransactor.fromHikariConfig[A](hikariConfig, ce)
-    } yield xa
+
+    // Hikari transactor is used in order to reuse connections to database
+    HikariTransactor.fromHikariConfig[A](
+      hikariConfig,
+      awaitConnectionExecutor, // await connection here
+      executeJdbcBlocker // execute JDBC operations here
+    )
   }
 
   /** Run db migrations with Flyway.
@@ -63,10 +67,7 @@ object TransactorFactory {
     * @return
     *   number of applied migrations
     */
-  def runDbMigrations[A[_]: Sync](
-      transactor: HikariTransactor[A],
-      classLoader: ClassLoader
-  ): Resource[A, Int] =
+  def runDbMigrations[A[_]: Sync](transactor: HikariTransactor[A], classLoader: ClassLoader): Resource[A, Int] =
     Resource.eval(
       transactor.configure(dataSource =>
         Sync[A].delay(
