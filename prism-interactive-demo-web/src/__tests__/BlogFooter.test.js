3f9be5af4b1ea56628157cf943f34b2fad1b1116
Atix <nicolas.bianco@iohk.io>
2021-12-29 13:19:21 -0300

    ATA-5681 Check for current year on test

--- prism-interactive-demo-web/src/__tests__/BlogFooter.test.js
+++ prism-interactive-demo-web/src/__tests__/BlogFooter.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { cleanup, render, screen } from '@testing-library/react';
+import moment from 'moment';
 import BlogFooter from '../components/footer/BlogFooter';
 
 describe('<BlogFooter />', () => {
@@ -14,6 +15,6 @@ describe('<BlogFooter />', () => {
     expect(screen.getByAltText('Atala-Logo')).toBeTruthy();
     expect(screen.getByText('Terms and Conditions')).toBeTruthy();
     expect(screen.getByText('Privacy Policy')).toBeTruthy();
-    expect(screen.getByText('Copyright © 2021 IOHK')).toBeTruthy();
+    expect(screen.getByText(`Copyright © ${moment().year()} IOHK`)).toBeTruthy();
   });
 });
