ee6a2008dab349d0dbc90ec898d8f4657f9711d9
Atala Dev <atala.dev@iohk.io>
2022-02-13 00:15:12 +0000

    Update typesafe:config to 1.4.2 in master

--- prism-backend/project/Dependencies.scala
+++ prism-backend/project/Dependencies.scala
@@ -30,7 +30,7 @@ object versions {
   val tofu = "0.10.6"
   val tofuDerevo = "0.12.8"
   val twirl = "1.5.1"
-  val typesafeConfig = "1.4.1"
+  val typesafeConfig = "1.4.2"
   val http4s = "0.21.7"
   val fs2 = "3.2.4"
   val prismSdk = "1.3.0-build-6-84b9950e"
