cb03047dec30e7b7e3df8a11dbfeae95e8b7d4be
atala-dev <57987237+atala-dev@users.noreply.github.com>
2022-02-14 16:52:46 +0300

    Update flyway-core to 8.4.4 in master (#643)

--- prism-backend/project/Dependencies.scala
+++ prism-backend/project/Dependencies.scala
@@ -13,7 +13,7 @@ object versions {
   val doobie = "1.0.0-RC2"
   val enumeratum = "1.7.0"
   val enumeratumDoobie = "1.7.0"
-  val flyway = "8.4.2"
+  val flyway = "8.4.4"
   val grpc = "1.43.2"
   val kamon = "2.4.2"
   val logback = "1.2.10"
