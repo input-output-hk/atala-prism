d7c20f433b3ecb877aba532a1db23f940651c8b9
atala-dev <atala-dev@users.noreply.github.com>
2022-06-15 11:37:09 +0000

    Bump SDK version to v1.3.3-snapshot-1655289162-0c749c7

--- prism-backend/project/Dependencies.scala
+++ prism-backend/project/Dependencies.scala
@@ -33,7 +33,7 @@ object versions {
   val typesafeConfig = "1.4.2"
   val http4s = "0.21.7"
   val fs2 = "3.2.5"
-  val prismSdk = "v1.3.3-snapshot-1654671951-8fa92eb"
+  val prismSdk = "v1.3.3-snapshot-1655289162-0c749c7"
   val vaultSdk = "0.1.0-build-2-96cc137d"
 }
 
