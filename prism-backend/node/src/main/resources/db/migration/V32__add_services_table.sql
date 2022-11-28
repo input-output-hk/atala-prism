7ad5a0ea848fbe52d0305a65b5d964573926ae5b
Shota Jolbordi <shota.jolbordi@iohk.io>
2022-11-28 20:48:30 +0400

    Add services migration

---
+++ prism-backend/node/src/main/resources/db/migration/V32__add_services_table.sql
@@ -0,0 +1,27 @@
+CREATE TABLE services
+(
+    service_id                ID_TYPE PRIMARY KEY      NOT NULL,
+    did_suffix                ID_TYPE                  NOT NULL,
+    type                      TEXT                     NOT NULL,
+
+    added_on_transaction_id   TRANSACTION_ID           NOT NULL,
+    added_on                  TIMESTAMP WITH TIME ZONE NOT NULL,
+    added_on_absn             INTEGER                  NOT NULL,
+    added_on_ons              INTEGER                  NOT NULL,
+
+    revoked_on_transaction_id TRANSACTION_ID           DEFAULT NULL,
+    revoked_on                TIMESTAMP WITH TIME ZONE DEFAULT NULL,
+    revoked_on_absn           INTEGER                  DEFAULT NULL,
+    revoked_on_ons            INTEGER                  DEFAULT NULL,
+
+    updated_on_transaction_id TRANSACTION_ID           DEFAULT NULL,
+    updated_on                TIMESTAMP WITH TIME ZONE DEFAULT NULL,
+    updated_on_absn           INTEGER                  DEFAULT NULL,
+    updated_on_ons            INTEGER                  DEFAULT NULL,
+
+    ledger                    VARCHAR(32)              NOT NULL,
+
+    CONSTRAINT services_did_suffux_fk
+        FOREIGN KEY (did_suffix) REFERENCES did_data (did_suffix)
+
+);
