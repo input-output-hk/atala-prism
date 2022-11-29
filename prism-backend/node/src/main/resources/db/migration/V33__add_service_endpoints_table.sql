4fdb70108500c9d4b4bfc71ce0f1a5b2647e223a
Shota Jolbordi <shota.jolbordi@iohk.io>
2022-11-29 04:06:07 +0400

    Add service endpoints migration

---
+++ prism-backend/node/src/main/resources/db/migration/V33__add_service_endpoints_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE service_endpoints
+(
+    service_endpoint_id ID_TYPE PRIMARY KEY NOT NULL,
+    service_id          ID_TYPE             NOT NULL,
+    url                 TEXT                NOT NULL,
+
+    CONSTRAINT service_endpoints_service_id_fk
+        FOREIGN KEY (service_id) REFERENCES services (service_id)
+
+);
