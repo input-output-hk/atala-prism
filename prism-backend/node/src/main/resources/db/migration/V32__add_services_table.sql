fe0df9550e272d033735d08628f8b69c4f04b35c
Shota Jolbordi <shota.jolbordi@iohk.io>
2022-12-01 04:34:52 +0400

    Change database according to the new flow

--- prism-backend/node/src/main/resources/db/migration/V32__add_services_table.sql
+++ prism-backend/node/src/main/resources/db/migration/V32__add_services_table.sql
@@ -1,33 +1,29 @@
 CREATE TABLE services
 (
-    service_id                ID_TYPE PRIMARY KEY      NOT NULL,
-    id                        TEXT                     NOT NULl,
-    did_suffix                ID_TYPE                  NOT NULL,
-    type                      TEXT                     NOT NULL,
+    service_id              ID_TYPE PRIMARY KEY      NOT NULL,
+    id                      TEXT                     NOT NULl,
+    did_suffix              ID_TYPE                  NOT NULL,
+    type                    TEXT                     NOT NULL,
 
-    added_on_transaction_id   TRANSACTION_ID           NOT NULL,
-    added_on                  TIMESTAMP WITH TIME ZONE NOT NULL,
-    added_on_absn             INTEGER                  NOT NULL,
+    added_on_transaction_id TRANSACTION_ID           NOT NULL,
+    added_on                TIMESTAMP WITH TIME ZONE NOT NULL,
+    added_on_absn           INTEGER                  NOT NULL,
     --^ Atala Block Sequence Number (absn) of the operation that added the key
-    added_on_ons              INTEGER                  NOT NULL,
+    added_on_ons            INTEGER                  NOT NULL,
     --^ Operation Sequence Number (osn) of the operation that added the key
 
-    revoked_on_transaction_id TRANSACTION_ID           DEFAULT NULL,
-    revoked_on                TIMESTAMP WITH TIME ZONE DEFAULT NULL,
-    revoked_on_absn           INTEGER                  DEFAULT NULL,
-    revoked_on_ons            INTEGER                  DEFAULT NULL,
+    deleted_by_update       BOOLEAN DEFAULT FALSE,
+    deleted_by_deletion     BOOLEAN DEFAULT FALSE,
 
-    updated_on_transaction_id TRANSACTION_ID           DEFAULT NULL,
-    updated_on                TIMESTAMP WITH TIME ZONE DEFAULT NULL,
-    updated_on_absn           INTEGER                  DEFAULT NULL,
-    updated_on_ons            INTEGER                  DEFAULT NULL,
+    deleted_on              TIMESTAMP WITH TIME ZONE NOT NULL,
+    deleted_on_absn         INTEGER                  NOT NULL,
+    deleted_on_ons          INTEGER                  NOT NULL,
 
-    ledger                    VARCHAR(32)              NOT NULL,
+    ledger                  VARCHAR(32)              NOT NULL,
 
     CONSTRAINT services_did_suffix_fk
         FOREIGN KEY (did_suffix) REFERENCES did_data (did_suffix),
 
     CONSTRAINT unique_id_and_did_suffix_on_services UNIQUE (did_suffix, id)
-
 
 );
