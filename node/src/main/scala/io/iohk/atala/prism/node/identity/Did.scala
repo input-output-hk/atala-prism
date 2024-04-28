166b62378a55f9041b7d12cb450a44a607788b18
Shota Jolbordi <shota.jolbordi@iohk.io>
2024-04-28 06:34:17 +0400

    formatting
    Signed-off-by: Shota Jolbordi <shota.jolbordi@iohk.io>

--- node/src/main/scala/io/iohk/atala/prism/node/identity/Did.scala
+++ node/src/main/scala/io/iohk/atala/prism/node/identity/Did.scala
@@ -13,7 +13,8 @@ object Did {
     val didPattern = "did:([A-Za-z0-9]+):((?:[a-zA-Z0-9._-]|%[0-9A-Fa-f]{2})+)(?::([a-zA-Z0-9_-]+))?".r
     didStr match {
       case didPattern(methodName, methodSpecificId, encodedState) =>
-        val methodSpecificIdWithState = Option(encodedState).fold(methodSpecificId)(encodedState => s"$methodSpecificId:$encodedState")
+        val methodSpecificIdWithState =
+          Option(encodedState).fold(methodSpecificId)(encodedState => s"$methodSpecificId:$encodedState")
         Did(DidMethod.fromString(methodName), DidMethodSpecificId.fromString(methodSpecificIdWithState))
       case _ => throw new IllegalArgumentException(s"Invalid DID format: $didStr")
     }
