3abf2f8bb619dca4831a75d4f0183b124b25f71c
Ezequiel Postan <ezequiel.postan@iohk.io>
2024-05-02 22:26:45 -0300

    ATL-7040: Delete DID based authentication (#843)
    This commit deletes the legacy DID based authentication

--- node/src/main/scala/io/iohk/atala/prism/node/tracing/Tracing.scala
+++ node/src/main/scala/io/iohk/atala/prism/node/tracing/Tracing.scala
@@ -1,15 +1,14 @@
 package io.iohk.atala.prism.node.tracing
 
 import io.grpc.Context
-import io.iohk.atala.prism.node.auth.grpc.GrpcAuthenticationHeaderParser
 import io.iohk.atala.prism.node.logging.TraceId
 
 object Tracing {
   def trace[F[_], A](
-      fa: TraceId => F[A],
-      parser: GrpcAuthenticationHeaderParser = GrpcAuthenticationHeaderParser
+      fa: TraceId => F[A]
   ): F[A] = {
-    val traceId = parser.getTraceId(Context.current())
+    val ctx = Context.current()
+    val traceId = Option(Context.key("trace-id").get(ctx)).map(TraceId(_)).getOrElse(TraceId.generateYOLO)
     fa(traceId)
   }
 }
